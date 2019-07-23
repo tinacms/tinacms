ca8ac63b0cf0fad4174bfb5ceab5a3d889bfe205
Nolan Phillips <ncphillips.19@gmail.com>
2019-07-23 16:07:41 -0300

    add cms package being used in gatsby-demo

--- packages/gatsby-demo/gatsby-browser.js
+++ packages/gatsby-demo/gatsby-browser.js
@@ -1,3 +1,8 @@
 // custom typefaces
 import "typeface-montserrat"
 import "typeface-merriweather"
+import { foo } from "@forestryio/cms"
+
+export const onClientEntry = () => {
+  console.log(foo)
+}
