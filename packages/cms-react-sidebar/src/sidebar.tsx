a0677a2e54b89d0ee705055db3ba998f822b6e94
Nolan Phillips <ncphillips.19@gmail.com>
2019-07-25 14:09:56 -0300

    move useSubscribable to cms-react

--- packages/cms-react-sidebar/src/sidebar.tsx
+++ packages/cms-react-sidebar/src/sidebar.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react'
 import Frame from 'react-frame-component'
 import { FormBuilder, FieldsBuilder } from '@forestryio/cms-final-form-builder'
-import { useCMS } from '@forestryio/cms-react'
+import { useCMS, useSubscribable } from '@forestryio/cms-react'
 
 export const Sidebar = () => {
   const cms = useCMS()
 
-  useSubscribeable(cms.forms)
+  useSubscribable(cms.forms)
 
   const form = cms.forms.all()[0]
   return (
@@ -27,17 +27,4 @@ export const Sidebar = () => {
       )}
     </Frame>
   )
-}
-
-/**
- * TODO: Is there a better approach?
- * TODO: move to cms-react
- */
-function useSubscribeable(thing: any) {
-  let [_, s] = React.useState(0)
-  React.useEffect(() => {
-    let forceUpdate = () => s(x => x + 1)
-    thing.subscribe(forceUpdate)
-    return () => thing.unsubscribe(forceUpdate)
-  })
 }
