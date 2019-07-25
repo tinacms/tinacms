d6209a275abb059ee49c47dfccf908a2b41a90e1
Nolan Phillips <ncphillips.19@gmail.com>
2019-07-25 12:48:52 -0300

    cms-forms remove forms

--- packages/cms/src/cms-forms/index.ts
+++ packages/cms/src/cms-forms/index.ts
@@ -32,6 +32,10 @@ export class FormManager extends Subscribeable {
     return this.__forms[name]
   }
 
+  removeForm(name: string) {
+    delete this.__forms[name]
+  }
+
   all() {
     return Object.keys(this.__forms).map(name => this.__forms[name])
   }
