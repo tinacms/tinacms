bcb4a709ee88b0ea795517e9d30776cdc271193f
James O'Halloran <james.p.ohalloran@gmail.com>
2019-07-29 16:09:42 -0300

    Fields meta (#21)
    * Add a wrapper to add meta labels around an input field
    
    * ability to add extra props onto fields
    
    * Don't require "extraProps" to be input as a child prop
    
    * pass all parent field components through through to the child field

--- packages/xeditor-react/src/fields/TextInput.tsx
+++ packages/xeditor-react/src/fields/TextInput.tsx
@@ -1,14 +1,7 @@
 import * as React from 'react'
-import { TextField } from '@forestryio/xeditor-react-fields'
+import { TextField, InputProps } from '@forestryio/xeditor-react-fields'
+import { wrapFieldsWithMeta } from './wrapFieldWithMeta'
 
-export const TextInput = ({ field, input, meta, ...props }: any) => {
-  return (
-    <div {...props}>
-      <div>
-        <label htmlFor={name}>{field.name}</label>
-      </div>
-      <TextField {...input} />
-      {meta.error && <p>{meta.error}</p>}
-    </div>
-  )
-}
+export const TextInput = wrapFieldsWithMeta<InputProps>(({ input }) => (
+  <TextField {...input} />
+))
