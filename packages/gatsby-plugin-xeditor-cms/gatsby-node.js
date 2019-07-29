303e56a002818145e30b54fc9de60f9a5712cf73
Nolan Phillips <ncphillips.19@gmail.com>
2019-07-29 12:24:50 -0300

    switch gatsby-node to typescript

--- packages/gatsby-plugin-xeditor-cms/gatsby-node.js
+++ packages/gatsby-plugin-xeditor-cms/gatsby-node.js
@@ -1,39 +1,6 @@
-const express = require('express')
-const cors = require('cors')
-const fs = require('fs')
-const yaml = require('js-yaml')
-
-let app
-
-exports.onPreBootstrap = () => {
-  app = express()
-  app.use(
-    cors({
-      origin: function(origin, callback) {
-        callback(null, true)
-      },
-    })
-  )
-  app.use(express.json())
-  app.get('/', (req, res) => {
-    res.json({ status: 'ok' })
-  })
-
-  app.put('/markdownRemark', (req, res) => {
-    let contents =
-      '---\n' + yaml.dump(req.body.frontmatter) + '---\n' + req.body.html
-    res.send(contents)
-    fs.writeFileSync(req.body.fileAbsolutePath, contents)
-  })
-
-  app.listen(4567, () =>
-    console.log('xeditor local backend running on port 4567')
-  )
-}
-
-/**
-
-/git/push
-/git/pull
-
-*/
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var server_1 = require("./markdownRemark/server");
+exports.onPreBootstrap = function () {
+    server_1.markdownRemarkServer();
+};
