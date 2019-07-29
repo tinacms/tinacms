1e9e3a277e45c5c98f9f26a80c1c67fe1ed1f7e7
Nolan Phillips <ncphillips.19@gmail.com>
2019-07-29 13:37:37 -0300

    gatsby-plugin: useRemarkForm requires the fileAbsolutePath

--- packages/gatsby-plugin-xeditor-cms/errors.ts
+++ packages/gatsby-plugin-xeditor-cms/errors.ts
@@ -17,12 +17,27 @@ export const ERROR_MISSING_REMARK_ID =
   'useRemarkForm(remark) Required attribute `id` was not found on `remark` argument' +
   `
 
-1. Check if the \`id\` attribute is included in the GraphQL query:
+1. Check if the \`id\` attribute is included in the GraphQL query. For example:
 
 export const pageQuery = graphql\`
   query BlogPostBySlug($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
       id
+      // etc...
+    }
+ }
+\`
+  `
+export const ERROR_MISSING_REMARK_PATH =
+  'useRemarkForm(remark) Required attribute `fileAbsolutePath` was not found on `remark` argument' +
+  `
+
+1. Check if the \`fileAbsolutePath\` attribute is included in the GraphQL query. For example:
+
+export const pageQuery = graphql\`
+  query BlogPostBySlug($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      fileAbsolutePath
       // etc...
     }
  }
