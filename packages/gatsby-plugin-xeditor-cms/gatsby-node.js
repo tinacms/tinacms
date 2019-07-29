5970a2667fe4c6272b8a6fc7bb30e4fcdce2b584
Nolan Phillips <ncphillips.19@gmail.com>
2019-07-29 12:27:51 -0300

    remove gatsby-node js

--- packages/gatsby-plugin-xeditor-cms/gatsby-node.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var server_1 = require("./markdownRemark/server");
-exports.onPreBootstrap = function () {
-    server_1.markdownRemarkServer();
-};
