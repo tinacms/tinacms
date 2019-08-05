2a247218c077d8796af23e8ad0e3d6c9aa87c4e1
DJ <dj@walker.dev>
2019-08-05 11:00:35 -0400

    extract gatsby remark stuff into gatsby-xeditor-remark pkg

--- packages/gatsby/gatsby-plugin-xeditor-cms/index.ts
+++ packages/gatsby/gatsby-plugin-xeditor-cms/index.ts
@@ -5,8 +5,6 @@ import {
   ColorPickerField,
 } from '@forestryio/xeditor-react'
 
-export * from './markdownRemark'
-
 export let cms = new CMS()
 
 cms.forms.addFieldPlugin({ name: 'text', Component: TextInput })
