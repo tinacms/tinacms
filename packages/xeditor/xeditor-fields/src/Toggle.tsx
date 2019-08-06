30022e799df4e6ce8bc4db21a1c9399e2f0c4641
Nichlas W. Andersen <nichlas@itsnwa.com>
2019-08-06 16:53:52 +0200

    clean

--- packages/xeditor/xeditor-fields/src/Toggle.tsx
+++ packages/xeditor/xeditor-fields/src/Toggle.tsx
@@ -1,6 +1,6 @@
-import { FC } from "react"
-import styled from "styled-components"
-import * as React from "react"
+import { FC } from 'react'
+import styled from 'styled-components'
+import * as React from 'react'
 
 export interface ToggleProps {
   name: string
@@ -15,7 +15,12 @@ export interface ToggleProps {
 export const Toggle: FC<ToggleProps> = props => (
   <ToggleElement>
     <ToggleInput id={props.name} type="checkbox" {...props} />
-    <ToggleLabel htmlFor={props.name} role="switch" disabled={props.disabled}>
+    <ToggleLabel
+      htmlFor={props.name}
+      role="switch"
+      disabled={props.disabled}
+      {...props}
+    >
       <ToggleSwitch xmlns="http://www.w3.org/2000/svg">
         <g>
           <rect rx="10" id="container" height="20" width="40" />
@@ -24,8 +29,8 @@ export const Toggle: FC<ToggleProps> = props => (
             rx="8"
             id="dot"
             cy="10"
-            cx={props.value ? "30" : "10"}
-            fill={props.value ? "#000" : "#999"}
+            cx={props.value ? '30' : '10'}
+            fill={props.value ? '#000' : '#999'}
           />
         </g>
       </ToggleSwitch>
@@ -45,11 +50,11 @@ const ToggleLabel = styled.label`
   color: inherit;
   padding: 0;
   font: inherit;
-  opacity: ${props => (props.disabled ? "0.4" : "1")};
+  opacity: ${props => (props.disabled ? '0.4' : '1')};
   outline: inherit;
   width: 40px;
   height: 20px;
-  pointer-events: ${props => (props.disabled ? "none" : "inherit")};
+  pointer-events: ${props => (props.disabled ? 'none' : 'inherit')};
 `
 
 const ToggleSwitch = styled.svg`
@@ -73,7 +78,7 @@ const ToggleInput = styled.input`
   height: 100%;
   opacity: 0;
   margin: 0;
-  cursor: ${props => (props.disabled ? "not-allowed" : "pointer")};
+  cursor: ${props => (props.disabled ? 'not-allowed' : 'pointer')};
 
   &:focus + ${ToggleLabel} {
     > ${ToggleSwitch} {
