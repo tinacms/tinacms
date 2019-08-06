0cf9c026604de8206d9dc9138ed68c6ea38d834b
Nolan Phillips <ncphillips.19@gmail.com>
2019-08-06 11:24:22 -0300

    gatsby-xeditor-json writes changes to git onChange

--- packages/demo/demo-gatsby/src/components/bio.js
+++ packages/demo/demo-gatsby/src/components/bio.js
@@ -63,8 +63,11 @@ const Bio = () => {
         }}
       />
       <p>
-        Written by <strong>{author}</strong> who lives and works in San
-        Francisco building useful things.
+        Written by{" "}
+        <strong>
+          {nolan.firstName} {nolan.lastName}
+        </strong>{" "}
+        who lives and works in San Francisco building useful things.
         {` `}
         <a href={`https://twitter.com/${social.twitter}`}>
           You should follow him on Twitter
