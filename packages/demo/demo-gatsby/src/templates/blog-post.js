a725f708bd2e4f4d52eccdb4bd08114bad64e918
Nolan Phillips <ncphillips.19@gmail.com>
2019-08-09 10:25:08 -0300

    gatsby-xeditor-remark has remarkForm HOC

--- packages/demo/demo-gatsby/src/templates/blog-post.js
+++ packages/demo/demo-gatsby/src/templates/blog-post.js
@@ -8,123 +8,118 @@ import { rhythm, scale } from "../utils/typography"
 import { toMarkdownString } from "@forestryio/gatsby-plugin-xeditor"
 import { useCMS } from "@forestryio/cms-react"
 import { relative } from "path"
-import { RemarkForm } from "@forestryio/gatsby-xeditor-remark"
+import { remarkForm } from "@forestryio/gatsby-xeditor-remark"
 
 function BlogPostTemplate(props) {
-  const staticPost = props.data.markdownRemark
+  const post = props.data.markdownRemark
   const siteTitle = props.data.site.siteMetadata.title
   const { previous, next } = props.pageContext
 
   let cms = useCMS()
   let filepath = relative(
     "/home/dj/Forestry/cms/packages/demo/demo-gatsby/",
-    staticPost.fields.fileRelativePath
+    post.fields.fileRelativePath
   )
 
   return (
-    <RemarkForm
-      remark={staticPost}
-      fields={[
-        {
-          label: "Title",
-          name: "frontmatter.title",
-          component: "text",
-          required: true,
-        },
-        {
-          label: "Date",
-          name: "frontmatter.date",
-          component: function ReadOnly({ input }) {
-            return <div>{input.value}</div>
-          },
-        },
-        {
-          label: "Description",
-          name: "frontmatter.description",
-          component: "textarea",
-        },
-        { label: "Body", name: "rawMarkdownBody", component: "textarea" },
-      ]}
-      render={({ markdownRemark }) => {
-        let post = markdownRemark
-        return (
-          <Layout location={props.location} title={siteTitle}>
-            <SEO
-              title={post.frontmatter.title}
-              description={post.frontmatter.description || post.excerpt}
-            />
-            <h1
-              style={{
-                marginTop: rhythm(1),
-                marginBottom: 0,
-              }}
-            >
-              {post.frontmatter.title}
-            </h1>
-            <button onClick={() => cms.api.gitlab.authorize()}>Login</button>
-            <button
-              onClick={() =>
-                cms.api.gitlab.onSubmit({
-                  path: filepath,
-                  contents: toMarkdownString(post),
-                })
-              }
-            >
-              Save
-            </button>
-            <p
-              style={{
-                ...scale(-1 / 5),
-                display: `block`,
-                marginBottom: rhythm(1),
-              }}
-            >
-              {post.frontmatter.date}
-            </p>
-            <div
-              dangerouslySetInnerHTML={{
-                __html: props.data.markdownRemark.html,
-              }}
-            />
-            <hr
-              style={{
-                marginBottom: rhythm(1),
-              }}
-            />
-            <Bio />
+    <Layout location={props.location} title={siteTitle}>
+      <SEO
+        title={post.frontmatter.title}
+        description={post.frontmatter.description || post.excerpt}
+      />
+      <h1
+        style={{
+          marginTop: rhythm(1),
+          marginBottom: 0,
+        }}
+      >
+        {post.frontmatter.title}
+      </h1>
+      <button onClick={() => cms.api.gitlab.authorize()}>Login</button>
+      <button
+        onClick={() =>
+          cms.api.gitlab.onSubmit({
+            path: filepath,
+            contents: toMarkdownString(post),
+          })
+        }
+      >
+        Save
+      </button>
+      <p
+        style={{
+          ...scale(-1 / 5),
+          display: `block`,
+          marginBottom: rhythm(1),
+        }}
+      >
+        {post.frontmatter.date}
+      </p>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: props.data.markdownRemark.html,
+        }}
+      />
+      <hr
+        style={{
+          marginBottom: rhythm(1),
+        }}
+      />
+      <Bio />
 
-            <ul
-              style={{
-                display: `flex`,
-                flexWrap: `wrap`,
-                justifyContent: `space-between`,
-                listStyle: `none`,
-                padding: 0,
-              }}
-            >
-              <li>
-                {previous && (
-                  <Link to={previous.fields.slug} rel="prev">
-                    ← {previous.frontmatter.title}
-                  </Link>
-                )}
-              </li>
-              <li>
-                {next && (
-                  <Link to={next.fields.slug} rel="next">
-                    {next.frontmatter.title} →
-                  </Link>
-                )}
-              </li>
-            </ul>
-          </Layout>
-        )
-      }}
-    />
+      <ul
+        style={{
+          display: `flex`,
+          flexWrap: `wrap`,
+          justifyContent: `space-between`,
+          listStyle: `none`,
+          padding: 0,
+        }}
+      >
+        <li>
+          {previous && (
+            <Link to={previous.fields.slug} rel="prev">
+              ← {previous.frontmatter.title}
+            </Link>
+          )}
+        </li>
+        <li>
+          {next && (
+            <Link to={next.fields.slug} rel="next">
+              {next.frontmatter.title} →
+            </Link>
+          )}
+        </li>
+      </ul>
+    </Layout>
   )
 }
 
-export default BlogPostTemplate
+const EditableBlogPostTemplate = remarkForm({
+  fields: [
+    {
+      label: "Title",
+      name: "frontmatter.title",
+      component: "text",
+      required: true,
+    },
+    {
+      label: "Date",
+      name: "frontmatter.date",
+      component: function ReadOnly({ input }) {
+        return <div>{input.value}</div>
+      },
+    },
+    {
+      label: "Description",
+      name: "frontmatter.description",
+      component: "textarea",
+    },
+    { label: "Body", name: "rawMarkdownBody", component: "textarea" },
+  ],
+})(BlogPostTemplate)
+
+export default EditableBlogPostTemplate
 
 export const pageQuery = graphql`
   query BlogPostBySlug($slug: String!) {
