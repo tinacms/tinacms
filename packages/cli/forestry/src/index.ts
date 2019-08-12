b3c0b908a7cd06f3576034aa366fc9a584430397
James O'Halloran <james.p.ohalloran@gmail.com>
2019-08-12 14:49:12 -0300

    use custom config, log out test

--- packages/cli/forestry/src/index.ts
+++ packages/cli/forestry/src/index.ts
@@ -1,1 +1,12 @@
 #!/usr/bin/env node
+
+const clear = require('clear')
+const chalk = require('chalk')
+const figlet = require('figlet')
+const path = require('path')
+const program = require('commander')
+
+clear()
+console.log(
+  chalk.red(figlet.textSync('Forestry', { horizontalLayout: 'full' }))
+)
