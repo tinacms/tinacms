19667bdc1d69e5b049130f24cedf718580054973
James O'Halloran <james.p.ohalloran@gmail.com>
2019-08-13 14:11:45 -0300

    split out initServer script from root

--- packages/cli/forestry/src/index.js
+++ packages/cli/forestry/src/index.js
@@ -1,78 +1,16 @@
-require('dotenv').config()
-import clear from 'clear'
-import figlet from 'figlet'
-import open from 'open'
-import inquirer from 'inquirer'
-import express from 'express'
-import cors from 'cors'
-import chalk from 'chalk'
+import minimist from 'minimist'
+import { initServer } from './cmds/initServer'
 
-export async function initServer(options) {
-  clear()
-  console.log(
-    chalk.green(figlet.textSync('Forestry', { horizontalLayout: 'full' }))
-  )
+export function init() {
+  const args = minimist(process.argv.slice(2))
+  const cmd = args._[0]
 
-  const questions = [
-    {
-      name: 'gitProvider',
-      type: 'list',
-      message: 'Choose a git provider:',
-      choices: ['github', new inquirer.Separator(), 'gitlab'],
-    },
-    {
-      name: 'private',
-      type: 'confirm',
-      message: 'Is your repo private?:',
-    },
-  ]
-
-  const providerDetails = {
-    ['github']: {
-      baseUrl: 'https://github.com',
-      clientId: process.env.GITHUB_CLIENT_ID,
-    },
-    ['gitlab']: {
-      baseUrl: 'https://gitlab.com',
-      clientId: process.env.GITLAB_CLIENT_ID,
-    },
+  switch (cmd) {
+    case 'init-server':
+      initServer(args)
+      break
+    default:
+      console.error(`"${cmd}" is not a valid command!`)
+      break
   }
-
-  inquirer.prompt(questions).then(answers => {
-    const { clientId, baseUrl } = providerDetails[answers.gitProvider]
-
-    let app = express()
-    app.use(
-      cors({
-        origin: function(origin, callback) {
-          // TODO: Only accept from localhost.
-          callback(null, true)
-        },
-      })
-    )
-    app.get(`/${answers.gitProvider}/callback`, (req, res) => {
-      console.log('receieved my auth callback')
-      console.log(`code: ${req.query.code}`)
-      res.send(`<p>Authorizing with ${answers.gitProvider}</p>`)
-    })
-    app.listen(4568, () => {
-      console.log('------------------------------------------')
-      console.log('wait for the auth response')
-      console.log('------------------------------------------')
-    })
-
-    let query = '&response_type=token'
-    if (answers.gitProvider === 'github') {
-      query += '&scope=public_repo,write:repo_hook,user:email'
-      if (answers.private) {
-        query += '&scope=repo,write:repo_hook,user:email'
-      }
-    }
-
-    const authUrl = `${baseUrl}/login/oauth/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(
-      `http://localhost:4568/${answers.gitProvider}/callback`
-    )}${query}`
-
-    open(authUrl)
-  })
 }
