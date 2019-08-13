3eef44e33c912fa5250f7ef8a3003280cbbfc792
James O'Halloran <james.p.ohalloran@gmail.com>
2019-08-13 09:53:26 -0300

    start express server and grab token from github response

--- packages/cli/forestry/index.js
+++ packages/cli/forestry/index.js
@@ -7,6 +7,8 @@ const path = require('path')
 const program = require('commander')
 const open = require('open')
 const inquirer = require('inquirer')
+const express = require('express')
+const cors = require('cors')
 
 clear()
 console.log(
@@ -41,8 +43,28 @@ const providerDetails = {
 inquirer.prompt(questions).then(answers => {
   const { clientId, baseUrl } = providerDetails[answers.gitProvider]
 
+  let app = express()
+  app.use(
+    cors({
+      origin: function(origin, callback) {
+        // TODO: Only accept from localhost.
+        callback(null, true)
+      },
+    })
+  )
+  app.get('/github/callback', (req, res) => {
+    console.log('receieved my auth callback')
+    console.log(`code: ${req.query.code}`)
+    res.send('<p>Authorizing with Github</p>')
+  })
+  app.listen(4568, () => {
+    console.log('------------------------------------------')
+    console.log('wait for the auth response')
+    console.log('------------------------------------------')
+  })
+
   const authUrl = `${baseUrl}/login/oauth/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(
-    'https://forestry.io'
+    `http://localhost:4568/${answers.gitProvider}/callback`
   )}&response_type=token&scope=${answers.private ? 'private' : 'public'}`
 
   open(authUrl)
