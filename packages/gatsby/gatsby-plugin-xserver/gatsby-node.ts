06cf25d40d0667cb26fd0ca5109b22b1a76ab52e
DJ <dj@walker.dev>
2019-08-14 12:00:54 -0400

    xserver gatsby plugin

---
+++ packages/gatsby/gatsby-plugin-xserver/gatsby-node.ts
@@ -0,0 +1,23 @@
+import { XServer } from '@forestryio/xserver'
+
+interface PluginConfig {
+  resolve: string
+}
+
+interface PluginOptions {
+  pathPrefix?: string
+  envPrefix?: string
+  port?: Number
+  plugins?: Array<PluginConfig>
+}
+
+exports.onPreBootstrap = (_: any, pluginOptions: PluginOptions) => {
+  const xserver = new XServer(pluginOptions)
+  if (pluginOptions.plugins && pluginOptions.plugins.length > 0) {
+    pluginOptions.plugins.map((pluginConfig: PluginConfig) => {
+      let plugin = require(pluginConfig.resolve)
+      xserver.extend(plugin.extendXserver as any)
+    })
+  }
+  xserver.start(pluginOptions.port || 4567)
+}
