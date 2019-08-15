8eabad1d596fd301bbe923982b9d0f31f7b0f669
James O'Halloran <james.p.ohalloran@gmail.com>
2019-08-15 10:21:00 -0300

    stub out a posting to forestry flow

--- packages/cli/forestry/src/cmds/initServer/index.js
+++ packages/cli/forestry/src/cmds/initServer/index.js
@@ -4,24 +4,18 @@ import chalk from 'chalk'
 import { requestGitProvider } from './requestGitProvider'
 import { retrieveAuthToken } from './retrieveAuthToken'
 import { requestRepoBranch } from './requestRepoBranch'
+import { postToForestry } from './postToForestry'
 
 export async function initServer() {
   clear()
   console.log(
     chalk.green(figlet.textSync('Forestry', { horizontalLayout: 'full' }))
   )
+  console.log('\n')
 
   const repoBranch = await requestRepoBranch()
   const gitProvider = await requestGitProvider() //todo - we can probably pull this from the git remote
   const authToken = await retrieveAuthToken(gitProvider)
 
-  console.log(
-    'TODO - send this to our dev-server starter: ' +
-      JSON.stringify({
-        token: authToken,
-        repo: repoBranch.repo,
-        branch: repoBranch.branch,
-        secrets: 'TODO',
-      })
-  )
+  await postToForestry(authToken, repoBranch.repo, repoBranch.branch)
 }
