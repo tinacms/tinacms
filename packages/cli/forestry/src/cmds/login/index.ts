fa34c715bff5639d22f3a9479eb55954f0de8146
Nolan's Preview Server <nolan@forestry.io>
2019-08-19 15:16:00 -0300

    Check that we're posting login to server

--- packages/cli/forestry/src/cmds/login/index.ts
+++ packages/cli/forestry/src/cmds/login/index.ts
@@ -1,10 +1,16 @@
 import { writeConfig } from '../../config'
 import { promptCredentials } from './prompt-credentials'
+import axios from 'axios'
+import { any } from 'prop-types'
 
+const FORESTRY_API = ''
 export async function login() {
   const fieldValues = await promptCredentials()
-
-  writeConfig({ token: 'abc123' })
+  const { token } = await axios.post<any, { token: string }>(
+    `${FORESTRY_API}/login`,
+    fieldValues
+  )
+  writeConfig({ token })
 
   // TODO - Post to forestry and grab token
   // TODO - Store token in ~/.forestry credentials file
