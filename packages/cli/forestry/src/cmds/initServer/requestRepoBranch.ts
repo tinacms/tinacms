5e780a112fffd9e941390950b3f00971e9644ecb
jamespohalloran <james.p.ohalloran@gmail.com>
2019-08-20 09:39:14 -0300

    use origin instead of requeestion remote

--- packages/cli/forestry/src/cmds/initServer/requestRepoBranch.ts
+++ packages/cli/forestry/src/cmds/initServer/requestRepoBranch.ts
@@ -6,22 +6,25 @@ export async function requestRepoBranch() {
   const branch = (await git.branchLocal()).current
 
   const remotes = await git.getRemotes(true)
-  if (!remotes) {
+  const repo = (remotes || []).find(remote => remote.name == 'origin')
+  if (!repo) {
     console.error('There are no git remotes setup within this directory')
     process.exit(1)
   }
+  const repoUrl = repo!.refs.fetch
+  console.log(repoUrl)
 
-  const remoteConfirmation = await inquirer.prompt([
-    {
-      name: 'remote',
-      type: 'list',
-      message: `Choose the remote you're looking to setup`,
-      choices: remotes.map((r: { refs: any }) => `${r.refs.fetch} (${branch})`),
-    },
-  ])
+  // const remoteConfirmation = await inquirer.prompt([
+  //   {
+  //     name: 'remote',
+  //     type: 'list',
+  //     message: `Choose the remote you're looking to setup`,
+  //     choices: remotes.map((r: { refs: any }) => `${r.refs.fetch} (${branch})`),
+  //   },
+  // ])
 
   return {
-    repo: remoteConfirmation.remote,
+    repo: repoUrl,
     branch,
   }
 }
