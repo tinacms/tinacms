14f520a200ab67ac59540b02dd163ac5ba0324ae
Nichlas W. Andersen <nichlas@itsnwa.com>
2019-08-20 14:23:37 +0200

    added new editor toggle

--- packages/gatsby/gatsby-plugin-xeditor/gatsby-browser.tsx
+++ packages/gatsby/gatsby-plugin-xeditor/gatsby-browser.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { CMSContext } from '@forestryio/cms-react'
 import { SidebarProvider, useSidebar } from '@forestryio/xeditor'
 import { cms } from './index'
+import styled from 'styled-components'
 
 export const wrapRootElement = ({ element }: any, options: any) => {
   return (
@@ -18,10 +19,38 @@ function SidebarToggle() {
   let sidebar = useSidebar()
 
   return (
-    <div style={{ top: 0, right: 0, padding: '2rem', position: 'fixed' }}>
-      <button onClick={() => sidebar.setIsOpen(!sidebar.isOpen)}>
-        {sidebar.isOpen ? 'Close' : 'Open'}CMS
-      </button>
-    </div>
+    <EditorToggle
+      open={sidebar.isOpen}
+      onClick={() => sidebar.setIsOpen(!sidebar.isOpen)}
+    />
   )
 }
+
+const OpenIcon = require('./assets/pen.svg')
+const CloseIcon = require('./assets/times.svg')
+
+const EditorToggle = styled.button<{ open: Boolean }>`
+  position: fixed;
+  right: 2rem;
+  bottom: 2rem;
+  height: 3rem;
+  width: 3rem;
+  border: 0;
+  outline: none;
+  border-radius: 0.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  background-color: #0084ff;
+  background-image: url(${p => (p.open ? CloseIcon : OpenIcon)});
+  background-repeat: no-repeat;
+  background-position: center;
+  transition: background 0.35s ease;
+  &:hover {
+    background-color: #4ea9ff;
+  }
+  &:active {
+    background-color: #0073df;
+  }
+`
