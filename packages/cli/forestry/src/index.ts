df462ef423af67b872c8534319d175b9d8b4284c
jamespohalloran <james.p.ohalloran@gmail.com>
2019-08-20 14:56:38 -0300

    Group commands, stub out some new commands taking in the app option

--- packages/cli/forestry/src/index.ts
+++ packages/cli/forestry/src/index.ts
@@ -12,21 +12,53 @@ export function init(args: any) {
 
   program.version(version)
   program
-    .command('create-account')
+    .command('auth:signup')
     .description('create a forestry account')
     .action(() => {
       createAccount()
     })
 
   program
-    .command('login')
+    .command('auth:login')
     .description('log in to forestry account')
     .action(() => {
       login()
     })
 
   program
-    .command('init-server')
+    .command('config:init')
+    .description('initialize the local project with a forestry config')
+    .action(() => {
+      console.log('TODO - INITIALIZE LOCAL APP CONFIG HERE')
+      process.exit(1)
+    })
+
+  program
+    .command('apps')
+    .description('list the forestry apps that this user has access to')
+    .action(() => {
+      console.log('TODO - LIST APPS HERE')
+      process.exit(1)
+    })
+
+  program
+    .command('apps:info <app>')
+    .description('log info about the forestry app')
+    .action(app => {
+      console.log('TODO - LOG APP INFO HERE FOR: ' + app)
+      process.exit(1)
+    })
+
+  program
+    .command('apps:destroy <app>')
+    .description('destroy the specified forestry app')
+    .action(app => {
+      console.log(`TODO - DESTROY ${app} IN FORESTRY HERE`)
+      process.exit(1)
+    })
+
+  program
+    .command('apps:create')
     .description('Set up the cloud development server')
     .action(() => verifyAuthorized(initServer))
 
