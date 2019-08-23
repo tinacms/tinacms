9651adba737079515a083ce77385a5e9cc68ef41
jamespohalloran <james.p.ohalloran@gmail.com>
2019-08-23 15:48:05 -0300

    expose a modal which renders in an iframe

---
+++ packages/xeditor/xeditor/src/modalProvider.tsx
@@ -0,0 +1,105 @@
+import * as React from 'react'
+import { useCallback, useState } from 'react'
+import { createPortal } from 'react-dom'
+import { StyledFrame } from './styled-frame'
+import styled from 'styled-components'
+
+interface Props {
+  children: any
+}
+
+export const ModalProvider = ({ children }: Props) => {
+  const [modalRootContainerRef, setModalRootContainerRef] = useState(
+    null as Element | null
+  )
+
+  const setModalRef = useCallback(node => {
+    if (node !== null) {
+      setModalRootContainerRef(node)
+    }
+  }, [])
+
+  return (
+    <>
+      <div id="modal-root" ref={setModalRef} />
+      <ModalContainerContext.Provider
+        value={{ portalNode: modalRootContainerRef }}
+      >
+        {...children}
+      </ModalContainerContext.Provider>
+    </>
+  )
+}
+
+interface ModalContainerProps {
+  portalNode: Element | null
+}
+
+const ModalContainerContext = React.createContext<ModalContainerProps | null>(
+  null
+)
+
+export function useModalContainer(): ModalContainerProps {
+  let modalContainer = React.useContext(ModalContainerContext)
+
+  if (!modalContainer) {
+    throw new Error('No Modal Container context provided')
+  }
+
+  return modalContainer
+}
+
+export const Modal = ({
+  ...props
+}: React.DetailedHTMLProps<
+  React.HTMLAttributes<HTMLDivElement>,
+  HTMLDivElement
+>) => {
+  const { portalNode } = useModalContainer()
+
+  return portalNode && portalNode
+    ? (createPortal(
+        <StyledFrame
+          frameStyles={{
+            height: '100vh',
+            width: '100vw',
+            margin: '0 auto',
+            position: 'absolute',
+          }}
+        >
+          <ModalOverlay>
+            <ModalInner>
+              <div {...props} />
+            </ModalInner>
+          </ModalOverlay>
+        </StyledFrame>,
+        portalNode
+      ) as any)
+    : null
+}
+
+export const ModalOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background: rgba(0, 0, 0, 0.5);
+  z-index: 1000;
+  overflow: auto;
+  padding: 1rem;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+`
+
+export const ModalInner = styled.div`
+  display: block;
+  z-index: 1;
+  overflow: visible; /* Keep this as "visible", select component needs to overflow */
+  background-color: #fefefe;
+  border-radius: 0.3rem;
+  margin: auto;
+  width: 460px;
+  max-width: 90%;
+`
