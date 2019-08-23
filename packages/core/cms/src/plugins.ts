03160612011d52e17f90ace13f1bc95296ee3987
Update from Forestry.io <nolan@forestry.io>
2019-08-23 15:45:55 -0300

    cms: fields are plugins now

--- packages/core/cms/src/plugins.ts
+++ packages/core/cms/src/plugins.ts
@@ -1,29 +1,69 @@
 import { Subscribable } from './subscribable'
 
+export interface Plugin {
+  __type: string
+  name: string
+}
+
 export class PluginManager extends Subscribable {
-  private plugins: { [key: string]: { [key: string]: Plugin } } = {}
+  private plugins: Map<PluginType> = {}
 
   constructor() {
     super()
   }
+  findOrCreateMap<T extends Plugin = Plugin>(type: string): PluginType<T> {
+    return (this.plugins[type] =
+      this.plugins[type] || new PluginType(type)) as PluginType<T>
+  }
   add(view: Plugin) {
-    let plugins = this.plugins[view.type] || {}
-    plugins[view.name] = view
-    this.plugins[view.type] = plugins
+    this.findOrCreateMap(view.__type).add(view)
     this.notifiySubscribers()
   }
-  remove(view: Plugin) {
-    let plugins = this.plugins[view.type] || {}
-    delete plugins[view.name]
+  remove(type: string, view: Plugin) {
+    this.findOrCreateMap(view.__type).remove(view)
     this.notifiySubscribers()
   }
   all<T extends Plugin = Plugin>(type: string): T[] {
-    let plugins = this.plugins[type] || {}
-    return Object.keys(plugins).map(key => this.plugins[type][key] as T)
+    return this.findOrCreateMap<T>(type).all()
   }
 }
 
-export interface Plugin {
-  type: string
-  name: string
+interface Map<T> {
+  [key: string]: T
+}
+
+type PluginMap<T extends Plugin = Plugin> = Map<T>
+
+export class PluginType<T extends Plugin = Plugin> {
+  __plugins: PluginMap<T> = {}
+  constructor(private __type: string) {}
+
+  add(plugin: T | Omit<T, '__type'>) {
+    let p = plugin as T
+
+    if (!p.__type) {
+      p.__type = this.__type
+    }
+
+    this.__plugins[p.name] = p
+  }
+
+  all(): T[] {
+    return Object.keys(this.__plugins).map(name => this.__plugins[name])
+  }
+
+  find(name: string): T | undefined {
+    return this.__plugins[name]
+  }
+
+  remove(pluginOrName: string | T): T | undefined {
+    let name =
+      typeof pluginOrName === 'string' ? pluginOrName : pluginOrName.name
+
+    let plugin = this.__plugins[name]
+
+    delete this.__plugins[name]
+
+    return plugin
+  }
 }
