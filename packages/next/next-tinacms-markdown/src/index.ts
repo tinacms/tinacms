abd4a6ea7e162d7ec667bc73d3d1eb392919cdd9
Nolan Phillips <ncphillips.19@gmail.com>
2019-09-04 14:19:24 -0300

    next: useMarkdownForm writes to disk onSubmit

--- packages/next/next-tinacms-markdown/src/index.ts
+++ packages/next/next-tinacms-markdown/src/index.ts
@@ -1,5 +1,8 @@
 // @ts-ignore the types are wrong?
 import matter from 'gray-matter'
+import { FormOptions } from '@tinacms/core'
+import { useCMS, useCMSForm } from '@tinacms/react-tinacms'
+import * as yaml from 'js-yaml'
 
 interface MarkdownCollection {
   path: string
@@ -42,4 +45,47 @@ function slugify(filename: string) {
     .split('.')
     .slice(0, -1)
     .join('.')
+}
+
+interface Markdown {
+  path: string
+  frontmatter: string
+  content: string
+}
+
+export function useMarkdownForm(
+  markdownRemark: Markdown,
+  formOverrrides: FormOptions<any>
+) {
+  let cms = useCMS()
+
+  // let throttledOnChange = React.useMemo(() => {
+  // return throttle(cms.api.git.onChange, 300)
+  // }, [])
+  let [values, form] = useCMSForm({
+    name: markdownRemark.path,
+    initialValues: markdownRemark,
+    async onSubmit(data) {
+      console.log({ data })
+      await cms.api.git.onChange!({
+        fileRelativePath: data.path,
+        content: toMarkdownString(data),
+      })
+      // return await cms.api.git.onSubmit!({
+      //   files: [data.path],
+      //   message: data.__commit_message || 'xeditor commit',
+      //   name: data.__commit_name,
+      //   email: data.__commit_email,
+      // })
+    },
+    ...formOverrrides,
+  })
+
+  return [values, form]
+}
+
+function toMarkdownString(remark: Markdown) {
+  return (
+    '---\n' + yaml.dump(remark.frontmatter) + '---\n' + (remark.content || '')
+  )
 }
