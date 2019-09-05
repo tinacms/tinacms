746534d17d21c9791b0b19a969a425949f12067e
jamespohalloran <james.p.ohalloran@gmail.com>
2019-09-05 16:35:55 -0300

    ^

---
+++ packages/tinacms/fields/src/ImageDropzone/ImageDropzone.tsx
@@ -0,0 +1,53 @@
+import * as React from 'react'
+import { useDropzone } from 'react-dropzone'
+import styled from 'styled-components'
+
+const getColor = (props: any) => {
+  if (props.isDragAccept) {
+    return '#00e676'
+  }
+  if (props.isDragReject) {
+    return '#ff1744'
+  }
+  if (props.isDragActive) {
+    return '#2196f3'
+  }
+  return '#eeeeee'
+}
+
+const Container = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+  border-width: 2px;
+  border-radius: 2px;
+  border-color: ${props => getColor(props)};
+  border-style: dashed;
+  background-color: #fafafa;
+  color: #bdbdbd;
+  outline: none;
+  transition: border 0.24s ease-in-out;
+`
+
+export const ImageDropzone = (props: any) => {
+  const {
+    getRootProps,
+    getInputProps,
+    isDragActive,
+    isDragAccept,
+    isDragReject,
+  } = useDropzone({ accept: 'image/*' })
+
+  return (
+    <div className="container">
+      <Container
+        {...getRootProps({ isDragActive, isDragAccept, isDragReject })}
+      >
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      </Container>
+    </div>
+  )
+}
