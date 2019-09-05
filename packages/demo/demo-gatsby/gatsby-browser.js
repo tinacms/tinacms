0bd6da8bf4bfc666aff17097ddd39569e9374039
Nolan Phillips <ncphillips.19@gmail.com>
2019-09-05 15:23:11 -0300

    gatsby-demo: add post button is everywheres

--- packages/demo/demo-gatsby/gatsby-browser.js
+++ packages/demo/demo-gatsby/gatsby-browser.js
@@ -1,32 +1,3 @@
 // custom typefaces
 import "typeface-montserrat"
 import "typeface-merriweather"
-import { TextInput, TextAreaInput } from "@tinacms/tinacms"
-import * as React from "react"
-import { useCMS } from "@tinacms/react-tinacms"
-
-export const wrapPageElement = ({ element }) => {
-  return <FieldRegistrar>{element}</FieldRegistrar>
-}
-
-let firstRender = true
-const FieldRegistrar = ({ children }) => {
-  const cms = useCMS()
-
-  React.useEffect(() => {
-    if (firstRender) {
-      //override any fields here
-      // cms.forms.addFieldPlugin({
-      //   name: "text",
-      //   Component: TextInput,
-      // })
-      // cms.forms.addFieldPlugin({
-      //   name: "textarea",
-      //   Component: TextAreaInput,
-      // })
-    }
-    firstRender = false
-  }, [])
-
-  return children
-}
