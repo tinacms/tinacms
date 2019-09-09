53f5bec27d1a77aaf58475c66939d0003ea278c8
Nolan Phillips <ncphillips.19@gmail.com>
2019-09-09 17:16:25 -0300

    wysiwyg can update and has some keyboard commands

--- packages/tinacms/fields/src/Wysiwyg/commands/blockquote-commands.test.ts
+++ packages/tinacms/fields/src/Wysiwyg/commands/blockquote-commands.test.ts
@@ -1,21 +1,28 @@
-import { PMTestHarness } from "@forestryio/prosemirror-test-utils"
-import { defaultBlockSchema } from "forestry-wysiwyg/schema"
+import { PMTestHarness } from '../../prosemirror-test-utils'
+import { defaultBlockSchema } from '../schema'
+import { liftBlockquote } from './blockquote-commands'
 
-import { liftBlockquote } from "forestry-wysiwyg/commands/blockquote-commands"
+let {
+  forDoc,
+  doc,
+  p,
+  orderedList,
+  bulletList,
+  text,
+  blockquote,
+} = new PMTestHarness(defaultBlockSchema)
 
-let { forDoc, doc, p, orderedList, bulletList, text, blockquote } = new PMTestHarness(defaultBlockSchema)
-
-describe("liftBlockquote", () => {
-  it("should do nothing to paragraph", () => {
-    const content = p(text("one"))
+describe('liftBlockquote', () => {
+  it('should do nothing to paragraph', () => {
+    const content = p(text('one'))
     forDoc(doc(content))
       .withTextSelection(2, 4)
       .apply(liftBlockquote)
       .expect(doc(content))
   })
 
-  it("should do nothing to ordered list", () => {
-    const content = p(orderedList(text("one")))
+  it('should do nothing to ordered list', () => {
+    const content = p(orderedList(text('one')))
 
     forDoc(doc(content))
       .withTextSelection(2, 5)
@@ -23,8 +30,8 @@ describe("liftBlockquote", () => {
       .expect(doc(content))
   })
 
-  it("should do nothing to bullet list", () => {
-    const content = p(bulletList(text("one")))
+  it('should do nothing to bullet list', () => {
+    const content = p(bulletList(text('one')))
 
     forDoc(doc(content))
       .withTextSelection(2, 5)
@@ -32,12 +39,12 @@ describe("liftBlockquote", () => {
       .expect(doc(content))
   })
 
-  it("should remove blockquotes", () => {
-    const content = p(blockquote(text("one")))
+  it('should remove blockquotes', () => {
+    const content = p(blockquote(text('one')))
 
     forDoc(doc(content))
       .withTextSelection(2, 5)
       .apply(liftBlockquote)
-      .expect(doc(p(text("one"))))
+      .expect(doc(p(text('one'))))
   })
 })
--- packages/tinacms/fields/src/Wysiwyg/commands/link-commands.test.ts
+++ packages/tinacms/fields/src/Wysiwyg/commands/link-commands.test.ts
@@ -4,49 +4,39 @@ import {
   updateLinkBeingEdited,
   startEditingLink,
   insertLinkToFile,
-} from "./link-commands"
-import { PMTestHarness } from "@forestryio/prosemirror-test-utils"
-import { defaultBlockSchema } from "forestry-wysiwyg/schema"
+} from './link-commands'
+import { PMTestHarness } from '../../prosemirror-test-utils'
+import { defaultBlockSchema } from '../schema'
 
-let { forDoc, shouldNotRun, doc, p, link, text } = new PMTestHarness(defaultBlockSchema)
+let { forDoc, shouldNotRun, doc, p, link, text } = new PMTestHarness(
+  defaultBlockSchema
+)
 
 /**
  * stopEditingLink(state, dispatch)
  */
 
-describe("stopEditingLink", () => {
-  it("should not run on an empty doc", () => {
+describe('stopEditingLink', () => {
+  it('should not run on an empty doc', () => {
     shouldNotRun(stopEditingLink, doc(p()))
   })
 
   it("should not change a link that's not being edited", () => {
-    shouldNotRun(stopEditingLink, doc(p(link("test", { href: "/test" }))))
+    shouldNotRun(stopEditingLink, doc(p(link('test', { href: '/test' }))))
   })
 
   it("should set editing to 'editing' on a link", () => {
-    forDoc(doc(p(link("test", { href: "/test", editing: "editing" }))))
+    forDoc(doc(p(link('test', { href: '/test', editing: 'editing' }))))
       .apply(stopEditingLink)
-      .expect(doc(p(link("test", { href: "/test", editing: "" }))))
+      .expect(doc(p(link('test', { href: '/test', editing: '' }))))
   })
 
   it("should set editing to '' on all links", () => {
-    forDoc(doc(p(link("one", { href: "/one", editing: "editing" }), link("two", { href: "/two", editing: "editing" }))))
-      .apply(stopEditingLink)
-      .expect(doc(p(link("one", { href: "/one", editing: "" }), link("two", { href: "/two", editing: "" }))))
-  })
-
-  it("should set editing creating to '' on a link", () => {
-    forDoc(doc(p(link("test", { href: "/test", editing: "editing", creating: "creating" }))))
-      .apply(stopEditingLink)
-      .expect(doc(p(link("test", { href: "/test", editing: "", creating: "" }))))
-  })
-
-  it("should set editing and creating to '' on all links", () => {
     forDoc(
       doc(
         p(
-          link("one", { href: "/one", editing: "editing", creating: "creating" }),
-          link("two", { href: "/two", editing: "editing", creating: "" })
+          link('one', { href: '/one', editing: 'editing' }),
+          link('two', { href: '/two', editing: 'editing' })
         )
       )
     )
@@ -54,63 +44,105 @@ describe("stopEditingLink", () => {
       .expect(
         doc(
           p(
-            link("one", { href: "/one", editing: "", creating: "" }),
-            link("two", { href: "/two", editing: "", creating: "" })
+            link('one', { href: '/one', editing: '' }),
+            link('two', { href: '/two', editing: '' })
+          )
+        )
+      )
+  })
+
+  it("should set editing creating to '' on a link", () => {
+    forDoc(
+      doc(
+        p(
+          link('test', {
+            href: '/test',
+            editing: 'editing',
+            creating: 'creating',
+          })
+        )
+      )
+    )
+      .apply(stopEditingLink)
+      .expect(
+        doc(p(link('test', { href: '/test', editing: '', creating: '' })))
+      )
+  })
+
+  it("should set editing and creating to '' on all links", () => {
+    forDoc(
+      doc(
+        p(
+          link('one', {
+            href: '/one',
+            editing: 'editing',
+            creating: 'creating',
+          }),
+          link('two', { href: '/two', editing: 'editing', creating: '' })
+        )
+      )
+    )
+      .apply(stopEditingLink)
+      .expect(
+        doc(
+          p(
+            link('one', { href: '/one', editing: '', creating: '' }),
+            link('two', { href: '/two', editing: '', creating: '' })
           )
         )
       )
   })
 })
 
-describe("insertLinkToFile", () => {
-  it("should replace text with new link", () => {
-    forDoc(doc(p(text("one"))))
+describe('insertLinkToFile', () => {
+  it('should replace text with new link', () => {
+    forDoc(doc(p(text('one'))))
       .withTextSelection(0, 4)
-      .apply(insertLinkToFile, "http://www.google.ca")
+      .apply(insertLinkToFile, 'http://www.google.ca')
       .expect(
         doc(
           p(
-            link("www.google.ca", {
-              href: "http://www.google.ca",
-              editing: "",
-              creating: "",
-              title: "www.google.ca",
+            link('www.google.ca', {
+              href: 'http://www.google.ca',
+              editing: '',
+              creating: '',
+              title: 'www.google.ca',
             })
           )
         )
       )
   })
 
-  it("should use friendly display name", () => {
-    forDoc(doc(p(text("one"))))
+  it('should use friendly display name', () => {
+    forDoc(doc(p(text('one'))))
       .withTextSelection(0, 4)
-      .apply(insertLinkToFile, "http://www.google.ca/nice_name.jpg")
+      .apply(insertLinkToFile, 'http://www.google.ca/nice_name.jpg')
       .expect(
         doc(
           p(
-            link("nice_name.jpg", {
-              href: "http://www.google.ca/nice_name.jpg",
-              editing: "",
-              creating: "",
-              title: "nice_name.jpg",
+            link('nice_name.jpg', {
+              href: 'http://www.google.ca/nice_name.jpg',
+              editing: '',
+              creating: '',
+              title: 'nice_name.jpg',
             })
           )
         )
       )
   })
 
-  it("should remove spaces from title", () => {
-    forDoc(doc(p(text("one"))))
+  it('should remove spaces from title', () => {
+    forDoc(doc(p(text('one'))))
       .withTextSelection(0, 4)
-      .apply(insertLinkToFile, "http://www.google.ca/nice%20name.jpg")
+      .apply(insertLinkToFile, 'http://www.google.ca/nice%20name.jpg')
       .expect(
         doc(
           p(
-            link("nice name.jpg", {
-              href: "http://www.google.ca/nice%20name.jpg",
-              editing: "",
-              creating: "",
-              title: "nice name.jpg",
+            link('nice name.jpg', {
+              href: 'http://www.google.ca/nice%20name.jpg',
+              editing: '',
+              creating: '',
+              title: 'nice name.jpg',
             })
           )
         )
@@ -121,76 +153,98 @@ describe("insertLinkToFile", () => {
 /**
  * removeLinkBeingEdited(state, dispatch)
  */
-describe("removeLinkBeingEdited", () => {
-  it("should remove a link being edited", () => {
-    forDoc(doc(p(link("one", { href: "/one", editing: "editing" }))))
+describe('removeLinkBeingEdited', () => {
+  it('should remove a link being edited', () => {
+    forDoc(doc(p(link('one', { href: '/one', editing: 'editing' }))))
       .apply(removeLinkBeingEdited)
-      .expect(doc(p(text("one"))))
+      .expect(doc(p(text('one'))))
   })
 
   it("should not remove a link that's not being edited", () => {
-    forDoc(doc(p(link("one", { href: "/one" }))))
+    forDoc(doc(p(link('one', { href: '/one' }))))
       .apply(removeLinkBeingEdited)
-      .expect(doc(p(link("one", { href: "/one" }))))
+      .expect(doc(p(link('one', { href: '/one' }))))
   })
 
-  it("should remove a link being edited no matter where it is", () => {
-    forDoc(doc(p(link("one", { href: "/one" }), link("two", { href: "/two", editing: "editing" }))))
+  it('should remove a link being edited no matter where it is', () => {
+    forDoc(
+      doc(
+        p(
+          link('one', { href: '/one' }),
+          link('two', { href: '/two', editing: 'editing' })
+        )
+      )
+    )
       .apply(removeLinkBeingEdited)
-      .expect(doc(p(link("one", { href: "/one" }), text("two"))))
+      .expect(doc(p(link('one', { href: '/one' }), text('two'))))
   })
 })
 
 /**
  * updateLinkBeingEdited(state, dispatchi, attrs)
  */
-describe("updateLinkBeingEdited", () => {
-  it("should set href on link being edited", () => {
-    forDoc(doc(p(link("one", { href: "/one", editing: "editing" }))))
+describe('updateLinkBeingEdited', () => {
+  it('should set href on link being edited', () => {
+    forDoc(doc(p(link('one', { href: '/one', editing: 'editing' }))))
       .apply(updateLinkBeingEdited, {
-        href: "/one-edit",
-        editing: "editing",
-        creating: "",
+        href: '/one-edit',
+        editing: 'editing',
+        creating: '',
       })
-      .expect(doc(p(link("one", { href: "/one-edit", editing: "editing" }))))
+      .expect(doc(p(link('one', { href: '/one-edit', editing: 'editing' }))))
   })
 
-  it("should set title on link being edited", () => {
-    forDoc(doc(p(link("one", { href: "/one", editing: "editing" }))))
-      .apply(updateLinkBeingEdited, { href: "/one", editing: "editing", creating: "", title: "Link Title" })
-      .expect(doc(p(link("one", { href: "/one", editing: "editing", title: "Link Title" }))))
+  it('should set title on link being edited', () => {
+    forDoc(doc(p(link('one', { href: '/one', editing: 'editing' }))))
+      .apply(updateLinkBeingEdited, {
+        href: '/one',
+        editing: 'editing',
+        creating: '',
+        title: 'Link Title',
+      })
+      .expect(
+        doc(
+          p(
+            link('one', {
+              href: '/one',
+              editing: 'editing',
+              title: 'Link Title',
+            })
+          )
+        )
+      )
   })
 })
 
 /**
  * startEditingLink(state, dispatch)
  */
-describe("startEditingLink", () => {
-  it("should not run whene Selection is not on a link", () => {
+describe('startEditingLink', () => {
+  it('should not run whene Selection is not on a link', () => {
     forDoc(doc(p()))
       .withTextSelection(2)
       .shouldNotRun(startEditingLink)
   })
 
-  it("should not run if the Selection is not a cursor", () => {
-    forDoc(doc(p(link("one", { href: "/one" }))))
+  it('should not run if the Selection is not a cursor', () => {
+    forDoc(doc(p(link('one', { href: '/one' }))))
       .withTextSelection(3, 5)
       .shouldNotRun(startEditingLink)
   })
 
-  describe("if selection is a cursor", () => {
-    it("should run if you click at the start of a link", () => {
-      forDoc(doc(p(link("one", { href: "/one", editing: "", creating: "" }))))
+  describe('if selection is a cursor', () => {
+    it('should run if you click at the start of a link', () => {
+      forDoc(doc(p(link('one', { href: '/one', editing: '', creating: '' }))))
         .withTextSelection(2)
         .apply(startEditingLink)
-        .expect(doc(p(link("one", { href: "/one", editing: "editing" }))))
+        .expect(doc(p(link('one', { href: '/one', editing: 'editing' }))))
     })
 
-    it("should run if you click in the middle of the link", () => {
-      forDoc(doc(p(link("one", { href: "/one", editing: "", creating: "" }))))
+    it('should run if you click in the middle of the link', () => {
+      forDoc(doc(p(link('one', { href: '/one', editing: '', creating: '' }))))
         .withTextSelection(3)
         .apply(startEditingLink)
-        .expect(doc(p(link("one", { href: "/one", editing: "editing" }))))
+        .expect(doc(p(link('one', { href: '/one', editing: 'editing' }))))
     })
   })
 })
