3774f6133fdb4f0ab61ca2961b26355f940d3e62
Scott Byrne <scott.byrne@forestry.io>
2019-09-12 12:29:23 -0300

    refactor tina structure

--- packages/tinacms/tinacms/src/Tina.tsx
+++ packages/tinacms/tinacms/src/Tina.tsx
@@ -3,73 +3,30 @@ import { CMSContext } from '@tinacms/react-tinacms'
 import { ModalProvider } from './modalProvider'
 import { SidebarProvider, useSidebar } from './sidebarProvider'
 import { cms } from './index'
-import styled from 'styled-components'
-import { Close, Edit } from '@tinacms/icons'
-import { StyledFrame } from './styled-frame'
+import styled, { css } from 'styled-components'
+import { SIDEBAR_WIDTH } from './Globals'
 
 export const Tina: React.FC = ({ children, ...sidebar }) => {
   return (
     <CMSContext.Provider value={cms}>
       <ModalProvider>
-        <SidebarProvider {...sidebar}>
+        <SidebarProvider {...sidebar} />
+        <SiteWrapper open={false} position={'fixed'}>
           {children}
-          <SidebarToggle />
-        </SidebarProvider>
+        </SiteWrapper>
       </ModalProvider>
     </CMSContext.Provider>
   )
 }
 
-function SidebarToggle() {
-  let sidebar = useSidebar()
-
-  return (
-    <StyledFrame
-      frameStyles={{
-        position: 'fixed',
-        right: '32px',
-        bottom: '32px',
-        height: '48px',
-        width: '48px',
-        borderRadius: '8px',
-        boxShadow: '0px 2px 3px rgba(48, 48, 48, 0.15)',
-        zIndex: 999999,
-        margin: 0,
-        border: 0,
-      }}
-    >
-      <EditorToggle
-        open={sidebar.isOpen}
-        onClick={() => sidebar.setIsOpen(!sidebar.isOpen)}
-      />
-    </StyledFrame>
-  )
-}
-
-const EditorToggle = styled(props => {
-  return <button {...props}>{props.open ? <Close /> : <Edit />}</button>
-})`
+const SiteWrapper = styled.div<{ open: boolean; position: string }>`
   position: absolute;
   top: 0;
-  left: 0;
-  width: 100%;
+  right: 0;
   height: 100%;
-  border: 0;
-  outline: none;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  fill: white;
-  text-align: center;
-  background-color: #0084ff;
-  background-repeat: no-repeat;
-  background-position: center;
-  transition: background 0.35s ease;
-  cursor: pointer;
-  &:hover {
-    background-color: #4ea9ff;
-  }
-  &:active {
-    background-color: #0073df;
-  }
+  z-index: -1;
+  width: ${props =>
+    props.position === 'fixed' && props.open
+      ? 'calc(100% - ' + SIDEBAR_WIDTH + ')'
+      : '100%'};
 `
