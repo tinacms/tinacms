f217f2f0623ef2911dcdde0c9224747466ea4d66
Scott Byrne <scott.byrne@forestry.io>
2019-09-12 14:34:03 -0300

    create css reset for exposed elements

--- packages/tinacms/tinacms/src/Globals.tsx
+++ packages/tinacms/tinacms/src/Globals.tsx
@@ -63,3 +63,72 @@ export const GlobalStyles = createGlobalStyle`
     box-sizing: content-box;
   }
 `
+
+export const TinaReset = styled.div`
+  * {
+    font-family: 'Inter', sans-serif;
+    font-size: 100%;
+    -webkit-text-size-adjust: 100%;
+    box-sizing: border-box;
+    overflow: hidden;
+    font-style: normal;
+    letter-spacing: normal;
+    font-stretch: normal;
+    font-weight: normal;
+    text-align-last: initial;
+    text-indent: 0px;
+    text-shadow: none;
+    text-transform: none;
+    backface-visibility: visible;
+    background-color: transparent;
+    background-image: none;
+    bottom: auto;
+    box-shadow: none;
+    box-sizing: content-box;
+    caption-side: top;
+    clear: none;
+    clip: auto;
+    color: inherit;
+    cursor: auto;
+    direction: ltr;
+    display: inline;
+    float: none;
+    height: auto;
+    left: auto;
+    line-height: inherit;
+    max-height: none;
+    max-width: none;
+    min-height: 0px;
+    min-width: 0px;
+    opacity: 1;
+    outline-offset: 0px;
+    perspective: none;
+    perspective-origin: 50% 50%;
+    pointer-events: auto;
+    position: static;
+    quotes: none;
+    resize: none;
+    right: auto;
+    size: auto;
+    table-layout: auto;
+    top: auto;
+    transform: none;
+    transform-origin: 50% 50% 0px;
+    vertical-align: baseline;
+    white-space: normal;
+    word-break: normal;
+    word-spacing: normal;
+    overflow-wrap: normal;
+    text-align: start;
+    -webkit-font-smoothing: antialiased;
+    font-variant: normal;
+    text-decoration: none;
+    border-width: 0px;
+    border-style: none;
+    border-color: transparent;
+    border-image: initial;
+    border-radius: 0px;
+    margin: 0px;
+    padding: 0px;
+  }
+`
--- packages/tinacms/tinacms/src/Tina.tsx
+++ packages/tinacms/tinacms/src/Tina.tsx
@@ -7,13 +7,11 @@ import styled, { css, ThemeProvider } from 'styled-components'
 
 import { Theme, GlobalStyles } from './Globals'
 import { Sidebar } from './sidebar'
-import { SIDEBAR_WIDTH } from './Globals'
-
+import { TinaReset, SIDEBAR_WIDTH } from './Globals'
 
 interface TinaProps {
-  position:'fixed'|'float'
+  position: 'fixed' | 'float'
 }
-
 
 export const Tina: React.FC<TinaProps> = ({ children, position }) => {
   const [isOpen, setIsOpen] = React.useState(false)
@@ -25,32 +23,31 @@ export const Tina: React.FC<TinaProps> = ({ children, position }) => {
 
   return (
     <CMSContext.Provider value={cms}>
-     
       <SidebarContext.Provider value={props}>
-          <ThemeProvider theme={Theme}>
-            <ModalProvider>
-              <GlobalStyles />
+        <ThemeProvider theme={Theme}>
+          <ModalProvider>
+            <TinaReset>
               <Sidebar />
-            </ModalProvider>
-          </ThemeProvider>
-          <SiteWrapper open={isOpen} position={position}>
-            {children}
-          </SiteWrapper>
-        </SidebarContext.Provider>
-
+            </TinaReset>
+          </ModalProvider>
+        </ThemeProvider>
+        <SiteWrapper open={isOpen} position={position}>
+          {children}
+        </SiteWrapper>
+      </SidebarContext.Provider>
     </CMSContext.Provider>
   )
 }
 
 const SiteWrapper = styled.div<{ open: boolean; position: string }>`
-  position: absolute;
-  top: 0;
-  right: 0;
-  height: 100%;
-  z-index: -1;
+  position: absolute !important;
+  top: 0 !important;
+  right: 0 !important;
+  height: 100% !important;
+  z-index: -1 !important;
   width: ${props =>
     props.position === 'fixed' && props.open
       ? 'calc(100% - ' + SIDEBAR_WIDTH + ')'
-      : '100%'};
-  transition: all ${props => (props.open ? 150 : 200)}ms ease-out;
+      : '100%'} !important;
+  transition: all ${props => (props.open ? 150 : 200)}ms ease-out !important;
 `
