2885497644f4c9f76abf0a104c10a887f76bc4f0
Nolan Phillips <ncphillips.19@gmail.com>
2019-09-16 10:29:47 -0300

    added unstyled menu to wysiwyg

---
+++ packages/tinacms/fields/src/Wysiwyg/state/plugins/Menu/blockControl.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react'
+import { EditorView } from 'prosemirror-view'
+
+interface BlockTool {
+  children: any
+  className: string
+  command: Function
+  typeName?: string
+  attrs?: any
+}
+
+export function blockTool(options: BlockTool) {
+  let { className, children, command, typeName, attrs } = options
+  return class extends React.Component<
+    { view: EditorView; onClick(): void },
+    any
+  > {
+    canDo = () => command(this.props.view.state)
+    onClick = () => {
+      command(this.props.view.state, this.props.view.dispatch)
+      this.props.view.focus()
+      this.props.onClick()
+    }
+    get active(): boolean {
+      if (!typeName) return false
+
+      let { state } = this.props.view
+      let $from = state.selection.$from
+      let node = $from.node($from.depth)
+      let correctNodeType = node.type.name === typeName
+      // Only works for Heading
+      let correctAttrs = attrs ? node.attrs.level === attrs.level : true
+      return correctNodeType && correctAttrs
+    }
+    render() {
+      return (
+        <li
+          // className={c(
+          //   'formatting-item',
+          //   { disabled: !this.canDo(), active: this.active },
+          //   className
+          // )}
+          onClick={this.onClick}
+        >
+          {children}
+        </li>
+      )
+    }
+  }
+}
