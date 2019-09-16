99b18bcaea5443f897f2a26523a1746b89301fd1
Scott Byrne <scott.byrne@forestry.io>
2019-09-16 14:27:57 -0300

    fixed icon exports, formatting

--- packages/tinacms/icons/src/Heading.tsx
+++ packages/tinacms/icons/src/Heading.tsx
@@ -1,7 +1,13 @@
 import * as React from 'react'
 
 export const HeadingIcon = () => (
-    <svg width="32" height="32" viewBox="0 0 32 32" fill="inherit" xmlns="http://www.w3.org/2000/svg">
-    <path d="M12 5.33334V9.33334H18.6667V25.3333H22.6667V9.33334H29.3333V5.33334H12ZM4 16H8V25.3333H12V16H16V12H4V16Z"/>
-    </svg>    
+  <svg
+    width="32"
+    height="32"
+    viewBox="0 0 32 32"
+    fill="inherit"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M12 5.33334V9.33334H18.6667V25.3333H22.6667V9.33334H29.3333V5.33334H12ZM4 16H8V25.3333H12V16H16V12H4V16Z" />
+  </svg>
 )
--- packages/tinacms/icons/src/Italic.tsx
+++ packages/tinacms/icons/src/Italic.tsx
@@ -1,7 +1,13 @@
 import * as React from 'react'
 
 export const ItalicIcon = () => (
-    <svg width="32" height="32" viewBox="0 0 32 32" fill="inherit" xmlns="http://www.w3.org/2000/svg">
+  <svg
+    width="32"
+    height="32"
+    viewBox="0 0 32 32"
+    fill="inherit"
+    xmlns="http://www.w3.org/2000/svg"
+  >
     <path d="M13.3333 5.33334V9.33334H16.28L11.72 20H8V24H18.6667V20H15.72L20.28 9.33334H24V5.33334H13.3333Z" />
-    </svg>    
+  </svg>
 )
