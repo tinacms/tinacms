f38d2cf0fa30b0d3d37b20ef73a43f8211d54392
Scott Byrne <scott.byrne@forestry.io>
2019-09-16 14:27:57 -0300

    fixed icon exports, formatting

--- packages/tinacms/icons/src/Code.tsx
+++ packages/tinacms/icons/src/Code.tsx
@@ -1,7 +1,13 @@
 import * as React from 'react'
 
 export const CodeIcon = () => (
-    <svg width="32" height="32" viewBox="0 0 32 32" fill="inherit" xmlns="http://www.w3.org/2000/svg">
-    <path d="M12.5333 22.1333L6.40001 16L12.5333 9.86667L10.6667 8L2.66667 16L10.6667 24L12.5333 22.1333ZM19.4667 22.1333L25.6 16L19.4667 9.86667L21.3333 8L29.3333 16L21.3333 24L19.4667 22.1333V22.1333Z"/>
-    </svg>    
+  <svg
+    width="32"
+    height="32"
+    viewBox="0 0 32 32"
+    fill="inherit"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M12.5333 22.1333L6.40001 16L12.5333 9.86667L10.6667 8L2.66667 16L10.6667 24L12.5333 22.1333ZM19.4667 22.1333L25.6 16L19.4667 9.86667L21.3333 8L29.3333 16L21.3333 24L19.4667 22.1333V22.1333Z" />
+  </svg>
 )
--- packages/tinacms/icons/src/Quote.tsx
+++ packages/tinacms/icons/src/Quote.tsx
@@ -1,7 +1,13 @@
 import * as React from 'react'
 
 export const QuoteIcon = () => (
-    <svg width="32" height="32" viewBox="0 0 32 32" fill="inherit" xmlns="http://www.w3.org/2000/svg">
+  <svg
+    width="32"
+    height="32"
+    viewBox="0 0 32 32"
+    fill="inherit"
+    xmlns="http://www.w3.org/2000/svg"
+  >
     <path d="M8.00001 22.6667H12L14.6667 17.3333V9.33334H6.66667V17.3333H10.6667L8.00001 22.6667ZM18.6667 22.6667H22.6667L25.3333 17.3333V9.33334H17.3333V17.3333H21.3333L18.6667 22.6667Z" />
-    </svg>    
+  </svg>
 )
