9f7ada1966688f74d6e3cff1a7f81e4a4469b816
Kendall Strautman <kendallstrautman@gmail.com>
2019-09-17 11:25:51 -0700

    removes unnecessary justify-content on sidebar

--- packages/tinacms/tinacms/src/sidebar.tsx
+++ packages/tinacms/tinacms/src/sidebar.tsx
@@ -291,7 +291,7 @@ const SidebarWrapper = styled.div<{ open: boolean }>`
   display: flex;
   flex-direction: column;
   flex-wrap: nowrap;
-  justify-content: space-between;
+  /* justify-content: space-between; */
   width: ${SIDEBAR_WIDTH};
   height: 100%;
   left: 0;
