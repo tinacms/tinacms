23f5c87f4787284a93094299830166cb9ec6d1bf
Kendall Strautman <kendallstrautman@gmail.com>
2019-09-17 11:03:01 -0700

    moves FormsList into its own component

--- packages/tinacms/tinacms/src/components/FormView.tsx
+++ packages/tinacms/tinacms/src/components/FormView.tsx
@@ -10,6 +10,7 @@ import { Modal, ModalBody, ModalHeader } from '..'
 import { Theme, GlobalStyles, HEADER_HEIGHT, FOOTER_HEIGHT } from '../Globals'
 import { Button } from './Button'
 import { ActionsMenu } from './ActionsMenu'
+import FormsList from './FormsList'
 
 export const FormsView = () => {
   const cms = useCMS()
@@ -120,35 +121,6 @@ const CreateContentButton = ({ plugin }: any) => {
   )
 }
 
-interface FormsListProps {
-  forms: Form[]
-  activeForm: Form | null
-  setActiveForm(form: Form): void
-}
-const FormsList = ({ forms, activeForm, setActiveForm }: FormsListProps) => {
-  console.log(forms)
-  return (
-    <StyledFormList>
-      <h1>Editable Files</h1>
-      <ul>
-        {forms.map(form => (
-          <li
-            key={form.name}
-            style={{
-              color:
-                activeForm && activeForm.name === form.name
-                  ? Theme.color.primary
-                  : 'inherit',
-            }}
-            onClick={() => setActiveForm(form)}
-          >
-            {form.name}
-          </li>
-        ))}
-      </ul>
-    </StyledFormList>
-  )
-}
 
 export const MediaView: ScreenPlugin = {
   __type: 'screen',
@@ -176,40 +148,6 @@ const NoFieldsPlaceholder = () => (
 
 const CreateButton = styled(Button)`
   width: 100%;
-`
-
-const StyledFormList = styled.div`
-  padding: ${Theme.padding}rem;
-  background-color: ${Theme.color.light};
-  border-bottom: 1px solid rgba(51,51,51,0.04);
-  h1 {
-    font-size: 1.2rem;
-    color: #333;
-    font-weight: normal;
-    padding: 0 0 ${Theme.input.padding} 0;
-    margin: 0 0 .85rem 0;
-    border-bottom: 0.5px solid lightgray;
-  }
-  ul {
-    cursor: pointer;
-    list-style: none;
-    margin: 0;
-    padding: 0;
-    color: #747474;
-    font-size: .8rem;
-    li:not(:last-child) {
-      margin-bottom: ${Theme.input.padding};
-      padding-bottom: .5rem;
-    }
-    li {
-      -webkit-transition: color 300ms ease;
-      transition: color 300ms ease;
-    }
-    li:active {
-      -webkit-transition: color 300ms ease;
-      transition: color 300ms ease;
-    }
-  }
 `
 
 export const FieldsWrapper = styled.div`
---
+++ packages/tinacms/tinacms/src/components/FormsList.tsx
@@ -0,0 +1,68 @@
+import * as React from 'react'
+import styled from 'styled-components'
+import { Form } from '@tinacms/core'
+
+interface FormsListProps {
+    forms: Form[]
+    activeForm: Form | null
+    setActiveForm(form: Form): void
+  }
+  const FormsList = ({ forms, activeForm, setActiveForm }: FormsListProps) => {
+    return (
+      <StyledFormList>
+        <h1>Editable Files</h1>
+        <ul>
+          {forms.map(form => (
+            <li
+              key={form.name}
+              style={{
+                color:
+                  activeForm && activeForm.name === form.name
+                    ? 'blue'
+                    : 'inherit',
+              }}
+              onClick={() => setActiveForm(form)}
+            >
+              {form.name}
+            </li>
+          ))}
+        </ul>
+      </StyledFormList>
+    )
+  }
+
+  export default FormsList;
+
+  const StyledFormList = styled.div`
+  padding: ${p => p.theme.padding}rem;
+  background-color: ${p => p.theme.color.light};
+  border-bottom: 1px solid rgba(51,51,51,0.04);
+  h1 {
+    font-size: 1.2rem;
+    color: #333;
+    font-weight: normal;
+    padding: 0 0 ${p => p.theme.input.padding} 0;
+    margin: 0 0 .85rem 0;
+    border-bottom: 0.5px solid lightgray;
+  }
+  ul {
+    cursor: pointer;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    color: #747474;
+    font-size: .8rem;
+    li:not(:last-child) {
+      margin-bottom: ${p => p.theme.input.padding};
+      padding-bottom: .5rem;
+    }
+    li {
+      -webkit-transition: color 300ms ease;
+      transition: color 300ms ease;
+    }
+    li:active {
+      -webkit-transition: color 300ms ease;
+      transition: color 300ms ease;
+    }
+  }
+`
