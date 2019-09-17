b7822d140320413ff19fe26736e81c735a1ecce8
Nolan Phillips <ncphillips.19@gmail.com>
2019-09-17 18:28:35 -0300

    react-dismissible props are documented readme is in package

--- packages/react/react-dismissible/src/Dismissible.tsx
+++ packages/react/react-dismissible/src/Dismissible.tsx
@@ -1,11 +1,32 @@
 import * as React from 'react'
 
 export interface Props {
+  /**
+   * The function that called in response to a dismissal.
+   */
   onDismiss: Function
+
+  /**
+   * If `true`, pressing `ESC` key will trigger a dismissal. Default: `false`
+   */
   escape?: boolean
+
+  /**
+   * When `true` clicking outside of the surrounding area triggers a dismissal. Default: `false`
+   */
   click?: boolean
+
+  /**
+   * When `true` there will be no dismissals. Default: `false`
+   */
   disabled?: boolean
-  document?: any
+
+  /**
+   * An extra Document to add the event listeners too.
+   *
+   * Used when the dismissible area is inside of an iframe.
+   */
+  document?: Document
 }
 
 export class Dismissible extends React.Component<Props, {}> {
