ad9a53f4ec1d21be137caa7711d6af326f795f62
Scott Byrne <scott.byrne@forestry.io>
2019-09-19 09:38:21 -0300

    fixed header option toggle

--- packages/tinacms/fields/src/Wysiwyg/state/plugins/Menu/blockControl.tsx
+++ packages/tinacms/fields/src/Wysiwyg/state/plugins/Menu/blockControl.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { EditorView } from 'prosemirror-view'
+import styled from 'styled-components'
 
 interface BlockTool {
   Component: any
@@ -34,17 +35,35 @@ export function blockTool(options: BlockTool) {
     }
     render() {
       return (
-        <li
-          // className={c(
-          //   'formatting-item',
-          //   { disabled: !this.canDo(), active: this.active },
-          //   className
-          // )}
+        <MenuOption
           onClick={this.onClick}
+          disabled={!this.canDo()}
+          active={this.active}
         >
           <Component>{children}</Component>
-        </li>
+        </MenuOption>
       )
     }
   }
 }
+
+export const MenuOption = styled.div<{ disabled: boolean; active: boolean }>`
+  display: block;
+  padding: 0.5rem 1rem;
+  transition: all 85ms ease-out;
+  cursor: pointer;
+  &:first-child {
+    padding-top: 0.75rem;
+  }
+  &:last-child {
+    padding-bottom: 0.75rem;
+  }
+  &:hover {
+    background-color: rgba(53, 50, 50, 0.09);
+  }
+  &:active {
+    color: #0084ff;
+    fill: #0084ff;
+    background-color: rgba(53, 50, 50, 0.05);
+  }
+`
