e889b8d1e8404b8cb7e8b7f232a1b3836a216a35
Scott Byrne <scott.byrne@forestry.io>
2019-09-26 17:07:14 -0300

    style: group ui

--- packages/tinacms/tinacms/src/plugins/fields/GroupFieldPlugin.tsx
+++ packages/tinacms/tinacms/src/plugins/fields/GroupFieldPlugin.tsx
@@ -3,6 +3,7 @@ import { Field, Form } from '@tinacms/core'
 import styled from 'styled-components'
 import { FieldsBuilder } from '@tinacms/form-builder'
 import { padding } from '@tinacms/styles'
+import { RightArrowIcon, CloseIcon } from '@tinacms/icons'
 
 export interface GroupFieldDefinititon extends Field {
   component: 'group'
@@ -17,102 +18,43 @@ export interface GroupProps {
   tinaForm: Form
 }
 
-export const Group = styled(function Group({
+export const Group = function Group({
   tinaForm,
   form,
   field,
   input,
   meta,
-  ...styleProps
 }: GroupProps) {
   let [isExpanded, setExpanded] = React.useState<boolean>(false)
   return (
-    <div {...styleProps}>
+    <>
       <Header onClick={() => setExpanded(p => !p)}>
-        <label>{field.label || field.name}</label>
+        {Label(field)}
+        <RightArrowIcon />
       </Header>
-
       <Panel
         isExpanded={isExpanded}
         setExpanded={setExpanded}
         field={field}
         tinaForm={tinaForm}
       />
-    </div>
+    </>
   )
-})`
-  border: 1px solid pink;
-  border-radius: 0.3rem;
-  margin: 1.5rem 0;
-  transition: background 0.15s ease;
-  overflow: visible;
-`
-
-const Header = styled.div`
-  position: relative;
-  cursor: pointer;
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-
-  > label {
-    flex: 1;
-    margin: 0;
-    cursor: pointer;
-    text-transform: none;
-    font-size: 0.85rem;
-    font-weight: 500;
-    color: #282828;
-    padding: 1.5rem;
-    overflow-x: hidden;
-
-    > div {
-      white-space: nowrap;
-      text-overflow: ellipsis;
-      overflow-x: hidden;
-    }
-
-    &.multiLine {
-      padding: calc(1.5rem - 10px) 0 calc(1.5rem - 9px) 1.5rem;
-    }
-
-    .move-icon + label {
-      padding: 1.5rem 0;
-    }
-  }
-
-  .move-icon {
-    cursor: move;
-    line-height: 0;
-    padding: 1.5rem;
-  }
-
-  .delete {
-    margin-left: auto;
-    opacity: 0.6;
-    line-height: 0;
-    padding: 1.5rem;
-
-    &:hover {
-      opacity: 1;
-      color: $error;
-    }
-  }
-`
+}
 
 interface PanelProps {
   setExpanded(next: boolean): void
   isExpanded: boolean
   tinaForm: Form
   field: GroupFieldDefinititon
+  children?: any
 }
-
-const Panel = styled(function Panel({
+const Panel = function Panel({
   setExpanded,
   isExpanded,
   tinaForm,
   field,
-  ...styleProps
+  children,
 }: PanelProps) {
   let fields: any[] = React.useMemo(() => {
     return field.fields.map((subField: any) => ({
@@ -122,22 +64,131 @@ const Panel = styled(function Panel({
   }, [field.fields])
 
   return (
-    <div {...styleProps}>
-      <button onClick={() => setExpanded(false)}>Back</button>
-      {isExpanded ? <FieldsBuilder form={tinaForm} fields={fields} /> : null}
-    </div>
+    <GroupPanel isExpanded={isExpanded}>
+      <PanelHeader onClick={() => setExpanded(false)}>
+        {Label(field)} <CloseIcon />
+      </PanelHeader>
+      <PanelBody>
+        {isExpanded ? <FieldsBuilder form={tinaForm} fields={fields} /> : null}
+      </PanelBody>
+    </GroupPanel>
   )
-})`
+}
+
+const Label = function(field: GroupFieldDefinititon) {
+  return <GroupLabel>{field.label || field.name}</GroupLabel>
+}
+
+const Header = styled.div`
+  position: relative;
+  cursor: pointer;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border: 1px solid #e1e1e1;
+  border-radius: 0.25rem;
+  margin: 0 0 1.5rem 0;
+  overflow: visible;
+  line-height: 1.35;
+  padding: 0.75rem;
+
+  svg {
+    width: 16px;
+    height: auto;
+    fill: #b4b4b4;
+  }
+
+  &:hover {
+    &:before {
+      opacity: 1;
+    }
+  }
+  &:before {
+    content: '';
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+    width: 100%;
+    height: 100%;
+    border: none;
+    background-color: #f2f2f2;
+    opacity: 0;
+    transition: all 85ms ease-out;
+    border-radius: 0.25rem;
+  }
+`
+
+const PanelHeader = styled.div`
+  position: relative;
+  flex: 0 0 auto;
+  cursor: pointer;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid #efefef;
+  margin: 0;
+  line-height: 1.35;
+  padding: 0.75rem 1.25rem;
+
+  svg {
+    width: 16px;
+    height: auto;
+    fill: #b4b4b4;
+  }
+
+  &:hover {
+    &:before {
+      opacity: 1;
+    }
+  }
+  &:before {
+    content: '';
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+    width: 100%;
+    height: 100%;
+    border: none;
+    background-color: #f2f2f2;
+    opacity: 0;
+    transition: all 85ms ease-out;
+    border-radius: 0.25rem;
+  }
+`
+
+const PanelBody = styled.div`
+  position: relative;
+  display: block;
+  flex: 1 1 auto;
+  overflow-y: auto;
+`
+
+const GroupLabel = styled.label`
+  margin: 0;
+  font-size: 0.85rem;
+  font-weight: 500;
+  color: #282828;
+`
+
+const GroupPanel = styled.div<{ isExpanded: boolean }>`
   position: fixed;
   width: 100%;
   height: 100%;
-  top: 4rem;
-  left: ${p => (p.isExpanded ? '0' : 'calc(100% - 0px)')};
-  overflow-y: auto;
+  top: 0;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  transform: translate3d(${p => (p.isExpanded ? '0' : '100%')}, 0, 0);
+  overflow-y: hidden;
+  overflow-x: hidden;
   background: white;
-  padding: ${padding()}rem;
-  z-index: 500;
-  transition: left 0.35s ease, max-height 0.5s ease;
+  z-index: 50;
+  transition: transform 250ms ease-out;
 `
 
 export interface GroupFieldProps {
