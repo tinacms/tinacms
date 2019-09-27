696a27b90f75839ff32bc35ee78656dbbb7b312c
Scott Byrne <scott.byrne@forestry.io>
2019-09-27 09:48:42 -0300

    style: improve group style

--- packages/tinacms/tinacms/src/plugins/fields/GroupFieldPlugin.tsx
+++ packages/tinacms/tinacms/src/plugins/fields/GroupFieldPlugin.tsx
@@ -3,7 +3,7 @@ import { Field, Form } from '@tinacms/core'
 import styled from 'styled-components'
 import { FieldsBuilder } from '@tinacms/form-builder'
 import { padding } from '@tinacms/styles'
-import { RightArrowIcon, CloseIcon } from '@tinacms/icons'
+import { EditIcon, ChevronUpIcon, LeftArrowIcon } from '@tinacms/icons'
 
 export interface GroupFieldDefinititon extends Field {
   component: 'group'
@@ -30,7 +30,7 @@ export const Group = function Group({
     <>
       <Header onClick={() => setExpanded(p => !p)}>
         {Label(field)}
-        <RightArrowIcon />
+        <EditIcon />
       </Header>
       <Panel
         isExpanded={isExpanded}
@@ -66,7 +66,7 @@ const Panel = function Panel({
   return (
     <GroupPanel isExpanded={isExpanded}>
       <PanelHeader onClick={() => setExpanded(false)}>
-        {Label(field)} <CloseIcon />
+        {Label(field)} <ChevronUpIcon />
       </PanelHeader>
       <PanelBody>
         {isExpanded ? <FieldsBuilder form={tinaForm} fields={fields} /> : null}
@@ -91,73 +91,25 @@ const Header = styled.div`
   overflow: visible;
   line-height: 1.35;
   padding: 0.75rem;
+  color: #282828;
 
   svg {
-    width: 16px;
+    width: 20px;
     height: auto;
     fill: #b4b4b4;
   }
 
   &:hover {
-    &:before {
-      opacity: 1;
-    }
-  }
-  &:before {
-    content: '';
-    display: block;
-    position: absolute;
-    top: 0;
-    left: 0;
-    z-index: -1;
-    width: 100%;
-    height: 100%;
-    border: none;
-    background-color: #f2f2f2;
-    opacity: 0;
-    transition: all 85ms ease-out;
-    border-radius: 0.25rem;
+    color: #0084ff;
   }
 `
 
-const PanelHeader = styled.div`
-  position: relative;
+const PanelHeader = styled(Header)`
   flex: 0 0 auto;
-  cursor: pointer;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+  border: none;
   border-bottom: 1px solid #efefef;
   margin: 0;
-  line-height: 1.35;
   padding: 0.75rem 1.25rem;
-
-  svg {
-    width: 16px;
-    height: auto;
-    fill: #b4b4b4;
-  }
-
-  &:hover {
-    &:before {
-      opacity: 1;
-    }
-  }
-  &:before {
-    content: '';
-    display: block;
-    position: absolute;
-    top: 0;
-    left: 0;
-    z-index: -1;
-    width: 100%;
-    height: 100%;
-    border: none;
-    background-color: #f2f2f2;
-    opacity: 0;
-    transition: all 85ms ease-out;
-    border-radius: 0.25rem;
-  }
 `
 
 const PanelBody = styled.div`
@@ -171,7 +123,8 @@ const GroupLabel = styled.label`
   margin: 0;
   font-size: 0.85rem;
   font-weight: 500;
-  color: #282828;
+  color: inherit;
+  transition: all 85ms ease-out;
 `
 
 const GroupPanel = styled.div<{ isExpanded: boolean }>`
