b26d85fa6d307c7f11377ebcaffbee1536e0bb59
Scott Byrne <scott.byrne@forestry.io>
2019-09-27 14:08:23 -0300

    feat: add trash icon

---
+++ packages/tinacms/icons/src/Trash.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+export const TrashIcon = () => (
+  <svg
+    width="32"
+    height="32"
+    viewBox="0 0 32 32"
+    fill="inherit"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M10 23C10 24.1 10.9 25 12 25H20C21.1 25 22 24.1 22 23V11H10V23ZM23 8H19.5L18.5 7H13.5L12.5 8H9V10H23V8Z" />
+  </svg>
+)
