c8467d806047d33ff994d85a54d7d2d83edcc42d
Nolan Phillips <ncphillips.19@gmail.com>
2019-10-01 10:00:55 -0300

    tests: clarified test names and added more cases

--- packages/react/react-tinacms/src/use-form.findInactiveFields.test.ts
+++ packages/react/react-tinacms/src/use-form.findInactiveFields.test.ts
@@ -11,28 +11,80 @@ function makeForm(initialValues: any, fields: Field[]) {
   })
 }
 describe('findInactiveFields', () => {
-  it('name', () => {
+  describe('a form with a "name" text field', () => {
     let form = makeForm({ name: 'test' }, [{ name: 'name', component: 'text' }])
 
-    expect(findInactiveFieldsInPath(form, 'name')).toEqual(['name'])
+    describe('given the "name" path', () => {
+      it('returns only that field', () => {
+        expect(findInactiveFieldsInPath(form, 'name')).toEqual(['name'])
+      })
+    })
   })
 
-  it('authors.INDEX.name | 2 authors', () => {
-    let data = { authors: [{ name: 'bob' }, { name: 'doug' }] }
-
-    let form = makeForm(data, [
+  describe('given the path to some authors name', () => {
+    let path = 'authors.INDEX.name'
+    let fields = [
       {
         name: 'authors',
         component: 'group-list',
         fields: [{ name: 'name', component: null }],
       },
-    ])
+    ]
 
-    let inactiveFields = ['authors.0.name', 'authors.1.name']
+    describe('a form with no authors', () => {
+      it('returns an empty array', () => {
+        let data = { authors: [] }
 
-    expect(findInactiveFieldsInPath(form, 'authors.INDEX.name')).toEqual(
-      inactiveFields
-    )
+        let form = makeForm(data, fields)
+
+        expect(findInactiveFieldsInPath(form, path)).toEqual([])
+      })
+    })
+    describe('a form with one author', () => {
+      it('returns the path to that authors name', () => {
+        let data = { authors: [{ name: 'bill' }] }
+
+        let form = makeForm(data, fields)
+
+        expect(findInactiveFieldsInPath(form, path)).toEqual(['authors.0.name'])
+      })
+    })
+    describe('a form with two authors', () => {
+      it('returns the path to both authors name', () => {
+        let data = { authors: [{ name: 'bob' }, { name: 'doug' }] }
+
+        let form = makeForm(data, fields)
+
+        let inactiveFields = ['authors.0.name', 'authors.1.name']
+
+        expect(findInactiveFieldsInPath(form, path)).toEqual(inactiveFields)
+      })
+    })
+    describe('a form with 5 authors', () => {
+      it('returns the path to all authors name', () => {
+        let data = {
+          authors: [
+            { name: 'bob' },
+            { name: 'rob' },
+            { name: 'tod' },
+            { name: 'rod' },
+            { name: 'doug' },
+          ],
+        }
+
+        let form = makeForm(data, fields)
+
+        let inactiveFields = [
+          'authors.0.name',
+          'authors.1.name',
+          'authors.2.name',
+          'authors.3.name',
+          'authors.4.name',
+        ]
+
+        expect(findInactiveFieldsInPath(form, path)).toEqual(inactiveFields)
+      })
+    })
   })
 
   it('authors.INDEX.books.INDEX.title | 1 author 2 books', () => {
