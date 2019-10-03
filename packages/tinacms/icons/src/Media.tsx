6235d3b2fce102927f50766135f948ab366bfa92
Scott Byrne <scott.byrne@forestry.io>
2019-10-03 10:57:33 -0300

    style: update icons

---
+++ packages/tinacms/icons/src/Media.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+export const MediaIcon = () => (
+  <svg
+    width="32"
+    height="32"
+    viewBox="0 0 32 32"
+    fill="inherit"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M26 20V8C26 6.9 25.1 6 24 6H12C10.9 6 10 6.9 10 8V20C10 21.1 10.9 22 12 22H24C25.1 22 26 21.1 26 20ZM15 16L17.03 18.71L20 15L24 20H12L15 16ZM6 10V24C6 25.1 6.9 26 8 26H22V24H8V10H6Z" />
+  </svg>
+)
