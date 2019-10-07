cb6b13e55f0d3e59198d249beb9e11216692e11d
Nolan Phillips <ncphillips.19@gmail.com>
2019-10-07 10:23:19 -0300

    chore: removed log

--- packages/tinacms/form-builder/src/final-form-builder.tsx
+++ packages/tinacms/form-builder/src/final-form-builder.tsx
@@ -30,7 +30,6 @@ export const FormBuilder: FC<FormBuilderProps> = ({ form, children }) => {
    *
    * See: https://github.com/final-form/react-final-form/blob/master/src/ReactFinalForm.js#L68-L72
    */
-  if (DEBUG) console.log('YOMOMMA')
   return (
     <FF form={form.finalForm} key={form.id}>
       {children}
