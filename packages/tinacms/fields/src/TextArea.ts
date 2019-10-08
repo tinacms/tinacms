da52afaa0a5f0f39f1fbeed769c6e6185f82e00b
Scott Byrne <scott.byrne@forestry.io>
2019-10-08 13:32:52 -0300

    style: move input styles to input component

--- packages/tinacms/fields/src/TextArea.ts
+++ packages/tinacms/fields/src/TextArea.ts
@@ -1,6 +1,6 @@
 import * as React from 'react'
-import { color, padding, font } from '@tinacms/styles'
 import styled, { css } from 'styled-components'
+import { InputCss } from '.'
 
 type a = React.DetailedHTMLProps<
   React.InputHTMLAttributes<HTMLInputElement>,
@@ -12,31 +12,7 @@ export interface TextAreaProps extends a {
 }
 
 export const TextArea = styled.textarea<{ error?: boolean }>`
-  position: relative;
-  background-color: white;
-  border-radius: 0.3rem;
-  font-size: ${font.size(2)};
-  line-height: 1.32;
-  transition: all 85ms ease-out;
-  padding: ${padding('small')};
-  border: 1px solid #edecf3;
-  width: 100%;
-  margin: 0;
-  outline: none;
+  ${InputCss};
   resize: vertical;
   height: 10rem;
-  box-shadow: 0 0 0 2px ${p => (p.error ? color.error() : 'transparent')};
-
-  &:hover {
-    box-shadow: 0 0 0 2px #e1ddec;
-  }
-
-  &:focus {
-    box-shadow: 0 0 0 2px ${p => (p.error ? color.error() : color.primary())};
-  }
-
-  &::placeholder {
-    font-size: 0.9rem;
-    color: #cfd3d7;
-  }
 `
--- packages/tinacms/fields/src/TextField.ts
+++ packages/tinacms/fields/src/TextField.ts
@@ -1,44 +1,20 @@
 import * as React from 'react'
-import { color, padding, font } from '@tinacms/styles'
 import styled, { css } from 'styled-components'
+import { InputCss, InputProps } from '.'
 
 type a = React.DetailedHTMLProps<
   React.InputHTMLAttributes<HTMLInputElement>,
   HTMLInputElement
 >
-export interface InputProps extends a {
+export interface TextFieldProps extends a {
   error?: boolean
   ref?: any
 }
 
-export const TextFieldCss = css<{ error?: boolean }>`
-  position: relative;
-  background-color: white;
-  border-radius: 0.3rem;
-  font-size: ${font.size(2)};
-  line-height: 1.32;
-  transition: all 85ms ease-out;
-  padding: ${padding('small')};
-  border: 1px solid #edecf3;
-  width: 100%;
-  margin: 0;
-  outline: none;
-  box-shadow: 0 0 0 2px ${p => (p.error ? color.error() : 'transparent')};
-
-  &:hover {
-    box-shadow: 0 0 0 2px #e1ddec;
-  }
-
-  &:focus {
-    box-shadow: 0 0 0 2px ${p => (p.error ? color.error() : color.primary())};
-  }
-
-  &::placeholder {
-    font-size: 0.9rem;
-    color: #cfd3d7;
-  }
+export const TextFieldCss = css<InputProps>`
+  ${InputCss};
 `
 
-export const TextField = styled.input<{ error?: boolean }>`
+export const TextField = styled.input<TextFieldProps>`
   ${TextFieldCss}
 `
