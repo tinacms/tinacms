559ce36874df8e562dd1f07f4c9a300efb714cd9
Scott Byrne <scott.byrne@forestry.io>
2019-10-11 10:44:32 -0300

    fix: safari form overflow scrolling

--- packages/tinacms/src/components/FormView.tsx
+++ packages/tinacms/src/components/FormView.tsx
@@ -306,6 +306,8 @@ const FormHeader = styled(
 export const FormBody = styled.div`
   position: relative;
   flex: 1 1 auto;
+  display: flex;
+  flex-direction: column;
   scrollbar-width: none;
   width: 100%;
   overflow: hidden;
