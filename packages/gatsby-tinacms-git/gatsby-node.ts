fc03b9972e460ca992bbcf0548541460ab92086e
Nolan Phillips <ncphillips.19@gmail.com>
2019-10-12 14:10:47 -0300

    refactor: react-*-remark => gatsby-*-remark
    Now a plugin
    Moved the definition of the MarkdownRemark resolvers

--- packages/gatsby-tinacms-git/gatsby-node.ts
+++ packages/gatsby-tinacms-git/gatsby-node.ts
@@ -17,41 +17,6 @@ limitations under the License.
 */
 
 import { router as gitRouter, GitRouterConfig } from '@tinacms/api-git'
-// @ts-ignore
-import { GraphQLString } from 'gatsby/graphql'
-
-exports.setFieldsOnGraphQLNodeType = ({ type }: any) => {
-  const pathRoot = process.cwd()
-  if (type.name === `MarkdownRemark`) {
-    return {
-      rawFrontmatter: {
-        type: GraphQLString,
-        args: {
-          myArgument: {
-            type: GraphQLString,
-          },
-        },
-        resolve: (source: any) => {
-          return JSON.stringify(source.frontmatter)
-        },
-      },
-      fileRelativePath: {
-        type: GraphQLString,
-        args: {
-          myArgument: {
-            type: GraphQLString,
-          },
-        },
-        resolve: (source: any) => {
-          return source.fileAbsolutePath.replace(pathRoot, '')
-        },
-      },
-    }
-  }
-
-  // by default return empty object
-  return {}
-}
 
 exports.onCreateDevServer = ({ app }: any, options: GitRouterConfig) => {
   app.use('/___tina', gitRouter(options))
---
+++ packages/gatsby-tinacms-remark/index.js
@@ -0,0 +1,2 @@
+// Blank. For Gatsby
+export * from './build'
