a3d727f86cafa7b533d76f08f8a937522c72a67f
Nolan Phillips <ncphillips.19@gmail.com>
2019-10-20 10:34:33 -0700

    refactor(form): move interfaces to top

--- packages/@tinacms/core/src/cms-forms/form.ts
+++ packages/@tinacms/core/src/cms-forms/form.ts
@@ -19,6 +19,25 @@ limitations under the License.
 import arrayMutators from 'final-form-arrays'
 import { FormApi, createForm, Config, Unsubscribe } from 'final-form'
 
+export interface FormOptions<S> extends Config<S> {
+  id: any
+  label: string
+  fields: Field[]
+  reset?(): void
+  actions?: any[]
+}
+
+export interface Field {
+  name: string
+  label?: string
+  description?: string
+  component: React.FC<any> | string | null
+  parse?: (value: string, name: string) => any
+  format?: (value: string, name: string) => any
+  defaultValue?: any
+  fields?: Field[]
+}
+
 interface FieldSubscription {
   path: string
   field: Field
@@ -154,23 +173,4 @@ export class Form<S = any> {
   get values() {
     return this.finalForm.getState().values
   }
-}
-
-export interface FormOptions<S> extends Config<S> {
-  id: any
-  label: string
-  fields: Field[]
-  reset?(): void
-  actions?: any[]
-}
-
-export interface Field {
-  name: string
-  label?: string
-  description?: string
-  component: React.FC<any> | string | null
-  parse?: (value: string, name: string) => any
-  format?: (value: string, name: string) => any
-  defaultValue?: any
-  fields?: Field[]
 }
