80188361ce8413575889fa1f330ed6d2647e70cd
Bolarinwa Balogun <bo.lariin@hotmail.com>
2019-10-24 12:53:09 -0400

    refactor: move `createJestConfig` into the @tinacms/scripts package (#353)
    refactor: move `createJestConfig` into the @tinacms/scripts package

--- packages/@tinacms/core/jest.config.js
+++ packages/@tinacms/core/jest.config.js
@@ -16,7 +16,7 @@ limitations under the License.
 
 */
 
-const createJestConfig = require('../../../create.jest.config.js')
+const { createJestConfig } = require('@tinacms/scripts')
 const pack = require('./package')
 
 module.exports = createJestConfig(pack)
