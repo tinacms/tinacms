ce8c07a0c59b0c315847e66a7e18afc708e8dfb9
Nolan Phillips <ncphillips.19@gmail.com>
2019-10-24 09:42:45 -0700

    test(checkFilePathIsInRepo): correct import

--- packages/@tinacms/api-git/src/router.test.ts
+++ packages/@tinacms/api-git/src/router.test.ts
@@ -1,15 +1,15 @@
-import { checkFilePathIsInGitRepo } from './router'
+import { checkFilePathIsInRepo } from './router'
 
-describe('checkFilePathIsInGitRepo', () => {
+describe('checkFilePathIsInRepo', () => {
   test('returns false if path not in repo', () => {
     const fileRelativePath = '../../../some-outside-file.json'
     const repoAbsPath = __dirname + '/..'
-    expect(checkFilePathIsInGitRepo(fileRelativePath, repoAbsPath)).toBeFalsy()
+    expect(checkFilePathIsInRepo(fileRelativePath, repoAbsPath)).toBeFalsy()
   })
 
   test('returns true if path in repo', () => {
     const fileRelativePath = './some-inside-file.json'
     const repoAbsPath = __dirname + '/..'
-    expect(checkFilePathIsInGitRepo(fileRelativePath, repoAbsPath)).toBeTruthy()
+    expect(checkFilePathIsInRepo(fileRelativePath, repoAbsPath)).toBeTruthy()
   })
 })
