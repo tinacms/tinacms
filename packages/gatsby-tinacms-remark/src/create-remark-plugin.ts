6fa1faee75bcdf7ca57fb666150529d96c878a53
Nolan Phillips <ncphillips.19@gmail.com>
2019-10-29 10:53:57 -0700

    feat(create remark plugin): defined as class
    deprecated old API

--- packages/gatsby-tinacms-remark/src/create-remark-plugin.ts
+++ packages/gatsby-tinacms-remark/src/create-remark-plugin.ts
@@ -17,7 +17,7 @@ limitations under the License.
 */
 
 import { toMarkdownString } from './to-markdown'
-import { CMS, Field } from '@tinacms/core'
+import { CMS, Field, Form } from '@tinacms/core'
 import { AddContentPlugin } from 'tinacms'
 
 type MaybePromise<T> = Promise<T> | T
@@ -35,38 +35,57 @@ const MISSING_FILENAME_MESSAGE =
 const MISSING_FIELDS_MESSAGE =
   'createRemarkButton must be given `fields: Field[]` with at least 1 item'
 
+/**
+ *
+ * @deprecated in favour of calling `CreateRemarkPlugin` class directly.
+ */
 export function createRemarkButton<FormShape = any, FrontmatterShape = any>(
   options: CreateRemarkButtonOptions<FormShape, FrontmatterShape>
-): AddContentPlugin {
-  if (!options.filename) {
-    console.error(MISSING_FILENAME_MESSAGE)
-    throw new Error(MISSING_FILENAME_MESSAGE)
-  }
-  if (!options.fields || options.fields.length === 0) {
-    console.error(MISSING_FIELDS_MESSAGE)
-    throw new Error(MISSING_FIELDS_MESSAGE)
-  }
-  const formatFilename = options.filename
-  const createFrontmatter = options.frontmatter || (() => ({}))
-  const createBody = options.body || (() => '')
-  return {
-    __type: 'content-button',
-    name: options.label,
-    fields: options.fields,
-    onSubmit: async (form: any, cms: CMS) => {
-      const filename = await formatFilename(form)
-      const rawFrontmatter = await createFrontmatter(form)
-      const rawMarkdownBody = await createBody(form)
+): AddContentPlugin<FormShape> {
+  return new CreateRemarkPlugin<FormShape, FrontmatterShape>(options)
+}
 
-      const fileRelativePath = filename
-      cms.api.git!.onChange!({
+export class CreateRemarkPlugin<FormShape = any, FrontmatterShape = any>
+  implements AddContentPlugin<FormShape> {
+  __type: 'content-button' = 'content-button'
+  name: AddContentPlugin<FormShape>['name']
+  fields: AddContentPlugin<FormShape>['fields']
+
+  // Remark Specific
+  filename: (form: FormShape) => MaybePromise<string>
+  frontmatter: (form: FormShape) => MaybePromise<FrontmatterShape>
+  body: (form: any) => MaybePromise<string>
+
+  constructor(options: CreateRemarkButtonOptions<FormShape, FrontmatterShape>) {
+    if (!options.filename) {
+      console.error(MISSING_FILENAME_MESSAGE)
+      throw new Error(MISSING_FILENAME_MESSAGE)
+    }
+
+    if (!options.fields || options.fields.length === 0) {
+      console.error(MISSING_FIELDS_MESSAGE)
+      throw new Error(MISSING_FIELDS_MESSAGE)
+    }
+
+    this.name = options.label
+    this.fields = options.fields
+    this.filename = options.filename
+    this.frontmatter = options.frontmatter || (() => ({} as FrontmatterShape))
+    this.body = options.body || (() => '')
+  }
+
+  async onSubmit(form: FormShape, cms: CMS) {
+    const fileRelativePath = await this.filename(form)
+    const rawFrontmatter = await this.frontmatter(form)
+    const rawMarkdownBody = await this.body(form)
+
+    cms.api.git!.onChange!({
+      fileRelativePath,
+      content: toMarkdownString({
         fileRelativePath,
-        content: toMarkdownString({
-          fileRelativePath,
-          rawFrontmatter,
-          rawMarkdownBody,
-        }),
-      })
-    },
+        rawFrontmatter,
+        rawMarkdownBody,
+      }),
+    })
   }
 }
