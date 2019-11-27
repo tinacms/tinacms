21ecd017cb8990ee1c5442806c1a7bfe8cbdaf83
Nolan Phillips <ncphillips.19@gmail.com>
2019-11-27 11:45:11 -0400

    test(Blocks): add basic spec for block components

--- packages/react-tinacms-blocks/src/index.tsx
+++ packages/react-tinacms-blocks/src/index.tsx
@@ -15,43 +15,5 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 */
-import * as React from 'react'
-import { BlockTemplate as DefaultBlockTemplate } from 'tinacms'
 
-interface BlockTemplate extends DefaultBlockTemplate {
-  Component: any
-}
-
-interface BlocksProps {
-  name: string
-  data: any[]
-  templates: {
-    [key: string]: BlockTemplate
-  }
-}
-
-export function Blocks({ name, data = [], templates = {} }: BlocksProps) {
-  return data.map((data, index) => {
-    const template = templates[data._template]
-
-    if (!template) {
-      return nullOrError('Unrecognized Block Type: ' + data._template)
-    }
-
-    if (!template.Component) {
-      return nullOrError(
-        `The "${template.label}" block template is missing the Component`
-      )
-    }
-
-    return <template.Component data={data} index={index} name={name} />
-  })
-}
-
-const nullOrError = (message: string) => {
-  if (process.env.NODE_ENV) {
-    throw new Error(message)
-  }
-
-  return null
-}
+export { Blocks, BlocksProps, BlockTemplate } from './blocks'
