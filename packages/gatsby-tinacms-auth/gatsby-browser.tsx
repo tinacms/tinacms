a8ff6c5f1981e9a263bc97de3fddae083cbe7bd9
James O'Halloran <james.p.ohalloran@gmail.com>
2019-12-02 16:54:42 -0400

    show content based on auth state

--- packages/gatsby-tinacms-auth/gatsby-browser.tsx
+++ packages/gatsby-tinacms-auth/gatsby-browser.tsx
@@ -17,52 +17,15 @@ limitations under the License.
 */
 
 import * as React from 'react'
-import { useEffect, useState } from 'react'
-import { VIRTUAL_SERVICE_DOMAIN, TINA_CONNECTOR_ID } from './contants'
+import { AuthContainer } from './src/AuthContainer'
 interface Options {
   authEnvironments: string[]
 }
 
 exports.wrapRootElement = ({ element }: any, options: Options) => {
   return (
-    <TinaTeamsAuth authEnvironments={options.authEnvironments}>
+    <AuthContainer authEnvironments={options.authEnvironments}>
       {element}
-    </TinaTeamsAuth>
+    </AuthContainer>
   )
-}
-
-const TinaTeamsAuth = (props: any) => {
-  const { authEnvironments, children } = props
-
-  const [authenticated, setAuthenticated] = useState(false)
-
-  let token: string
-  if ((authEnvironments || []).indexOf(process.env.NODE_ENV || '') >= 0) {
-    token = new URL(window.location.href).searchParams.get('token') || ''
-    if (!token) {
-      window.location.href = `https://api.${VIRTUAL_SERVICE_DOMAIN}/auth-proxy?connector_id=${TINA_CONNECTOR_ID}&origin=${encodeURIComponent(
-        window.location.href
-      )}`
-      return
-    }
-
-    //todo - put token in cookie
-    // strip token
-  }
-
-  useEffect(() => {
-    const verifyAccess = async () => {
-      const result = await fetch('/___tina/teams/auth', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json; charset=utf-8',
-        },
-        body: JSON.stringify({ token }),
-      })
-      // console.log(JSON.stringify('result ' + JSON.stringify(result.status)))
-    }
-    verifyAccess()
-  }, [])
-
-  return children
 }
