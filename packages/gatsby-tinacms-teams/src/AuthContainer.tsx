dbae45351b4e945f20d346cd526bc31cca1de987
Nolan Phillips <ncphillips.19@gmail.com>
2019-12-05 10:31:25 -0400

    chore: rename plugin to gatsby-tinacms-teams

--- packages/gatsby-tinacms-teams/src/AuthContainer.tsx
+++ packages/gatsby-tinacms-teams/src/AuthContainer.tsx
@@ -15,7 +15,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 */
-import * as React from 'react'
 
 export const AuthContainer = (props: any) => {
   const { children } = props
