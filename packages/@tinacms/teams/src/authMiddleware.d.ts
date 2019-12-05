ce1545a431722fef25dc255870d7fe68da530a11
Nolan Phillips <ncphillips.19@gmail.com>
2019-12-05 14:59:28 -0400

    chore: remove js and .d.ts files from git

--- packages/@tinacms/teams/src/authMiddleware.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function authenticate(req: any, res: any, next: any): any;
-export declare function redirectNonAuthenticated(req: any, res: any, next: any): void;
-export declare function authorize(req: any, res: any, next: any): void;
