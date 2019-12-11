84f16c7a8399db3dc6e7809fa12ac13fc22d74b5
Scott Byrne <scott.byrne@forestry.io>
2019-12-11 14:57:32 -0400

    style(demo): improve plain text component

--- packages/demo-gatsby/src/components/edit-toggle.js
+++ packages/demo-gatsby/src/components/edit-toggle.js
@@ -1,15 +1,16 @@
 import React from "react"
+import styled from "styled-components"
 import {
   Button as TinaButton,
-  GlobalStyles as TinaGlobalStyles,
+  TinaResetStyles as TinaStyles,
 } from "@tinacms/styles"
-import styled from "styled-components"
+import { EditIcon } from "@tinacms/icons"
 
 export const EditToggle = styled(
   ({ isEditing, setIsEditing, ...styleProps }) => {
     return (
       <TinaButton onClick={() => setIsEditing(p => !p)} primary {...styleProps}>
-        {isEditing ? "Stop Editing" : "Start Editing"}
+        <EditIcon /> {isEditing ? "Preview" : "Edit"}
       </TinaButton>
     )
   }
@@ -17,4 +18,9 @@ export const EditToggle = styled(
   position: fixed;
   bottom: 2.875rem;
   margin-left: 4rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-width: 8rem;
+  font-family: "Inter", sans-serif;
 `
