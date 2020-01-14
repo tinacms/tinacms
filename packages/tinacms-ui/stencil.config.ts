a7296ab40cc3cbb682d3f8f077a7c55311061adf
Nolan Phillips <ncphillips.19@gmail.com>
2020-01-14 17:14:42 -0400

    feat: created tinacms-ui component lib

---
+++ packages/tinacms-ui/stencil.config.ts
@@ -0,0 +1,18 @@
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  namespace: 'tinacms-ui',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader'
+    },
+    {
+      type: 'docs-readme'
+    },
+    {
+      type: 'www',
+      serviceWorker: null // disable service workers
+    }
+  ]
+};
