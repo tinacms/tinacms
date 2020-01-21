cec3ee55d4d7a024931ed160b81f7f1b57d08cfa
Kendall Strautman <kendallstrautman@gmail.com>
2020-01-21 11:54:37 -0800

    merging master

---
+++ packages/@tinacms/styles/src/stencil-components.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import { JSX } from 'tinacms-ui';
+
+import { defineCustomElements, applyPolyfills } from 'tinacms-ui/loader';
+
+applyPolyfills().then(() => { defineCustomElements(window); });
+export const MyComponent = /*@__PURE__*/createReactComponent<JSX.MyComponent, HTMLMyComponentElement>('my-component');
