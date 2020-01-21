7f4208e1be3ca15e5aa5300344fa0ede94cc3b34
Kendall Strautman <kendallstrautman@gmail.com>
2020-01-21 15:38:42 -0800

    fix: removes generated component style lib

--- packages/@tinacms/styles/src/react-component-lib/index.ts
+++
@@ -1,3 +0,0 @@
-export { createReactComponent } from './createComponent';
-export { createControllerComponent } from './createControllerComponent';
-export { createOverlayComponent } from './createOverlayComponent';
