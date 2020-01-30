d58ab346e3b31465de1e6b7461911406c38b8a13
jpuri <jyotipuri@gmail.com>
2020-01-30 19:19:46 +0530

    feat: table column alignment

--- packages/@tinacms/fields/src/Wysiwyg/state/plugins/Menu/FloatingTableMenu/TableOptionsMenu.tsx
+++ packages/@tinacms/fields/src/Wysiwyg/state/plugins/Menu/FloatingTableMenu/TableOptionsMenu.tsx
@@ -19,14 +19,39 @@ limitations under the License.
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { EditorView } from 'prosemirror-view'
-import { deleteTable } from 'prosemirror-tables'
+import { deleteTable, TableMap } from 'prosemirror-tables'
 import styled from 'styled-components'
+import {
+  findParentNodeOfType,
+  forEachCellInColumn,
+  setCellAttrs,
+} from 'prosemirror-utils'
 
-import { TrashIcon } from '@tinacms/icons'
+import { AlignCenter, AlignLeft, AlignRight, TrashIcon } from '@tinacms/icons'
 import { IconButton } from '@tinacms/styles'
 
 interface TableOptionsMenu {
   view: EditorView
+}
+
+const alignColumn = (view: EditorView, align: string) => {
+  const { state, dispatch } = view
+  const { selection } = state
+  const { table } = state.schema.nodes
+  const tableNode = findParentNodeOfType(table)(state.selection)
+  if (!tableNode) return
+  const tableMap = TableMap.get(tableNode.node)
+  const pos = Object.entries(tableMap.map).find(
+    entry => entry[1] > selection.head - tableNode.start
+  )
+  if (!pos) return
+  const cellPos = parseInt(pos[0]) - 1
+  const columnPos = cellPos % tableMap.width
+  dispatch(
+    forEachCellInColumn(columnPos, (cell, tr) =>
+      setCellAttrs(cell, { align })(tr)
+    )(state.tr)
+  )
 }
 
 export default ({ view }: TableOptionsMenu) => {
@@ -43,6 +68,15 @@ export default ({ view }: TableOptionsMenu) => {
   const { height, width } = tableElm.getBoundingClientRect()
   return ReactDOM.createPortal(
     <Wrapper height={height} width={width}>
+      <IconButton onClick={() => alignColumn(view, 'left')} small primary>
+        <AlignLeft />
+      </IconButton>
+      <IconButton onClick={() => alignColumn(view, 'center')} small primary>
+        <AlignCenter />
+      </IconButton>
+      <IconButton onClick={() => alignColumn(view, 'right')} small primary>
+        <AlignRight />
+      </IconButton>
       <IconButton onClick={deleteSelectedTable} small primary>
         <TrashIcon />
       </IconButton>
@@ -56,10 +90,13 @@ const Wrapper = styled.div<
 >`
   background-color: #ffffff;
   border-radius: 2px;
-  // border: 1px solid #e1ddec;
+  display: flex;
   cursor: default;
   padding: 0px 4px;
   position: absolute;
   top: ${({ height }) => `${height + 24}px`};
-  left: ${({ width }) => `${width / 2 - 8}px`};
+  left: ${({ width }) => `${width / 2 - 64}px`};
+  button:not(:first-of-type) {
+    margin-left: 10px;
+  }
 `
---
+++ packages/@tinacms/icons/src/AlignLeft.tsx
@@ -0,0 +1,31 @@
+/**
+
+Copyright 2019 Forestry.io Inc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+
+import * as React from 'react'
+
+export const AlignLeft = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24"
+    viewBox="0 0 24 24"
+    width="24"
+  >
+    <path d="M15 15H3v2h12v-2zm0-8H3v2h12V7zM3 13h18v-2H3v2zm0 8h18v-2H3v2zM3 3v2h18V3H3z" />
+    <path d="M0 0h24v24H0z" fill="none" />
+  </svg>
+)
