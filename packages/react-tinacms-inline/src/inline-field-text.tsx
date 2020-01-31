3cfb059f1f355dcad0712a9e4d5000871e9484c5
Nolan Phillips <ncphillips.19@gmail.com>
2020-01-31 15:42:21 -0400

    feat(InlineTextField): makes editing inline text easy

--- packages/react-tinacms-inline/src/inline-field-text.tsx
+++ packages/react-tinacms-inline/src/inline-field-text.tsx
@@ -16,6 +16,25 @@ limitations under the License.
 
 */
 
-export * from './inline-form'
-export * from './inline-field'
-export * from './inline-field-text'
+import * as React from 'react'
+import { InlineField } from './inline-field'
+
+/**
+ * InlineTextField
+ */
+export interface InlineTextFieldProps {
+  name: string
+}
+
+export function InlineTextField({ name }: InlineTextFieldProps) {
+  return (
+    <InlineField name={name}>
+      {({ input, status }) => {
+        if (status === 'active') {
+          return <input type="text" {...input} />
+        }
+        return <>{input.value}</>
+      }}
+    </InlineField>
+  )
+}
