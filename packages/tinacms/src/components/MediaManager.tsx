14aed0d03bb0741fc40f47c5f0cd0841c8298b56
Nolan Phillips <ncphillips.19@gmail.com>
2020-02-05 15:08:56 -0400

    feat(media manager): media can be selected

--- packages/tinacms/src/components/MediaManager.tsx
+++ packages/tinacms/src/components/MediaManager.tsx
@@ -1,12 +1,22 @@
 import * as React from 'react'
 import { useState, useEffect } from 'react'
-import { MediaProps } from '../media'
 import { useCMS } from '../react-tinacms'
+import { MediaFilter, Media } from '../media'
+
+interface MediaProps {
+  multiple?: boolean
+  selected?: string[]
+  disabled?: MediaFilter
+  filter?: MediaFilter
+  onChoose?(media: Media[]): void
+}
 
 export const MediaManager = (props: MediaProps) => {
   let cms = useCMS()
-  let [media, setMedia] = useState([] as any)
-  let [selected, setSelected] = useState(props.selected)
+  let [allMedia, setMedia] = useState([] as any)
+
+  let { selected, handleClickToSelect } = useSelection(props.selected)
+
   useEffect(() => {
     ;(async () => {
       let media = await cms.media.store.list({ limit: 8, offset: 0 })
@@ -16,13 +26,77 @@ export const MediaManager = (props: MediaProps) => {
 
   return (
     <div style={{ display: 'flex' }}>
-      {media.map((m: any) => (
-        <div
-          style={{ border: '1px dotted', padding: '.5rem', margin: '.5rem' }}
+      <nav>
+        <button
+          disabled={!selected.length}
+          onClick={() => cms.media.store.delete(selected[0])}
         >
-          file: {m.src}
+          Delete {!!selected.length && `${selected.length} items`}
+        </button>
+      </nav>
+      {allMedia.map((media: Media) => (
+        <div
+          key={media.src}
+          style={{
+            border: '2px solid',
+            padding: '.5rem',
+            margin: '.5rem',
+            borderColor: selected.includes(media.reference) ? 'green' : 'black',
+          }}
+          onClick={event => {
+            handleClickToSelect(event, media)
+          }}
+        >
+          file: {media.src}
         </div>
       ))}
     </div>
   )
+}
+
+/**
+ * NOTE: This is curently more complex then necessary.
+ * Using a reducer will be useful once we implement multiselect.
+ */
+function useSelection(preselect: string[] = []) {
+  const [state, dispatch] = React.useReducer(singleSelectReducer, {
+    lastToggled: null,
+    selected: preselect,
+  })
+
+  return {
+    ...state,
+    handleClickToSelect(
+      _: React.MouseEvent<HTMLDivElement, MouseEvent>,
+      media: Media
+    ) {
+      dispatch({
+        type: 'SELECT_MEDIA',
+        selected: [media.reference],
+      })
+    },
+  }
+}
+
+export interface SelectionState {
+  lastToggled: string | null
+  selected: string[]
+}
+
+function singleSelectReducer(
+  state: SelectionState,
+  changes: any
+): SelectionState {
+  switch (changes.type) {
+    case 'SELECT_MEDIA':
+    case 'SHIFT_SELECT_MEDIA':
+      let lastSelected = changes.selected[changes.selected.length - 1]
+      return {
+        ...state,
+        selected:
+          state.selected.indexOf(lastSelected) >= 0 ? [] : [lastSelected],
+      }
+    default:
+      return { ...state, ...changes }
+  }
 }
