6bdc4bdf886b35845803f0c7ff8caf27878f27b5
DJ <dj@walker.dev>
2020-02-06 16:01:53 -0500

    chore(demo-next): fix withTina usage in demo

--- packages/demo-next/pages/_app.js
+++ packages/demo-next/pages/_app.js
@@ -22,12 +22,10 @@ import { withTina } from 'tinacms'
 import { GitClient } from '@tinacms/git-client'
 
 export default withTina(App, {
-  cms: {
-    apis: {
-      git: new GitClient('http://localhost:3000/___tina')
-    }
+  apis: {
+    git: new GitClient('http://localhost:3000/___tina'),
   },
   sidebar: {
-    hidden: process.env.NODE_ENV === 'production'
-  }
+    hidden: process.env.NODE_ENV === 'production',
+  },
 })
