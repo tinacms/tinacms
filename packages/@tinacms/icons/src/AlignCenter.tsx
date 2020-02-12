382e34a59fefa26bf96e1836c89864d6c3828a6d
Scott Byrne <scott.byrne@forestry.io>
2020-02-12 10:00:55 -0400

    style: improve text aligment icons

--- packages/@tinacms/icons/src/AlignCenter.tsx
+++ packages/@tinacms/icons/src/AlignCenter.tsx
@@ -19,13 +19,10 @@ limitations under the License.
 import * as React from 'react'
 
 export const AlignCenter = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    height="24"
-    viewBox="0 0 24 24"
-    width="24"
-  >
-    <path d="M3 21h18v-2H3v2zm0-4h18v-2H3v2zm0-4h18v-2H3v2zm0-4h18V7H3v2zm0-6v2h18V3H3z" />
-    <path d="M0 0h24v24H0z" fill="none" />
+  <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M9.125 24H22.875V26H9.125V24ZM5 18H27V20H5V18ZM5 6H27V8H5V6ZM9.125 12H22.875V14H9.125V12Z"
+      fill="inherit"
+    />
   </svg>
 )
