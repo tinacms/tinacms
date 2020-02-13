1896f32062ff75e6c8be51c3f60338e3d51b0a0f
Jordan Patterson <jordan@jpatterson.me>
2020-02-12 21:43:31 -0400

    refactor: attempt to fix tests for windows

--- packages/@tinacms/api-git/src/utils/paths.test.ts
+++ packages/@tinacms/api-git/src/utils/paths.test.ts
@@ -23,13 +23,13 @@ import { checkFilePathIsInParent } from './paths'
 describe('checkFilePathIsInParent', () => {
   test('returns false if path not in repo', () => {
     const fileRelativePath = '../../../some-outside-file.json'
-    const repoAbsPath = path.resolve('./')
+    const repoAbsPath = path.posix.resolve('./')
     expect(checkFilePathIsInParent(fileRelativePath, repoAbsPath)).toBeFalsy()
   })
 
   test('returns true if path in repo', () => {
     const fileRelativePath = './some-inside-file.json'
-    const repoAbsPath = path.resolve('./')
+    const repoAbsPath = path.posix.resolve('./')
     expect(checkFilePathIsInParent(fileRelativePath, repoAbsPath)).toBeTruthy()
   })
 })
