8e798491e461e940ed9fdf06b9c78ddfa6fb79e8
Jordan Patterson <jordan@jpatterson.me>
2020-02-14 10:54:48 -0400

    test: added some tests for repo class. more to come

--- packages/@tinacms/api-git/src/repo.ts
+++ packages/@tinacms/api-git/src/repo.ts
@@ -43,7 +43,7 @@ export class Repo {
   }
 
   get tmpDir() {
-    return path.join(this.contentAbsolutePath, '/tmp/')
+    return path.join(this.contentAbsolutePath, '/tmp')
   }
 
   get sshKeyPath() {
