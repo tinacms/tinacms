b9b85d3d0d978ec0e2d37c9eb2a63040f17d4221
JavaScript Joe <jjprevite@gmail.com>
2020-02-16 19:29:47 -0800

    feat: add new mdx page and mdx component

---
+++ packages/demo-gatsby/src/pages/hello.js
@@ -0,0 +1,11 @@
+import React from "react"
+import TestMdxForm from "../components/test-mdx-form"
+
+const HelloPage = () => (
+  <div>
+    <h1>Hi Joe</h1>
+    {/* <TestMdxForm /> */}
+  </div>
+)
+
+export default HelloPage
