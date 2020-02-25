18334046dc77f16e0108f40f9920bea28d0099f3
Nolan Phillips <ncphillips.19@gmail.com>
2020-02-25 11:22:54 -0400

    chore: add license to files

--- packages/@tinacms/fields/src/Wysiwyg/Translator/DOMTranslator/index.ts
+++ packages/@tinacms/fields/src/Wysiwyg/Translator/DOMTranslator/index.ts
@@ -1,6 +1,24 @@
-import { Schema, DOMParser as PDOMParser, Node } from "prosemirror-model"
-import { DOMSerializer } from "./to_dom"
-import { Translator } from "../Translator"
+/**
+
+Copyright 2019 Forestry.io Inc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+
+import { Schema, DOMParser as PDOMParser, Node } from 'prosemirror-model'
+import { DOMSerializer } from './to_dom'
+import { Translator } from '../Translator'
 
 export class DOMTranslator extends Translator {
   schema: Schema
@@ -24,14 +42,14 @@ export class DOMTranslator extends Translator {
     try {
       al = window.document.createRange().createContextualFragment(value)
     } catch (e) {
-      al = new DOMParser().parseFromString(value, "text/html")
+      al = new DOMParser().parseFromString(value, 'text/html')
     }
 
     return this.parser.parse(al)
   }
 
   stringFromNode(node: Node): string {
-    const el = document.createElement("div")
+    const el = document.createElement('div')
 
     el.appendChild(this.serializer.serializeFragment(node.content))
 
