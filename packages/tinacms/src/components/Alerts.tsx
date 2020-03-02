681b30d634341f4e07f9b54081da13b83d5f9b9d
Scott Byrne <scott.byrne@forestry.io>
2020-03-02 15:50:03 -0400

    style: add close button

--- packages/tinacms/src/components/Alerts.tsx
+++ packages/tinacms/src/components/Alerts.tsx
@@ -20,7 +20,13 @@ import { useCMS, useSubscribable } from '../react-tinacms'
 import React from 'react'
 import styled, { css, keyframes } from 'styled-components'
 import { padding, color, radius, font, shadow } from '@tinacms/styles'
-import { AlertIcon, InfoIcon, WarningIcon, ErrorIcon } from '@tinacms/icons'
+import {
+  AlertIcon,
+  InfoIcon,
+  WarningIcon,
+  ErrorIcon,
+  CloseIcon,
+} from '@tinacms/icons'
 import { AlertLevel } from '../tina-cms/alerts'
 
 export function Alerts() {
@@ -46,7 +52,8 @@ export function Alerts() {
                 {alert.level === 'success' && <AlertIcon />}
                 {alert.level === 'warn' && <WarningIcon />}
                 {alert.level === 'error' && <ErrorIcon />}
-                {alert.message}
+                <p>{alert.message}</p>
+                <CloseAlert />
               </Alert>
             )
           })}
@@ -59,8 +66,9 @@ export function Alerts() {
 const AlertContainer = styled.div`
   position: fixed;
   bottom: 0;
-  left: 50%;
-  transform: translate3d(-50%, 0, 0);
+  left: 0;
+  right: 0;
+  padding: 24px;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -89,11 +97,13 @@ const Alert = styled.div<{ level: AlertLevel; index: number }>`
   font-weight: 500;
   cursor: pointer;
   font-size: ${font.size(2)};
-  padding: 8px 12px;
+  padding: 8px 4px 8px 12px;
   transition: all 85ms ease-out;
   margin-bottom: 16px;
   display: flex;
   align-items: center;
+  min-width: 350px;
+  max-width: 100%;
 
   animation-name: ${AlertEntranceAnimation};
   animation-timing-function: ease-out;
@@ -101,10 +111,17 @@ const Alert = styled.div<{ level: AlertLevel; index: number }>`
   animation-fill-mode: both;
   animation-duration: 150ms;
 
+  p {
+    margin: 0;
+    flex: 1 0 auto;
+    text-align: left;
+  }
+
   svg {
     width: 20px;
     height: 20px;
     margin-right: 8px;
+    flex: 0 0 auto;
   }
 
   ${props =>
@@ -134,4 +151,27 @@ const Alert = styled.div<{ level: AlertLevel; index: number }>`
       fill: ${color.error()};
       border-left: 6px solid ${color.error()};
     `};
+`
+
+const CloseAlert = styled(({ ...styleProps }) => {
+  return (
+    <button {...styleProps}>
+      <CloseIcon />
+    </button>
+  )
+})`
+  border: none;
+  background: transparent;
+  padding: 0;
+  margin-left: 14px;
+  outline: none;
+  fill: ${color.grey(5)};
+  display: flex;
+  align-items: center;
+
+  svg {
+    width: 20px;
+    height: 20px;
+    flex: 0 0 auto;
+  }
 `
