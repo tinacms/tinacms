ff1424cb2227c3933527fecfc26d8e1d14d67b3c
Nolan Phillips <ncphillips.19@gmail.com>
2020-03-04 10:15:23 -0400

    refactor: group all form-related components

--- packages/tinacms/src/plugins/screens/index.tsx
+++ packages/tinacms/src/plugins/screens/index.tsx
@@ -20,7 +20,7 @@ import * as React from 'react'
 import { SettingsIcon } from '@tinacms/icons'
 import { Form } from '@tinacms/forms'
 import { ScreenPlugin } from '../screen-plugin'
-import { FormView } from '../../components/FormView'
+import { FormView } from '../../components/form/FormView'
 
 export class GlobalFormPlugin implements ScreenPlugin {
   __type: ScreenPlugin['__type'] = 'screen'
