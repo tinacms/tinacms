8b6f8f8d3c6873b85f27d9f6b9e78a2789584482
Kendall Strautman <kendallstrautman@gmail.com>
2020-03-17 13:37:21 -0600

    chore: gets media store working

--- packages/demo-next/pages/_app.js
+++ packages/demo-next/pages/_app.js
@@ -16,20 +16,45 @@ limitations under the License.
 
 */
 
+import React from 'react'
 import App from 'next/app'
-import { withTina } from 'tinacms'
+import { Tina, TinaCMS, withTina } from 'tinacms'
 import { GitClient, GitMediaStore } from '@tinacms/git-client'
 
-const client = new GitClient('http://localhost:3000/___tina')
+export default class Site extends App {
+  constructor() {
+    super()
+    this.cms = new TinaCMS({
+      sidebar: {
+        position: 'overlay',
+        hidden: process.env.NODE_ENV === 'production',
+      },
+    })
+    const client = new GitClient('http://localhost:3000/___tina')
+    this.cms.registerApi('git', client)
+    this.cms.media.store = new GitMediaStore(client)
+  }
 
-export default withTina(App, {
-  apis: {
-    git: client,
-  },
-  media: {
-    store: new GitMediaStore(client),
-  },
-  sidebar: {
-    hidden: process.env.NODE_ENV === 'production',
-  },
-})
+  render() {
+    const { Component, pageProps } = this.props
+    return (
+      <Tina cms={this.cms}>
+        <Component {...pageProps} />
+      </Tina>
+    )
+  }
+}
+
+// const client = new GitClient('http://localhost:3000/___tina')
+
+// export default withTina(App, {
+//   apis: {
+//     git: client,
+//   },
+//   media: {
+//     store: new GitMediaStore(client),
+//   },
+//   sidebar: {
+//     hidden: process.env.NODE_ENV === 'production',
+//   },
+// })
