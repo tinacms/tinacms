9fbcb3796389372ddfbedcf535959d74c7ef0121
Nolan Phillips <ncphillips.19@gmail.com>
2020-03-18 15:15:55 -0300

    chore: bring over modal components from tinacms

--- packages/@tinacms/react-modals/src/index.ts
+++ packages/@tinacms/react-modals/src/index.ts
@@ -15,3 +15,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 */
+
+export * from './ModalFullscreen'
+export * from './ModalPopup'
+export * from './ModalProvider'
