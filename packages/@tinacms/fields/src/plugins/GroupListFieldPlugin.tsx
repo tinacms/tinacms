61290093295011a8ccee7656f77596457241e538
Nolan Phillips <ncphillips.19@gmail.com>
2020-03-26 12:13:11 -0300

    chore: fix build

--- packages/@tinacms/fields/src/plugins/GroupListFieldPlugin.tsx
+++ packages/@tinacms/fields/src/plugins/GroupListFieldPlugin.tsx
@@ -30,7 +30,7 @@ import {
   LeftArrowIcon,
 } from '@tinacms/icons'
 import { GroupPanel, PanelHeader, PanelBody } from './GroupFieldPlugin'
-import { useFormPortal } from '../../components/form/FormPortal'
+import { useFormPortal } from '@tinacms/react-forms'
 
 interface GroupFieldDefinititon extends Field {
   component: 'group'
