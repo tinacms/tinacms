ca35be3c41fdac3314750b4c402677cbe105771b
Kendall Strautman <kendallstrautman@gmail.com>
2020-04-02 16:38:26 -0600

    chore: toolbar provider expects toolbar state

--- packages/@tinacms/react-toolbar/src/components/ToolbarProvider.tsx
+++ packages/@tinacms/react-toolbar/src/components/ToolbarProvider.tsx
@@ -1,12 +1,20 @@
 import * as React from 'react'
 import { Toolbar } from './Toolbar'
+import { ToolbarState } from '../toolbar'
 
 interface ToolbarProviderProps {
+  toolbar: ToolbarState
   hidden?: boolean
 }
 
-export function ToolbarProvider({ hidden }: ToolbarProviderProps) {
-  if (hidden) return null
+export function ToolbarProvider({ hidden, toolbar }: ToolbarProviderProps) {
+  React.useEffect(() => {
+    if (typeof hidden !== 'undefined') {
+      toolbar.hidden = hidden
+    }
+  }, [hidden])
+
+  if (toolbar.hidden) return null
 
   return <Toolbar />
 }
