10256c6266bff9e4dee11f214adbcd3cd0530ece
Nolan Phillips <ncphillips.19@gmail.com>
2020-04-01 21:52:32 -0300

    refactor: tinacms delegates to @tinacms/react-sidebar

--- packages/tinacms/src/tina-cms.ts
+++ packages/tinacms/src/tina-cms.ts
@@ -16,7 +16,7 @@
 
  */
 
-import { CMS, CMSConfig, PluginType, Subscribable } from '@tinacms/core'
+import { CMS, CMSConfig, PluginType } from '@tinacms/core'
 import { FieldPlugin } from '@tinacms/form-builder'
 import { ScreenPlugin } from '@tinacms/react-screens'
 import {
@@ -37,8 +37,7 @@ import MarkdownFieldPlugin from './plugins/fields/MarkdownFieldPlugin'
 import { Form } from '@tinacms/forms'
 import { MediaManager, MediaStore, MediaUploadOptions } from '@tinacms/media'
 import { Alerts } from './tina-cms/alerts'
-
-export declare type SidebarPosition = 'fixed' | 'float' | 'displace' | 'overlay'
+import { SidebarState, SidebarStateOptions } from '@tinacms/react-sidebar'
 
 export interface TinaCMSConfig extends CMSConfig {
   sidebar?: SidebarStateOptions
@@ -84,59 +83,6 @@ export class TinaCMS extends CMS {
 
   get screens(): PluginType<ScreenPlugin> {
     return this.plugins.findOrCreateMap('screen')
-  }
-}
-
-interface SidebarStateOptions {
-  hidden?: boolean
-  position?: SidebarPosition
-  buttons?: SidebarButtons
-}
-
-interface SidebarButtons {
-  save: string
-  reset: string
-}
-
-export class SidebarState extends Subscribable {
-  private _isOpen: boolean = false
-
-  position: SidebarPosition = 'displace'
-  _hidden: boolean = false
-  buttons: SidebarButtons = {
-    save: 'Save',
-    reset: 'Reset',
-  }
-
-  constructor(options: SidebarStateOptions = {}) {
-    super()
-    this.position = options.position || 'displace'
-    this._hidden = !!options.hidden
-
-    if (options.buttons?.save) {
-      this.buttons.save = options.buttons.save
-    }
-    if (options.buttons?.reset) {
-      this.buttons.reset = options.buttons.reset
-    }
-  }
-
-  get isOpen() {
-    return this._isOpen
-  }
-
-  set isOpen(nextValue: boolean) {
-    this._isOpen = nextValue
-    this.notifiySubscribers()
-  }
-
-  get hidden() {
-    return this._hidden
-  }
-
-  set hidden(nextValue: boolean) {
-    this._hidden = nextValue
-    this.notifiySubscribers()
   }
 }
 
