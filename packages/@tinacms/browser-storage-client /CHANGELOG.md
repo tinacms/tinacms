2d98393016d02470d63f238f4f2785f4955c8a25
Joel Huggett <joel@forestry.io>
2020-04-06 12:52:49 -0300

    added package

---
+++ packages/@tinacms/browser-storage-client /CHANGELOG.md
@@ -0,0 +1,11 @@
+# Change Log
+
+All notable changes to this project will be documented in this file.
+See [Conventional Commits](https://conventionalcommits.org) for commit guidelines.
+
+<!-- 
+# 0.1.0-alpha.0 (2019-11-28)
+
+### Features
+
+- **Blocks:** add Block component ([cfcb618](https://github.com/tinacms/tinacms/commit/cfcb618)) -->
---
+++ packages/@tinacms/browser-storage-client /src/BrowserStorageApi.ts
@@ -0,0 +1,60 @@
+/**
+
+Copyright 2019 Forestry.io Inc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+
+export class BrowserStorageApi {
+  data: any = {}
+  timeout: NodeJS.Timeout | null = null
+  namespace: string
+  storage: Storage // should work with window.localStorage or window.sessionStorage
+
+  constructor(storage: Storage, namespace: string | null = null) {
+    if (!namespace) {
+      this.namespace = `tina-local-storage:${window.location.hostname}`
+    } else {
+      this.namespace = namespace
+    }
+    this.storage = storage
+    const persistedData = this.storage.getItem(this.namespace)
+    if (persistedData) {
+      this.data = JSON.parse(persistedData)
+    }
+  }
+
+  save(id: string, content: any) {
+    this.data[id] = content
+    this.debouncePersist()
+  }
+
+  load(id: string) {
+    return this.data[id]
+  }
+
+  clear(id: string) {
+    delete this.data[id]
+    this.debouncePersist()
+  }
+
+  private debouncePersist() {
+    this.timeout && clearTimeout(this.timeout)
+    this.timeout = setTimeout(this.persist.bind(this), 1000)
+  }
+
+  private persist() {
+    this.storage.setItem(this.namespace, JSON.stringify(this.data))
+  }
+}
