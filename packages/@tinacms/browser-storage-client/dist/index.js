bf480f5380722c9155ffea82d6e803ba3b735475
Joel Huggett <joel@forestry.io>
2020-04-06 14:10:52 -0300

    removed dist

--- packages/@tinacms/browser-storage-client/dist/index.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./browser-storage-client.cjs.production.min.js.js')
-} else {
-  module.exports = require('./browser-storage-client.cjs.development.js.js')
-}
