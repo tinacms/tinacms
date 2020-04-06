f498636a1d9253382d387da228cf909a2a020243
James O'Halloran <james.p.ohalloran@gmail.com>
2020-04-06 15:40:26 -0300

    refactor(github): move getGithubDataFromPreviewProps to site from tinacms

--- packages/github-tinacms-content/src/sourceProviderConnection.ts
+++ packages/github-tinacms-content/src/sourceProviderConnection.ts
@@ -20,33 +20,3 @@ export interface SourceProviderConnection {
   forkFullName: string
   headBranch: string
 }
-
-export interface Response {
-  accessToken: string | null
-  sourceProviderConnection: SourceProviderConnection | null
-}
-
-export interface PreviewData {
-  fork_full_name: string
-  head_branch: string
-  github_access_token: string
-}
-
-export const getGithubDataFromPreviewProps = (
-  previewData?: PreviewData
-): Response => {
-  if (!previewData) {
-    return {
-      sourceProviderConnection: null,
-      accessToken: null,
-    }
-  }
-
-  return {
-    accessToken: previewData.github_access_token,
-    sourceProviderConnection: {
-      forkFullName: previewData.fork_full_name,
-      headBranch: previewData.head_branch || 'master',
-    },
-  }
-}
