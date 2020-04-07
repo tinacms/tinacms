5dbd2601d3858f79bb072a31e951bdafa786df61
Joel Huggett <joel@forestry.io>
2020-04-07 13:34:10 -0300

    fixed index issue

---
+++ packages/@tinacms/browser-storage-client/dist/index.js
@@ -0,0 +1,8 @@
+
+'use strict'
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./browser-storage-client.cjs.production.min.js')
+} else {
+  module.exports = require('./browser-storage-client.cjs.development.js')
+}
