bb81c4bed811aa22dadb234dc9a291b16daeb5be
Nolan Phillips <ncphillips.19@gmail.com>
2020-04-20 14:51:14 -0300

    chore: remove unused functions

--- packages/react-tinacms-github/src/github-editing-context/repository.ts
+++ packages/react-tinacms-github/src/github-editing-context/repository.ts
@@ -21,22 +21,8 @@ import Cookies from 'js-cookie'
 export const FORK_COOKIE_KEY = 'fork_full_name'
 export const HEAD_BRANCH_COOKIE_KEY = 'head_branch'
 
-export const getForkName = () => {
-  return getCookie(FORK_COOKIE_KEY)
-}
 export const setForkName = (val: string) => {
   setCookie(FORK_COOKIE_KEY, val)
-}
-
-export const getHeadBranch = () => {
-  return getCookie(HEAD_BRANCH_COOKIE_KEY) || 'master'
-}
-export const setHeadBranch = (val: string) => {
-  setCookie(HEAD_BRANCH_COOKIE_KEY, val)
-}
-
-const getCookie = (cookieName: string): string | undefined => {
-  return Cookies.get(cookieName)
 }
 
 const setCookie = (cookieName: string, val: string) => {
