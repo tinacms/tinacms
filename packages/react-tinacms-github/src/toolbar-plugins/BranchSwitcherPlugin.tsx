f9df0f3e75613a66bf29725496ddabb8adb7ec5f
Scott Byrne <scott.byrne@forestry.io>
2020-04-27 15:08:43 -0300

    feat: add lock icon

--- packages/react-tinacms-github/src/toolbar-plugins/BranchSwitcherPlugin.tsx
+++ packages/react-tinacms-github/src/toolbar-plugins/BranchSwitcherPlugin.tsx
@@ -11,7 +11,7 @@ import {
   FormBuilder,
   FieldsBuilder,
 } from 'tinacms'
-import { AddIcon, ChevronDownIcon } from '@tinacms/icons'
+import { AddIcon, ChevronDownIcon, LockIcon } from '@tinacms/icons'
 import { Button } from '@tinacms/styles'
 import { Dismissible } from 'react-dismissible'
 import styled, { css } from 'styled-components'
@@ -90,7 +90,7 @@ const BranchSwitcher = ({ onBranchChange }: BranchSwitcherProps) => {
                       }
                     }}
                   >
-                    {option.locked && <LockedIcon />} {option.name}
+                    {option.locked && <LockIcon />} {option.name}
                   </SelectOption>
                 ))}
               {data.filter(option => {
@@ -418,21 +418,6 @@ const SelectCurrent = styled.span`
   white-space: nowrap;
   overflow: hidden;
 `
-
-export const LockedIcon = () => (
-  <svg
-    width="32"
-    height="32"
-    viewBox="0 0 32 32"
-    fill="inherit"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      d="M25 16.5C25 15.3419 23.9909 14.4 22.75 14.4H21.625V10.25C21.625 7.35515 19.1016 5 16 5C12.8984 5 10.375 7.35515 10.375 10.25V14.4H9.25C8.00912 14.4 7 15.3419 7 16.5V23.9C7 25.0581 8.00912 26 9.25 26H22.75C23.9909 26 25 25.0581 25 23.9V16.5ZM12.625 10.25C12.625 8.5133 14.1392 7.1 16 7.1C17.8608 7.1 19.375 8.5133 19.375 10.25V14.4H12.625V10.25Z"
-      fill="inherit"
-    />
-  </svg>
-)
 
 export const BranchSwitcherPlugin = {
   __type: 'toolbar:widget',
