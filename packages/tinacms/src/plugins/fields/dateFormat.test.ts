ab9d63701bd16f3b911a800cc43739a18cbce7e2
Brent Mitton <bmitton@gatech.edu>
2020-05-19 15:04:43 -0400

    test: remove unneeded parsing tests
    * added in a quick test just to confirm that `parse` is still formatting
    correctly

--- packages/tinacms/src/plugins/fields/dateFormat.test.ts
+++ packages/tinacms/src/plugins/fields/dateFormat.test.ts
@@ -50,36 +50,22 @@ describe('date format', () => {
   })
 
   describe('parse', () => {
-    const dateString = '07 02 1972'
+    const dateString = '03 02 1972'
     const dateFormat = 'MM DD YYYY'
 
     describe('with moment input', () => {
-      it('returns correct date', () => {
+      it('returns properly formatted string', () => {
         const date = moment(dateString, dateFormat)
-        const result = parse(date, 'date', { dateFormat: 'MM YYYY' }) as Date
-
-        console.log('mmm ' + result)
-        console.log('month: ' + result.getMonth())
-        expect(result.getMonth()).toEqual(6)
-        expect(result.getFullYear()).toEqual(1972)
+        const result = format(date, 'date', { dateFormat: 'MM YYYY' })
+        expect(result).toEqual('03 1972')
       })
     })
 
     describe('with date string input', () => {
-      it('returns correct date', () => {
-        const result = parse('07/02/1992', 'date', {
-          dateFormat: 'MM/DD/YYYY', // should match date
-        }) as Date
-
-        expect(result.getMonth()).toEqual(6)
-        expect(result.getFullYear()).toEqual(1992)
-      })
-    })
-
-    describe('with non-date string input', () => {
-      it('returns input string', () => {
-        const result = parse('hello!', 'date', { dateFormat: 'MM YYYY' })
-        expect(result).toEqual('hello!')
+      it('returns properly formatted string', () => {
+        const dateString = '03 02 1972'
+        const result = format(dateString, 'date', { dateFormat: 'MM YYYY' })
+        expect(result).toEqual('03 1972')
       })
     })
   })
