6a72ce6eb215a3f2d33141e933febca5bd59708e
Brent Mitton <bmitton@gatech.edu>
2020-05-19 13:01:19 -0400

    fix: we weren't handling time formatting in any way

--- packages/tinacms/src/plugins/fields/dateFormat.ts
+++ packages/tinacms/src/plugins/fields/dateFormat.ts
@@ -1,25 +1,26 @@
 /**
 
-Copyright 2019 Forestry.io Inc
+ Copyright 2019 Forestry.io Inc
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+ http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
 
-*/
+ */
 
 import moment from 'moment'
 import { DatetimepickerProps } from 'react-datetime'
 
 const DEFAULT_DATE_DISPLAY_FORMAT = 'MMM DD, YYYY'
+const DEFAULT_TIME_DISPLAY_FORMAT = 'h:mm A'
 
 // Format date for display
 export const format = (
@@ -27,16 +28,15 @@ export const format = (
   _name: string,
   field: DatetimepickerProps
 ): string => {
-  const dateFormat =
-    typeof field.dateFormat === 'string'
-      ? field.dateFormat
-      : DEFAULT_DATE_DISPLAY_FORMAT
+  const dateFormat = parseDateFormat(field.dateFormat)
+  const timeFormat = parseTimeFormat(field.timeFormat)
+  const combinedFormat = `${dateFormat} ${timeFormat}`
 
   if (typeof val === 'string') {
-    const date = moment(val) // TODO Take in export date here once it's configurable
-    return date.isValid() ? date.format(dateFormat) : val
+    const date = moment(val)
+    return date.isValid() ? date.format(combinedFormat) : val
   }
-  return moment(val).format(dateFormat)
+  return val.format(combinedFormat)
 }
 
 // Format datepicker value for export
@@ -45,13 +45,21 @@ export const parse = (
   _name: string,
   field: DatetimepickerProps
 ) => {
-  const dateFormat =
-    typeof field.dateFormat === 'string'
-      ? field.dateFormat
-      : DEFAULT_DATE_DISPLAY_FORMAT
-  if (typeof val === 'string') {
-    const date = moment(val, dateFormat)
-    return date.isValid() ? date.toDate() : val
+  return format(val, _name, field)
+}
+
+function parseDateFormat(format: string | boolean | undefined) {
+  if (typeof format === 'string') {
+    return format
   }
-  return val.toDate()
+  return DEFAULT_DATE_DISPLAY_FORMAT
+}
+
+function parseTimeFormat(format: string | boolean | undefined) {
+  if (typeof format === 'string') {
+    return format
+  } else if (format) {
+    return DEFAULT_TIME_DISPLAY_FORMAT
+  }
+  return ''
 }
