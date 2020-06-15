a494f8f94cb7480397b741b184689391f24f6424
Joel Huggett <joel@forestry.io>
2020-06-15 14:19:02 -0300

    feat: reworked api calls to work with the new auth flow

--- packages/next-tinacms-github/src/next/preview.ts
+++ packages/next-tinacms-github/src/next/preview.ts
@@ -16,19 +16,37 @@ limitations under the License.
 
 */
 
-import {
-  ACCESS_TOKEN_KEY,
-  WORKING_REPO_KEY,
-  HEAD_BRANCH_KEY,
-} from '../constants'
+import { CSRF_TOKEN_KEY, WORKING_REPO_KEY, HEAD_BRANCH_KEY } from '../constants'
+import { AES, enc } from 'crypto-js'
 
-export const previewHandler = (req: any, res: any) => {
-  const previewData = {
-    working_repo_full_name: req.cookies[WORKING_REPO_KEY],
-    github_access_token: req.cookies[ACCESS_TOKEN_KEY],
-    head_branch:
-      req.cookies[HEAD_BRANCH_KEY] || process.env.BASE_BRANCH || 'master',
+export const previewHandler = (signingKey: string) => (req: any, res: any) => {
+  const expectedCSRFToken = req.cookies[CSRF_TOKEN_KEY]
+
+  // Parse out the amalgamated token
+  const token = (req.headers['authorization'] || '').split(' ')[1] || null
+
+  var verifiedAuthToken: string | null = null
+
+  if (token && expectedCSRFToken) {
+    const decryptedToken = AES.decrypt(token, signingKey).toString(enc.Utf8)
+
+    const [csrfToken, authToken] = decryptedToken.split('.')
+
+    if (csrfToken == expectedCSRFToken) {
+      verifiedAuthToken = authToken
+    }
   }
-  res.setPreviewData(previewData)
-  res.status(200).end()
+
+  if (verifiedAuthToken) {
+    const previewData = {
+      working_repo_full_name: req.cookies[WORKING_REPO_KEY],
+      github_access_token: verifiedAuthToken,
+      head_branch:
+        req.cookies[HEAD_BRANCH_KEY] || process.env.BASE_BRANCH || 'master',
+    }
+    res.setPreviewData(previewData)
+    res.status(200).end()
+  } else {
+    res.status(400).json('Bad Credentials')
+  }
 }
