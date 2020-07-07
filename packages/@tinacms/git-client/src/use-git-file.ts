9f43ba60e394c191ee6262bf04efda3195389da9
Nolan Phillips <ncphillips.19@gmail.com>
2020-07-07 16:16:20 -0300

    chore: space

--- packages/@tinacms/git-client/src/use-git-file.ts
+++ packages/@tinacms/git-client/src/use-git-file.ts
@@ -15,7 +15,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 */
-
 import { useMemo } from 'react'
 import { useCMS } from '@tinacms/react-core'
 import { GitFile } from './git-file'
