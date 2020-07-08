c9c70fbdc9da1c7b74e9d2aad733688a1d3946f7
logan-anderson <landerson2@upei.ca>
2020-07-08 16:09:25 -0300

    chore: added file header

--- packages/react-tinacms-github/src/toolbar-plugins/createMarkdownDeleteAction.tsx
+++ packages/react-tinacms-github/src/toolbar-plugins/createMarkdownDeleteAction.tsx
@@ -1,3 +1,21 @@
+/**
+
+Copyright 2019 Forestry.io Inc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+
 import * as React from 'react'
 import {
   ActionButton,
@@ -16,12 +34,12 @@ const getTitleDefault = (form: Form) => {
 }
 
 interface options {
-  getTitle: (form: Form) => string
-  getFilePath: (form: Form) => string
+  getTitle?: (form: Form) => string
+  getFilePath?: (form: Form) => string
 }
 
 export const createMarkdownDeleteAction = (
-  { getTitle, getFilePath }: options = {
+  { getTitle = getTitleDefault, getFilePath = getTitleDefault }: options = {
     getTitle: getTitleDefault,
     getFilePath: getTitleDefault,
   }
@@ -31,8 +49,8 @@ export const createMarkdownDeleteAction = (
     const [active, setActive] = React.useState(false)
     const open = () => setActive(true)
     const close = () => setActive(false)
-    const title = getTitle(form)
-    const filePath = getFilePath(form)
+    const title = getTitle!(form)
+    const filePath = getFilePath!(form)
 
     return (
       <div>
