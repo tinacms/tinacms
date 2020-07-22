27e0452f3efec05bbba0063a7e415c45f2d7bd20
Brent Mitton <bmitton@gatech.edu>
2020-07-22 10:54:30 -0400

    chore: uploadFile into class & strapiUrl in constructor

--- packages/react-tinacms-strapi/src/strapi-media.store.ts
+++ packages/react-tinacms-strapi/src/strapi-media.store.ts
@@ -22,19 +22,42 @@ import Cookies from 'js-cookie'
 import { STRAPI_JWT } from './tina-strapi-client'
 
 export class StrapiMediaStore {
+  strapiUrl: string
   accept = '*'
+
+  constructor(strapiUrl: string) {
+    this.strapiUrl = strapiUrl
+  }
 
   async persist(files: MediaUploadOptions[]): Promise<Media[]> {
     const uploaded: Media[] = []
 
     for (const { file } of files) {
-      const upload = await uploadFile(file)
+      const upload = await this.uploadFile(file)
       uploaded.push({
         directory: '/uploads',
         filename: upload[0].hash + upload[0].ext + `?${upload[0].id}`,
       })
     }
     return uploaded
+  }
+
+  async uploadFile(file: File) {
+    const authToken = Cookies.get(STRAPI_JWT)
+    const formData = new FormData()
+    formData.append('files', file)
+    const uploadResponse: Response = await fetch(`${this.strapiUrl}/upload`, {
+      method: 'post',
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
+      body: formData,
+    })
+
+    if (uploadResponse.status != 200) {
+      throw Error(uploadResponse.statusText)
+    }
+    return uploadResponse.json()
   }
 
   getFilePath(fileUrl: string): string {
@@ -48,25 +71,4 @@ export class StrapiMediaStore {
   getAbsolutePath(fileUrl: string): string {
     return process.env.STRAPI_URL + fileUrl
   }
-}
-
-export async function uploadFile(file: File) {
-  const authToken = Cookies.get(STRAPI_JWT)
-  const formData = new FormData()
-  formData.append('files', file)
-  const uploadResponse: Response = await fetch(
-    process.env.STRAPI_URL + '/upload',
-    {
-      method: 'post',
-      headers: {
-        Authorization: `Bearer ${authToken}`,
-      },
-      body: formData,
-    }
-  )
-
-  if (uploadResponse.status != 200) {
-    throw Error(uploadResponse.statusText)
-  }
-  return uploadResponse.json()
 }
