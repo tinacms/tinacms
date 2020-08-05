ae50da8252aff2878097b5bdbd41f410ca9e7c05
Nolan Phillips <ncphillips.19@gmail.com>
2020-08-05 15:26:04 -0300

    test(next-tinacms-github): missing csrf responds with 401

--- packages/next-tinacms-github/src/next/preview.test.ts
+++ packages/next-tinacms-github/src/next/preview.test.ts
@@ -1,11 +1,12 @@
 import { previewHandler } from './preview'
+import { ReqStub, ResStub } from '../test-helpers'
 
 describe('previewHandler', () => {
   describe('without a signing key', () => {
     it('responds with status 500', async () => {
       // @ts-ignore That's the point of the test.
       const handler = previewHandler(undefined)
-      const req = {}
+      const req = new ReqStub()
       const res = new ResStub()
 
       await handler(req, res)
@@ -15,7 +16,7 @@ describe('previewHandler', () => {
     it('sends JSON response', async () => {
       // @ts-ignore That's the point of the test.
       const handler = previewHandler(undefined)
-      const req = {}
+      const req = new ReqStub()
       const res = new ResStub()
 
       await handler(req, res)
@@ -23,9 +24,15 @@ describe('previewHandler', () => {
       expect(res.json).toHaveBeenCalled()
     })
   })
-})
+  describe('when CSRF token is not in the request cookies ', () => {
+    it('responds with status 401', async () => {
+      const handler = previewHandler('example-signing-key')
+      const req = new ReqStub()
+      const res = new ResStub()
 
-class ResStub {
-  status = jest.fn(() => this)
-  json = jest.fn()
-}
+      await handler(req, res)
+
+      expect(res.status).toHaveBeenCalledWith(401)
+    })
+  })
+})
