688e8e038920dd2061ca76e5872bbee0cd29d1aa
Nolan Phillips <ncphillips.19@gmail.com>
2020-08-05 11:20:03 -0300

    fix(next-tinacms-github): auth handler sends 500 error when missing signing key

---
+++ packages/next-tinacms-github/src/github/create-auth-handler.test.ts
@@ -0,0 +1,31 @@
+import { createAuthHandler } from './create-auth-handler'
+
+describe('createAuthHandler', () => {
+  describe('without a signing key', () => {
+    it('responds with status 500', async () => {
+      // @ts-ignore That's the point of the test.
+      const handler = createAuthHandler('test', 'test', undefined)
+      const req = {}
+      const res = new ResStub()
+
+      await handler(req, res)
+
+      expect(res.status).toHaveBeenCalledWith(500)
+    })
+    it('sends JSON response', async () => {
+      // @ts-ignore That's the point of the test.
+      const handler = createAuthHandler('test', 'test', undefined)
+      const req = {}
+      const res = new ResStub()
+
+      await handler(req, res)
+
+      expect(res.json).toHaveBeenCalled()
+    })
+  })
+})
+
+class ResStub {
+  status = jest.fn(() => this)
+  json = jest.fn()
+}
