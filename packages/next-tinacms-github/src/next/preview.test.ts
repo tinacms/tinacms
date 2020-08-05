7458c8fa3f5dc0adcdd970802af13b628f7cc3d8
Nolan Phillips <ncphillips.19@gmail.com>
2020-08-05 15:33:58 -0300

    test: updated test list

--- packages/next-tinacms-github/src/next/preview.test.ts
+++ packages/next-tinacms-github/src/next/preview.test.ts
@@ -24,15 +24,39 @@ describe('previewHandler', () => {
       expect(res.json).toHaveBeenCalled()
     })
   })
-  describe('when CSRF token is not in the request cookies ', () => {
-    it('responds with status 401', async () => {
-      const handler = previewHandler('example-signing-key')
-      const req = new ReqStub()
-      const res = new ResStub()
+  describe('with a signing key', () => {
+    describe('when CSRF token is not in the request cookies ', () => {
+      it('responds with status 401', async () => {
+        const handler = previewHandler('example-signing-key')
+        const req = new ReqStub()
+        const res = new ResStub()
 
-      await handler(req, res)
+        await handler(req, res)
 
-      expect(res.status).toHaveBeenCalledWith(401)
+        expect(res.status).toHaveBeenCalledWith(401)
+      })
+    })
+    describe('when CSRF token is in the request cookies', () => {
+      describe('when "authorization" token is not in headers', () => {
+        it('responds with status 401', async () => {
+          const handler = previewHandler('example-signing-key')
+          const req = new ReqStub()
+          const res = new ResStub()
+
+          await handler(req, res)
+
+          expect(res.status).toHaveBeenCalledWith(401)
+        })
+      })
+      describe('when "authorization" token is in headers', () => {
+        describe('when CSRF token is valid', () => {
+          it.skip('sets preview data', () => {})
+          it.skip('responds with status 200', () => {})
+        })
+        describe('when CSRF token is invalid', () => {
+          it.skip('responds with status 401', () => {})
+        })
+      })
     })
   })
 })
