180dd77deb7507b0e260d679f32b5f9abb423e03
Nolan Phillips <ncphillips.19@gmail.com>
2020-09-10 14:23:22 -0300

    chore: add "type" to media instances

--- packages/react-tinacms-github/src/github-media-store/index.ts
+++ packages/react-tinacms-github/src/github-media-store/index.ts
@@ -40,6 +40,7 @@ export class GithubMediaStore implements MediaStore {
         await this.githubClient.upload(path, content, 'Upload', true)
 
         uploaded.push({
+          type: 'file',
           directory: directory,
           filename: file.name,
         })
