c02a877a6301c8b4edf681f9bcc154a4abc28802
Kendall Strautman <kendallstrautman@gmail.com>
2020-09-14 16:18:35 -0600

    chore(gatsby-tinacms-remark): update cms type to TinaCMS

--- packages/gatsby-tinacms-remark/src/remark-creator-plugin.ts
+++ packages/gatsby-tinacms-remark/src/remark-creator-plugin.ts
@@ -17,7 +17,7 @@ limitations under the License.
 */
 
 import { toMarkdownString } from './to-markdown'
-import { CMS, Field, AddContentPlugin } from 'tinacms'
+import { TinaCMS, Field, AddContentPlugin } from 'tinacms'
 
 type MaybePromise<T> = Promise<T> | T
 
@@ -77,7 +77,7 @@ export class RemarkCreatorPlugin<FormShape = any, FrontmatterShape = any>
     this.body = options.body || (() => '')
   }
 
-  async onSubmit(form: FormShape, cms: CMS) {
+  async onSubmit(form: FormShape, cms: TinaCMS) {
     const fileRelativePath = await this.filename(form)
     const rawFrontmatter = await this.frontmatter(form)
     const rawMarkdownBody = await this.body(form)
