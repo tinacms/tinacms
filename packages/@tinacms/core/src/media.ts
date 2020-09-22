6da2959d988dd5eae5c330e5e051bc670455e81f
Nolan Phillips <ncphillips.19@gmail.com>
2020-09-22 14:20:43 -0300

    chore: 2nd and 3rd previewSrc args are optionsal

--- packages/@tinacms/core/src/media.ts
+++ packages/@tinacms/core/src/media.ts
@@ -83,7 +83,7 @@ export interface MediaStore {
    * Given a `src` string it returns a url for previewing that content.
    * This is helpful in cases where the file may not be available in production yet.
    */
-  previewSrc(src: string, fieldPath: string, formValues: any): Promise<string>
+  previewSrc(src: string, fieldPath?: string, formValues?: any): Promise<string>
 
   /**
    * Lists all media in a specific directory.
