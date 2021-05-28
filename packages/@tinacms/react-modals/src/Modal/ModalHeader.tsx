b27f72b48c8a3f0c68a6c82ce1ac1fafca003292
jbtleloup <jb.t.leloup@gmail.com>
2021-05-28 12:50:34 -0400

    LUC-366 Add tabs to screen manager

--- packages/@tinacms/react-modals/src/Modal/ModalHeader.tsx
+++ packages/@tinacms/react-modals/src/Modal/ModalHeader.tsx
@@ -1,35 +1,58 @@
 /**
 
-Copyright 2021 Forestry.io Holdings, Inc.
+ Copyright 2021 Forestry.io Holdings, Inc.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+ http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
 
-*/
+ */
 
 import * as React from 'react'
 import styled from 'styled-components'
 import { CloseIcon } from '@tinacms/icons'
+import { useState } from 'react'
 
 export interface ModalHeaderProps {
   children: React.ReactChild | React.ReactChild[]
   close?(): void
+  updateCurrentTab?: (tabNumber: number) => void
+  allTabs?: string[]
 }
 
+const StyledTab = styled.button<{isActive: boolean}>`
+  border: none;
+  border-bottom: ${(props) => (props.isActive && '2px solid cornflowerblue')};
+  cursor: pointer;
+`
+
 export const ModalHeader = styled(
-  ({ children, close, ...styleProps }: ModalHeaderProps) => {
+  ({ children, close, updateCurrentTab, allTabs, ...styleProps }: ModalHeaderProps) => {
+    const [currentTab, setCurrentTab] = useState(0)
+    const handleClick = (idx: number) => {
+      setCurrentTab(idx)
+      updateCurrentTab && updateCurrentTab(idx)
+    }
     return (
       <div {...styleProps}>
-        <ModalTitle>{children}</ModalTitle>
+        <ModalTitle>
+          {children}
+        </ModalTitle>
+        {allTabs &&
+        <div>
+          {allTabs.map((tab, i) =>
+            <StyledTab key={i} onClick={() => handleClick(i)} isActive={i === currentTab}>{tab}</StyledTab>,
+          )}
+        </div>
+        }
         {close && (
           <CloseButton onClick={close}>
             <CloseIcon />
@@ -37,7 +60,7 @@ export const ModalHeader = styled(
         )}
       </div>
     )
-  }
+  },
 )`
   height: 56px;
   display: flex;
@@ -65,10 +88,12 @@ const CloseButton = styled.div`
   fill: var(--tina-color-grey-5);
   cursor: pointer;
   transition: fill 85ms ease-out;
+
   svg {
     width: 24px;
     height: auto;
   }
+
   &:hover {
     fill: var(--tina-color-grey-8);
   }
