98fc0ef2820f41409cce293a6842a4fea4bd7320
Chris Davis <chris.enigmatical@gmail.com>
2021-07-20 13:49:47 -0500

    feat(@tinacms/field): Adds new CheckboxGroup Field
    Adds support for component: checkbox-group to Tina Forms.
    
    CheckboxGroupField is an array of options that allow for multiple values (as opposed to Select or RadioGroup which only allows for a single value from options).
    
    On submission, CheckboxGroupField submits as an array of each checked option.

--- packages/@tinacms/toolkit/src/packages/icons/CircleCheck.tsx
+++ packages/@tinacms/toolkit/src/packages/icons/CircleCheck.tsx
@@ -16,17 +16,17 @@ limitations under the License.
 
 */
 
-export {
-  BlocksFieldPlugin,
-  ColorFieldPlugin,
-  GroupFieldPlugin,
-  GroupListFieldPlugin,
-  NumberFieldPlugin,
-  SelectFieldPlugin,
-  RadioGroupFieldPlugin,
-  TextFieldPlugin,
-  TextareaFieldPlugin,
-  ToggleFieldPlugin,
-  DateFieldPlugin,
-  CheckboxGroupFieldPlugin,
-} from '../../packages/fields'
+import * as React from 'react'
+
+export const CircleCheck = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    fill="currentColor"
+    className="bi bi-check-circle-fill"
+    viewBox="0 0 16 16"
+  >
+    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+  </svg>
+)
