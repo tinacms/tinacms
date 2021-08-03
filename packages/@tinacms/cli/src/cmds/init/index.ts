a5714af6cf1cb929dbaf425b4e25c325d2c4f247
Logan Anderson <landerson2@upei.ca>
2021-08-03 15:39:20 -0300

    added regex to add css files to generated file

--- packages/@tinacms/cli/src/cmds/init/index.ts
+++ packages/@tinacms/cli/src/cmds/init/index.ts
@@ -60,10 +60,10 @@ export async function installDeps(ctx: any, next: () => void, options) {
     prog: '',
   })
   const installCMD = `yarn add ${deps.join(' ')}`
-  await execShellCommand(installCMD)
+  // await execShellCommand(installCMD)
 
   // Fake installed used for dev
-  // await delay(2000)
+  await delay(2000)
   bar.tick({
     prog: '✅',
   })
@@ -105,24 +105,39 @@ export async function tinaSetup(ctx: any, next: () => void, options) {
   if (!fs.pathExistsSync(appPath) && !fs.pathExistsSync(appPathTS)) {
     // if they don't have a _app.js or an _app.tsx just make one
     logger.info(logText('Adding _app.js ... ✅'))
-    fs.writeFileSync(appPath, AppJsContent)
+    fs.writeFileSync(appPath, AppJsContent())
   } else {
     // Ask the user if they want to update there _app.js
     const override = await prompts({
       name: 'res',
       type: 'confirm',
-      message: `do you want us to override your _app${appExtension}?`,
+      message: `do you want us to ${chalk.bold(
+        `override`
+      )} your _app${appExtension}?`,
     })
     if (override.res) {
       logger.info(logText(`Adding _app${appExtension} ... ✅`))
       const appPathWithExtension = p.join(pagesPath, `_app${appExtension}`)
-      fs.writeFileSync(appPathWithExtension, AppJsContent)
+      const fileContent = fs.pathExistsSync(appPath)
+        ? readFileSync(appPath)
+        : readFileSync(appPathTS)
+      console.log({ fileContent: fileContent.toString() })
+      const matches = [
+        ...fileContent.toString().matchAll(/^.*import.*\.css".*$/gm),
+      ]
+      console.log({
+        matches,
+      })
+      console.log({
+        content: AppJsContent(matches.join('/n')),
+      })
+      fs.writeFileSync(appPathWithExtension, AppJsContent(matches.join('/n')))
     } else {
       wrapper = true
       logger.info(
         dangerText(
           `Heads up, to enable live-editing you'll need to wrap your page or site in Tina:\n`,
-          warnText(AppJsContent)
+          warnText(AppJsContent())
         )
       )
     }
