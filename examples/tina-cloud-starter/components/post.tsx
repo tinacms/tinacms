2b15e491344ddc395d418310b30f5033f9634e6f
Jack Bevis <jack.bevis@forestry.io>
2021-08-02 18:13:14 -0600

    update tests

--- examples/tina-cloud-starter/components/post.tsx
+++ examples/tina-cloud-starter/components/post.tsx
@@ -20,8 +20,9 @@ export const Post = ({ data }) => {
     yellow:
       "from-yellow-400 to-yellow-500 dark:from-yellow-300 dark:to-yellow-500",
   };
-  const date = Date.parse(data.date)
-  const formattedDate = format(date, 'MMM dd, yyyy')
+  const date = new Date(data.date)
+  const dateUTC = date.valueOf() + date.getTimezoneOffset() * 60 * 1000
+  const formattedDate = format(dateUTC, 'MMM dd, yyyy')
 
   return (
     <Section className="flex-1">
