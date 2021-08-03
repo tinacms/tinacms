7a277cb852f59d712361bbfe5cbbdc1f3aef3bfe
Jack Bevis <jack.bevis@forestry.io>
2021-08-03 08:45:28 -0600

    testing tweaks

--- packages/@tinacms/graphql/src/primitives/resolver/index.ts
+++ packages/@tinacms/graphql/src/primitives/resolver/index.ts
@@ -648,7 +648,7 @@ const resolveDateInput = (
   if (!isValid(date)) {
     throw 'Invalid Date'
   }
-  
+
   const dateUTC = new Date(
     date.valueOf() + date.getTimezoneOffset() * 60 * 1000
   )
