43f5c5747962f4ae9013024559c7b745cb4f2d58
Logan Anderson <landerson2@upei.ca>
2021-08-05 12:43:56 -0300

    fix for markdown image plugin

--- examples/tina-cloud-starter/pages/_app.js
+++ examples/tina-cloud-starter/pages/_app.js
@@ -3,7 +3,7 @@ import dynamic from "next/dynamic";
 import { TinaEditProvider } from "tinacms/dist/edit-state";
 import { Layout } from "../components/layout";
 const TinaCMS = dynamic(() => import("tinacms"), { ssr: false });
-// import { TinaCloudCloudinaryMediaStore } from "next-tinacms-cloudinary";
+import { TinaCloudCloudinaryMediaStore } from "next-tinacms-cloudinary";
 
 const NEXT_PUBLIC_TINA_CLIENT_ID = process.env.NEXT_PUBLIC_TINA_CLIENT_ID;
 const NEXT_PUBLIC_USE_LOCAL_CLIENT =
@@ -19,6 +19,7 @@ const App = ({ Component, pageProps }) => {
             branch="main"
             clientId={NEXT_PUBLIC_TINA_CLIENT_ID}
             isLocalClient={Boolean(Number(NEXT_PUBLIC_USE_LOCAL_CLIENT))}
+            mediaStore={TinaCloudCloudinaryMediaStore}
             cmsCallback={(cms) => {
               import("react-tinacms-editor").then(({ MarkdownFieldPlugin }) => {
                 cms.plugins.add(MarkdownFieldPlugin);
