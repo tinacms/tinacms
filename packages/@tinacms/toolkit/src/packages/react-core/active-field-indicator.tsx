54017319fa9cd2559b3eaeefce31758df43e7aeb
DJ <dj@forestry.io>
2021-08-24 16:00:46 -0400

    add indicator to highlight off-screen elements

--- packages/@tinacms/toolkit/src/packages/react-core/active-field-indicator.tsx
+++ packages/@tinacms/toolkit/src/packages/react-core/active-field-indicator.tsx
@@ -1,10 +1,54 @@
 import * as React from 'react'
+import styled from 'styled-components'
+import { ChevronUpIcon, ChevronDownIcon } from '../icons'
 
 const FOCUS_EVENT = 'tina:activefield'
 
 export const setActiveField = (field: string | null) => {
   const fieldEvent = new CustomEvent(FOCUS_EVENT, { detail: field })
   window.dispatchEvent(fieldEvent)
+}
+
+const IndicatorWrap = styled.div`
+  position: fixed;
+  z-index: var(--tina-z-index-3);
+  left: 0;
+  padding: 0.5rem 0;
+  margin-left: var(--tina-sidebar-width);
+  width: calc(100% - var(--tina-sidebar-width));
+  text-align: center;
+  top: ${(props: { position?: 'top' | 'bottom' }) =>
+    props.position === 'top' ? 0 : 'auto'};
+  bottom: ${(props: { position?: 'top' | 'bottom' }) =>
+    props.position === 'top' ? 'auto' : 0};
+`
+
+const ArrowWrap = styled.div`
+  display: inline-block;
+  fill: white;
+  background-color: var(--tina-color-primary);
+  border-radius: 50%;
+  box-shadow: 0 0 10px -5px;
+`
+
+const AboveViewportIndicator = () => {
+  return (
+    <IndicatorWrap position="top">
+      <ArrowWrap>
+        <ChevronUpIcon />
+      </ArrowWrap>
+    </IndicatorWrap>
+  )
+}
+
+const BelowViewportIndicator = () => {
+  return (
+    <IndicatorWrap position="bottom">
+      <ArrowWrap>
+        <ChevronDownIcon />
+      </ArrowWrap>
+    </IndicatorWrap>
+  )
 }
 
 export const ActiveFieldIndicator = () => {
@@ -26,11 +70,27 @@ export const ActiveFieldIndicator = () => {
 
   const { top, left, width, height } = activeEle.getBoundingClientRect()
 
+  const eleTopY = top + window.scrollY
+  const eleBottomY = top + height + window.scrollY
+  const viewportTopY = window.scrollY
+  const viewportBottomY = window.innerHeight + window.scrollY
+
+  if (eleTopY > viewportBottomY) {
+    // element is below the viewport
+    return <BelowViewportIndicator />
+  }
+
+  if (eleBottomY < viewportTopY) {
+    // element is above the viewport
+    return <AboveViewportIndicator />
+  }
+
   return (
     <div
       style={{
         position: 'absolute',
-        zIndex: 999,
+        // @ts-ignore it's a number, trust me
+        zIndex: `var(--tina-z-index-3)`,
         top: top + window.scrollY,
         left: left + window.scrollX,
         width,
