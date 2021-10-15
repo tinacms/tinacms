b775354955bd47e6c187b840adeb52f619d7601f
Chris Davis <chris.enigmatical@gmail.com>
2021-10-15 10:43:03 -0500

    Adds useGetCollection hook and GetCollection to Collection List Page

---
+++ packages/tinacms/src/admin/hooks/useGetCollection.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react'
+
+const useGetCollection = (cms, collectionName) => {
+  const [collection, setCollection] = useState(undefined)
+
+  useEffect(() => {
+    const fetchCollection = async () => {
+      const response = await cms.api.tina.request(
+        `query($collection: String!){getCollection(collection: $collection) { label, name } }`,
+        { variables: { collection: collectionName } }
+      )
+      setCollection(response.getCollection)
+    }
+
+    fetchCollection()
+  }, [cms])
+
+  return collection
+}
+
+export default useGetCollection
--- packages/tinacms/src/admin/pages/CollectionListPage.tsx
+++ packages/tinacms/src/admin/pages/CollectionListPage.tsx
@@ -1,17 +1,38 @@
 import React from 'react'
 import { useParams, useLocation, Link } from 'react-router-dom'
 
+import useGetCollection from '../hooks/useGetCollection'
+import GetCMS from '../components/GetCMS'
+
+const GetCollection = ({ cms, collectionName, children }) => {
+  const collection = useGetCollection(cms, collectionName)
+  if (!collection) {
+    return null
+  }
+  return <>{children(collection)}</>
+}
+
 const CollectionListPage = () => {
   const location = useLocation()
   const { collectionName } = useParams()
 
   return (
-    <div>
-      <h1>Collection List Page</h1>
-      <h2>{collectionName}</h2>
-      <br />
-      <Link to={`${location.pathname}/create`}>&raquo; Create New</Link>
-    </div>
+    <GetCMS>
+      {(cms) => (
+        <GetCollection cms={cms} collectionName={collectionName}>
+          {(collection) => (
+            <div>
+              <h1>Collection List Page</h1>
+              <h2>
+                {collection.name} {collection.label}
+              </h2>
+              <br />
+              <Link to={`${location.pathname}/create`}>&raquo; Create New</Link>
+            </div>
+          )}
+        </GetCollection>
+      )}
+    </GetCMS>
   )
 }
 
