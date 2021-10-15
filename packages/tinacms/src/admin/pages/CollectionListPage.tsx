3777da068d57c19f87a9c6b4ee5d667a53b641eb
Scott Byrne <scott.byrne@forestry.io>
2021-10-15 11:44:13 -0300

    base styles

--- packages/tinacms/src/admin/pages/CollectionListPage.tsx
+++ packages/tinacms/src/admin/pages/CollectionListPage.tsx
@@ -6,11 +6,13 @@ const CollectionListPage = () => {
   const { collectionName } = useParams()
 
   return (
-    <div>
-      <h1>Collection List Page</h1>
-      <h2>{collectionName}</h2>
-      <br />
-      <Link to={`${location.pathname}/create`}>&raquo; Create New</Link>
+    <div className="w-full flex justify-between pt-4">
+      <h3 className="text-4xl">{collectionName}</h3>
+      <Link to={`${location.pathname}/create`} passHref>
+        <a className="inline-flex items-center px-6 py-3 border border-transparent text-base leading-5 font-medium rounded-full text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue focus:border-blue-700 active:bg-blue-700 transition duration-150 ease-in-out">
+          Create New
+        </a>
+      </Link>
     </div>
   )
 }
