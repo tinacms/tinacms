22c4be50dbd4bd7790792b957fa50dedaa5be487
Scott Byrne <scott.byrne@forestry.io>
2021-10-21 16:11:19 -0300

    style: branding

--- packages/tinacms/src/admin/index.tsx
+++ packages/tinacms/src/admin/index.tsx
@@ -31,6 +31,7 @@ import CollectionCreatePage from './pages/CollectionCreatePage'
 import CollectionUpdatePage from './pages/CollectionUpdatePage'
 
 import useEmbedTailwind from './hooks/useEmbedTailwind'
+import { TinaIcon } from '@tinacms/toolkit'
 
 export const TinaAdmin = () => {
   useEmbedTailwind()
@@ -48,10 +49,24 @@ export const TinaAdmin = () => {
             <Layout>
               <Router>
                 <div className="flex items-stretch h-screen overflow-hidden">
-                  <div className="flex flex-col w-64 md:w-80 lg:w-96 flex-shrink-0 bg-gradient-to-b from-white to-gray-50 border-r border-gray-200">
-                    <div className="px-6 py-4 border-b border-gray-200">
-                      <h2 className="text-2xl tracking-wide">
-                        <Link to={`/admin`}>Tina Admin</Link>
+                  <div className="flex flex-col w-64 md:w-80 flex-shrink-0 bg-gradient-to-b from-white to-gray-50 border-r border-gray-150">
+                    <div className="px-5 py-4 border-b border-gray-150">
+                      <h2 className="text-2xl tracking-wide text-gray-700">
+                        <Link
+                          to={`/admin`}
+                          className="flex items-center gap-0.5 hover:opacity-70 transition-opacity ease-out duration-150"
+                        >
+                          <svg
+                            viewBox="0 0 32 32"
+                            fill="#EC4815"
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="w-10 h-auto"
+                          >
+                            <path d="M18.6466 14.5553C19.9018 13.5141 20.458 7.36086 21.0014 5.14903C21.5447 2.9372 23.7919 3.04938 23.7919 3.04938C23.7919 3.04938 23.2085 4.06764 23.4464 4.82751C23.6844 5.58738 25.3145 6.26662 25.3145 6.26662L24.9629 7.19622C24.9629 7.19622 24.2288 7.10204 23.7919 7.9785C23.355 8.85496 24.3392 17.4442 24.3392 17.4442C24.3392 17.4442 21.4469 22.7275 21.4469 24.9206C21.4469 27.1136 22.4819 28.9515 22.4819 28.9515H21.0296C21.0296 28.9515 18.899 26.4086 18.462 25.1378C18.0251 23.8669 18.1998 22.596 18.1998 22.596C18.1998 22.596 15.8839 22.4646 13.8303 22.596C11.7767 22.7275 10.4072 24.498 10.16 25.4884C9.91287 26.4787 9.81048 28.9515 9.81048 28.9515H8.66211C7.96315 26.7882 7.40803 26.0129 7.70918 24.9206C8.54334 21.8949 8.37949 20.1788 8.18635 19.4145C7.99321 18.6501 6.68552 17.983 6.68552 17.983C7.32609 16.6741 7.97996 16.0452 10.7926 15.9796C13.6052 15.914 17.3915 15.5965 18.6466 14.5553Z" />
+                            <path d="M11.1268 24.7939C11.1268 24.7939 11.4236 27.5481 13.0001 28.9516H14.3511C13.0001 27.4166 12.8527 23.4155 12.8527 23.4155C12.1656 23.6399 11.3045 24.3846 11.1268 24.7939Z" />
+                          </svg>
+                          <span>Tina Admin</span>
+                        </Link>
                       </h2>
                     </div>
                     <div className="px-6 py-7">
