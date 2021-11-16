fa9a96d3df8c0e13584f0c866f6a3edf126189ae
Logan Anderson <landerson2@upei.ca>
2021-11-16 14:08:39 -0400

    update script for updateing deps

--- scripts/transformDeps.sh
+++ scripts/transformDeps.sh
@@ -1,4 +1,9 @@
-git checkout -b examples
+# this is ment to be run localy 
+git fetch
+git pull origin main
+git pull origin examples
+git merge origin main
+git checkout examples --
 yarn swapDeps
 git commit -am "update deps"
 git push origin examples
