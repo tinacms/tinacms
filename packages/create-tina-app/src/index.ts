454edc7ff91e437035e6b743a3bd48efa7eab3c3
Logan Anderson <landerson2@upei.ca>
2021-11-18 15:47:12 -0400

    testing localy

--- packages/create-tina-app/src/index.ts
+++ packages/create-tina-app/src/index.ts
@@ -29,6 +29,7 @@ let projectName = ''
 program
   .version(version)
   .option('-e, --example <example>', 'Choose which example to start from')
+  .option('-d, --dir', 'Choose which directory to run this script from')
   .arguments('[project-directory]')
   .usage(`${chalk.green('<project-directory>')} [options]`)
   .action((name) => {
@@ -38,9 +39,10 @@ program
 export const run = async () => {
   program.parse(process.argv)
   const opts = program.opts()
-  if (opts.dir) {
-    process.chdir(opts.dir)
-  }
+  // console.log({ dir: opts.dir })
+  // if (opts.dir) {
+  //   process.chdir(opts.dir)
+  // }
   const example = opts.example || 'basic'
   // const displayedCommand = useYarn ? 'yarn' : 'npm'
   const displayedCommand = 'yarn'
