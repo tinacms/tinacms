0a5fadfafd6533e3b4658e012f3a9420bd80b917
Jeff See <jeffsee.55@gmail.com>
2021-11-18 16:07:06 -0800

    Add validation to schema during compile

--- packages/@tinacms/graphql/src/primitives/spec/build-schema/index.spec.ts
+++ packages/@tinacms/graphql/src/primitives/spec/build-schema/index.spec.ts
@@ -11,27 +11,103 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import path from 'path'
-import { setup, setupFixture, setupFixture2 } from '../setup'
-import { toMatchFile } from 'jest-file-snapshot'
-import { createSchema } from '../../schema'
+import { validateSchema } from '../../schema/validate'
+import { TinaField } from '../../..'
 
-describe('The given configuration', () => {
-  it(`errors`, async () => {
-    const schema = await createSchema({
-      schema: {
-        collections: [
+const baseField: TinaField = {
+  label: 'Title',
+  name: 'myTitle',
+  type: 'string',
+}
+
+const baseCollection = {
+  label: 'Some label',
+  name: 'someName',
+  path: 'content/some-path',
+  fields: [baseField],
+}
+
+describe('The schema validation', () => {
+  it(`Throws an error for "global" templates which aren't yet supported`, async () => {
+    await expect(
+      validateSchema({
+        templates: [
           {
-            label: 'Some label',
-            name: 'someName',
-            path: 'content/some-path',
-            fields: [{ label: 'Title', name: 'title', type: 'string' }],
+            name: 'globalTemplate',
+            label: 'Global Template',
+            fields: [baseField],
           },
         ],
-      },
+
+        collections: [
+          {
+            ...baseCollection,
+            fields: 'globalTemplate',
+          },
+        ],
+      })
+    ).rejects.toThrowErrorMatchingInlineSnapshot(
+      `"Global templates are not yet supported"`
+    )
+  })
+  it(`Throws an error for field names that use title-casing`, async () => {
+    await expect(
+      validateSchema({
+        collections: [
+          {
+            ...baseCollection,
+            fields: [{ name: 'my-name', label: 'My Name', type: 'string' }],
+          },
+        ],
+      })
+    ).rejects.toThrowErrorMatchingInlineSnapshot(
+      `"Field's 'name' must match /^[a-zA-Z0-9_]*$/ at someName.my-name"`
+    )
+  })
+  it(`Throws an error for field types that don't exist`, async () => {
+    await expect(
+      validateSchema({
+        collections: [
+          {
+            ...baseCollection,
+            // @ts-ignore
+            fields: [{ ...baseField, type: 'some-type' }],
+          },
+        ],
+      })
+    ).rejects.toThrowErrorMatchingInlineSnapshot(
+      `"'type' must be one of: string, number, boolean, datetime, image, reference, object, rich-text, but got 'some-type' at someName.myTitle"`
+    )
+  })
+  it(`Trims the "collection.path" missing`, async () => {
+    await expect(
+      validateSchema({
+        collections: [
+          {
+            ...baseCollection,
+            path: undefined,
+          },
+        ],
+      })
+    ).rejects.toThrowErrorMatchingInlineSnapshot(`"path is a required field"`)
+  })
+  it(`Trims the "collection.path" configuration automatically`, async () => {
+    const validSchema = await validateSchema({
+      collections: [
+        {
+          ...baseCollection,
+          path: 'some/path/',
+        },
+      ],
     })
-    // todo validate schema
-    console.log(schema.schema)
-    expect(true).toBe(true)
+
+    expect(validSchema).toMatchObject({
+      collections: [
+        {
+          ...baseCollection,
+          path: 'some/path',
+        },
+      ],
+    })
   })
 })
