064b8e802332d258d2c581cff1c56bc24a84fa9c
Jeff See <jeffsee.55@gmail.com>
2021-12-14 14:18:28 -0800

    Move datalayer into its own package

---
+++ packages/@tinacms/datalayer/src/database/store/memory.ts
@@ -0,0 +1,98 @@
+/**
+Copyright 2021 Forestry.io Holdings, Inc.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import type { Store } from './index'
+import fs from 'fs-extra'
+import path from 'path'
+import { sequential } from '../../util'
+
+/**
+ * This is the bridge from whatever datasource we need for I/O.
+ * The basic example here is for the filesystem, one is needed
+ * for Github has well.
+ */
+export class MemoryStore implements Store {
+  public rootPath
+  public db
+  private map: object
+  constructor(rootPath: string, object: object = {}) {
+    this.map = object
+    this.rootPath = rootPath || ''
+    this.db = {
+      get: async (filepath: string) => {
+        return this.map[filepath]
+      },
+      put: async (filepath: string, content: object) => {
+        this.map[filepath] = content
+        await this.print()
+      },
+    }
+  }
+  public async query(queryStrings: string[], hydrator) {
+    const resultSets = await sequential(queryStrings, async (queryString) => {
+      const res = await this.get(queryString)
+      return res || []
+    })
+    let items = []
+    if (resultSets.length > 0) {
+      items = resultSets.reduce((p, c) => p.filter((e) => c.includes(e)))
+    }
+
+    return sequential(items, async (documentString) => {
+      return hydrator(documentString)
+    })
+  }
+  public async seed(filepath: string, data: object) {
+    await this.put(filepath, data)
+  }
+  public supportsSeeding() {
+    return true
+  }
+  public supportsIndexing() {
+    return true
+  }
+  public async print() {
+    // Uncomment to print
+    // await fs.outputFile(
+    //   path.join(this.rootPath, '.tina/__generated__/map.json'),
+    //   JSON.stringify(this.map, null, 2)
+    // )
+  }
+  public async clear() {
+    this.map = {}
+  }
+
+  public async glob(pattern: string, callback) {
+    const strings = Object.keys(this.map).filter((key) => {
+      if (key.startsWith(pattern)) {
+        return true
+      } else {
+        return false
+      }
+    })
+    if (callback) {
+      return sequential(strings, async (item) => {
+        return callback(item)
+      })
+    } else {
+      return strings
+    }
+  }
+  public async get(filepath: string) {
+    const content = await this.db.get(filepath)
+    return content
+  }
+  public async put(filepath: string, data: object) {
+    await this.db.put(filepath, data)
+  }
+}
---
+++ packages/@tinacms/datalayer/src/index.ts
@@ -0,0 +1,88 @@
+/**
+Copyright 2021 Forestry.io Holdings, Inc.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import fs from 'fs-extra'
+import path from 'path'
+import { buildASTSchema } from 'graphql'
+import { indexDB } from './build'
+
+export { indexDB } from './build'
+export { resolve } from './resolve'
+export { createDatabase } from './database'
+export { GithubBridge } from './database/bridge/github'
+export { GithubStore } from './database/store/github'
+export { FilesystemBridge } from './database/bridge/filesystem'
+export { FilesystemStore } from './database/store/filesystem'
+export { MemoryStore } from './database/store/memory'
+export { LevelStore } from './database/store/level'
+
+export type { GithubManagerInit } from './database/bridge/github'
+import type { Database } from './database'
+
+export const buildSchema = async (rootPath: string, database: Database) => {
+  const tempConfig = path.join(rootPath, '.tina', '__generated__', 'config')
+  const config = await fs
+    .readFileSync(path.join(tempConfig, 'schema.json'))
+    .toString()
+  await fs.rmdir(tempConfig, { recursive: true })
+  await indexDB({ database, config: JSON.parse(config) })
+  const gqlAst = await database.getGraphQLSchemaFromBridge()
+  return buildASTSchema(gqlAst)
+}
+
+import type {
+  TinaCloudSchema as TinaCloudSchemaBase,
+  TinaCloudCollection as TinaCloudCollectionBase,
+  TinaCloudTemplateBase as TinaTemplate,
+  TinaFieldBase,
+} from './types'
+
+import { Octokit } from '@octokit/rest'
+
+export const listBranches = async ({ auth, owner, repo }) => {
+  const appOctoKit = new Octokit({ auth })
+  const branchList = await appOctoKit.repos.listBranches({
+    owner,
+    repo,
+    per_page: 100,
+  })
+
+  return branchList
+}
+
+export const createBranch = async ({ auth, owner, repo, name, baseBranch }) => {
+  const appOctoKit = new Octokit({ auth })
+  const currentBranch = await appOctoKit.repos.getBranch({
+    owner,
+    repo,
+    branch: baseBranch,
+  })
+
+  const newBranch = await appOctoKit.git.createRef({
+    owner,
+    repo,
+    ref: `refs/heads/${name}`,
+    sha: currentBranch.data.commit.sha,
+  })
+
+  return newBranch
+}
+
+export type TinaCloudSchema = TinaCloudSchemaBase<false>
+// Alias to remove Cloud
+export type TinaSchema = TinaCloudSchema
+export type TinaCloudCollection = TinaCloudCollectionBase<false>
+// Alias to remove Cloud
+export type TinaCollection = TinaCloudCollectionBase<false>
+export type TinaField = TinaFieldBase
+export type { TinaTemplate }
