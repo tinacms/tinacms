c88bc5e24ea4b89221a808cf1be4cd6e22e8eb85
Chris Davis <chris.enigmatical@gmail.com>
2022-01-04 14:16:03 -0600

    2374 - Incorrect messaging on admin when in local-mode
    When in `local-mode`, `TinaAdmin` was using old messaging about logging into "Tina Cloud".
    
    Ideally, we'd have a way to know if we're in `localMode` or not and alter the messaging on the Login page, but unfortunately, our indicator of `localMode` is attached to a `cms` instance that doesn't exist before you've logged in.
    
    As a quick fix, I removed the message entirely and altered the `Log In` button's label to speak more about `edit-mode` - which exists in either scenario.

--- packages/tinacms/src/admin/pages/LoginPage.tsx
+++ packages/tinacms/src/admin/pages/LoginPage.tsx
@@ -21,7 +21,7 @@ const LoginPage = () => {
   const { setEdit } = useEditState()
   const login = () => setEdit(true)
   return (
-    <AuthTemplate message="Please log in to Tina Cloud to access your content.">
+    <AuthTemplate>
       <a
         href="/"
         className="flex-1 text-center inline-flex justify-center items-center px-8 py-3 shadow-sm text-sm leading-4 font-medium rounded-full text-gray-600 border border-gray-150 hover:opacity-80 hover:bg-gray-50 focus:outline-none focus:shadow-outline-blue  transition duration-150 ease-out"
@@ -35,7 +35,7 @@ const LoginPage = () => {
         className="flex-1 justify-center text-center inline-flex items-center px-8 py-3 shadow-sm border border-transparent text-sm leading-4 font-medium rounded-full text-white hover:opacity-80 focus:outline-none focus:shadow-outline-blue  transition duration-150 ease-out"
         style={{ background: '#0084FF' }}
       >
-        <BiLogIn className="w-6 h-auto mr-1.5 opacity-80" /> Log in
+        <BiLogIn className="w-6 h-auto mr-1.5 opacity-80" /> Enter edit-mode
       </button>
     </AuthTemplate>
   )
