9fb30dfcff606240c7615bfbd0a3c84f5b8c8ad5
Logan Anderson <landerson2@upei.ca>
2022-01-10 09:51:01 -0500

    fix https in cloudinary

--- packages/tinacms/src/tina-cms.tsx
+++ packages/tinacms/src/tina-cms.tsx
@@ -268,8 +268,14 @@ export const TinaCMSProvider2 = ({
     props.query
   }
 
+  const validOldSetup =
+    new Boolean(props?.isLocalClient) ||
+    (new Boolean(props?.clientId) && new Boolean(props?.branch))
+
+  console.log({ api: props.apiURL, validOldSetup })
+
   // branch & clientId are still supported, so don't throw if they're provided
-  if (!props.apiURL && !(props?.clientId || props?.isLocalClient)) {
+  if (!props.apiURL && !validOldSetup) {
     throw new Error(`apiURL is a required field`)
   }
 
