75287c430eef5c3426a13bad1dcde08cee97ea76
Scott Byrne <scott.byrne@forestry.io>
2022-02-02 16:56:00 -0400

    fix type error

--- packages/@tinacms/toolkit/src/packages/fields/plugins/BlocksFieldPlugin.tsx
+++ packages/@tinacms/toolkit/src/packages/fields/plugins/BlocksFieldPlugin.tsx
@@ -141,39 +141,40 @@ const Blocks = ({ tinaForm, form, field, input }: BlockFieldProps) => {
                   leaveTo="transform opacity-0 -translate-y-2"
                 >
                   <Popover.Panel className="relative overflow-hidden rounded-lg shadow-lg bg-white border border-gray-100">
-                    <div className="min-w-[192px] max-h-[24rem] overflow-y-auto flex flex-col w-full h-full">
-                      {showFilter && (
-                        <div className="sticky top-0 bg-gray-50 p-2 border-b border-gray-100 z-10">
-                          <input
-                            type="text"
-                            className="bg-white text-xs rounded-sm border border-gray-100 shadow-inner py-1 px-2 w-full block placeholder-gray-200"
-                            onClick={(event: any) => {
-                              event.stopPropagation()
-                              event.preventDefault()
-                            }}
-                            value={filter}
-                            onChange={(event: any) => {
-                              setFilter(event.target.value)
-                            }}
-                            placeholder="Filter..."
-                          />
-                        </div>
-                      )}
-                      {filteredBlocks.map(([name, template]) => (
-                        <Popover.Button as="React.Fragment">
+                    {({ close }) => (
+                      <div className="min-w-[192px] max-h-[24rem] overflow-y-auto flex flex-col w-full h-full">
+                        {showFilter && (
+                          <div className="sticky top-0 bg-gray-50 p-2 border-b border-gray-100 z-10">
+                            <input
+                              type="text"
+                              className="bg-white text-xs rounded-sm border border-gray-100 shadow-inner py-1 px-2 w-full block placeholder-gray-200"
+                              onClick={(event: any) => {
+                                event.stopPropagation()
+                                event.preventDefault()
+                              }}
+                              value={filter}
+                              onChange={(event: any) => {
+                                setFilter(event.target.value)
+                              }}
+                              placeholder="Filter..."
+                            />
+                          </div>
+                        )}
+                        {filteredBlocks.map(([name, template]) => (
                           <button
                             className="relative text-center text-xs py-2 px-4 border-l-0 border-t-0 border-r-0 border-b border-gray-50 w-full outline-none transition-all ease-out duration-150 hover:text-blue-500 focus:text-blue-500 focus:bg-gray-50 hover:bg-gray-50"
                             key={name}
                             onClick={() => {
                               addItem(name, template)
                               setFilter('')
+                              close()
                             }}
                           >
                             {template.label ? template.label : name}
                           </button>
-                        </Popover.Button>
-                      ))}
-                    </div>
+                        ))}
+                      </div>
+                    )}
                   </Popover.Panel>
                 </Transition>
               </div>
