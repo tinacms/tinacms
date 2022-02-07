b3ff06f08d6db3e888ec11bd6746e6f66964c517
James O'Halloran <james.p.ohalloran@gmail.com>
2022-02-07 09:53:21 -0400

    rename DynamicTina

--- examples/basic/.tina/components/provider/index.jsx
+++ examples/basic/.tina/components/provider/index.jsx
@@ -2,7 +2,7 @@ import dynamic from 'next/dynamic'
 const TinaProvider = dynamic(() => import('./_TinaProvider'), { ssr: false })
 import { TinaEditProvider } from 'tinacms/dist/edit-state'
 
-const Tina = ({ children }) => {
+const DynamicTina = ({ children }) => {
   return (
     <>
       <TinaEditProvider editMode={<TinaProvider>{children}</TinaProvider>}>
@@ -12,4 +12,4 @@ const Tina = ({ children }) => {
   )
 }
 
-export default Tina
+export default DynamicTina
