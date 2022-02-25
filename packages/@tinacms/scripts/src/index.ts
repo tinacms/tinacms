e2fe1e6e34f88f4f4abd569e82a52a0287e0841a
Kelly Davis <kelly.davis@forestry.io>
2022-02-25 15:13:00 -0500

    fix issues with leveldown as a peer dependency

--- packages/@tinacms/scripts/src/index.ts
+++ packages/@tinacms/scripts/src/index.ts
@@ -406,7 +406,7 @@ const buildIt = async (entryPoint, packageJSON) => {
   // @ts-ignore
   const peerDeps = packageJSON.peerDependencies
   // TODO brute force level into external
-  const external = [ 'leveldown', 'level', 'levelup', ...Object.keys({ ...deps, ...peerDeps })]
+  const external = Object.keys({ ...deps, ...peerDeps })
   const globals = {}
 
   external.forEach((ext) => (globals[ext] = 'NOOP'))
