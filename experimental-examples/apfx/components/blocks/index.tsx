23b5e2d5bb75b6707b122321238ba8e2fad7caa5
Jeff See <jeffsee.55@gmail.com>
2022-03-30 13:03:14 -0700

    Add back nav and footer on apfx

--- experimental-examples/apfx/components/blocks/index.tsx
+++ experimental-examples/apfx/components/blocks/index.tsx
@@ -151,7 +151,7 @@ export default function Home(props: HomeProps) {
       </Head>
 
       <div>
-        {/* {props.data.navigation && <Nav {...props.data.navigation} />} */}
+        {props.data.navigation && <Nav {...props.data.navigation} />}
         {props.data.page.blocks?.map((block, i) => {
           return (
             // <div key={i} data-tinaField={getTinaField(block)}>
@@ -160,7 +160,7 @@ export default function Home(props: HomeProps) {
             </div>
           )
         })}
-        {/* {props.data.footer && <Footer {...props.data.footer} />} */}
+        {props.data.footer && <Footer {...props.data.footer} />}
       </div>
     </>
   )
