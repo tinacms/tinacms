06a006cc99396070792379d30de136383bab8652
Jeff See <jeffsee.55@gmail.com>
2022-04-05 17:23:43 -0700

    Add real build scripts to each package

--- experimental-examples/tinacms.org/next.config.js
+++ experimental-examples/tinacms.org/next.config.js
@@ -56,6 +56,13 @@ const config = {
 
     return config
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 }
 
 module.exports = withBundleAnalyzer(withSvgr(config))
