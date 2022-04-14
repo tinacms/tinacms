603e0c7ce0b92f18549aa3bb193e24fa0e490112
Brent Mitton <brent.mitton+github@gmail.com>
2022-04-14 10:41:58 -0400

    more format changes

--- packages/@tinacms/cli/src/cmds/init/index.ts
+++ packages/@tinacms/cli/src/cmds/init/index.ts
@@ -272,16 +272,20 @@ export async function tinaSetup(_ctx: any, next: () => void, _options) {
 }
 
 export async function successMessage(ctx: any, next: () => void, options) {
-  logger.info(`Tina setup ${chalk.underline.green('done')} ✅`)
+  logger.info(`Tina setup ${chalk.underline.green('done')} ✅\n`)
 
   logger.info(`${chalk.bold('Run your site with Tina')}`)
-  logger.info(`  yarn dev`)
+  logger.info(`  yarn dev \n`)
+
   logger.info(`${chalk.bold('Start Editing')}`)
-  logger.info(`  Go to 'http://localhost:3000/admin'`)
+  logger.info(`  Go to 'http://localhost:3000/admin' \n`)
+
   logger.info(`${chalk.bold('Read the docs')}`)
   logger.info(
-    `  Check out 'https://tina.io/docs/introduction/tina-init/#adding-tina' for help getting started with Tina`
+    `  Check out 'https://tina.io/docs/introduction/tina-init/#adding-tina' for help getting started with Tina \n`
   )
+
+  logger.info(`Enjoy Tina! 🦙`)
 
   next()
 }
