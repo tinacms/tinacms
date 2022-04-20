04c041924b7bcc64238e9d6b7b5f5b0ed137fbca
Logan Anderson <landerson2@upei.ca>
2022-04-20 09:50:41 -0400

    update tests

--- experimental-examples/kitchen-sink/.tina/__generated__/types.ts
+++ experimental-examples/kitchen-sink/.tina/__generated__/types.ts
@@ -231,6 +231,7 @@ export type Mutation = {
   __typename?: 'Mutation';
   addPendingDocument: DocumentNode;
   updateDocument: DocumentNode;
+  deleteDocument: DocumentNode;
   createDocument: DocumentNode;
   updatePageDocument: PageDocument;
   createPageDocument: PageDocument;
@@ -250,6 +251,12 @@ export type MutationUpdateDocumentArgs = {
   collection?: Maybe<Scalars['String']>;
   relativePath: Scalars['String'];
   params: DocumentMutation;
+};
+
+
+export type MutationDeleteDocumentArgs = {
+  collection?: Maybe<Scalars['String']>;
+  relativePath: Scalars['String'];
 };
 
 
