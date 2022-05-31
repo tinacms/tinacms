97655472ff8415124b85d70957717918fd00019e
Logan Anderson <landerson2@upei.ca>
2022-05-31 12:30:24 -0400

    only use pagination when flag is on

--- examples/basic/.tina/schema.js
+++ examples/basic/.tina/schema.js
@@ -182,13 +182,10 @@ const apiURL =
     ? 'http://localhost:4001/graphql'
     : `https://content.tinajs.io/content/${process.env.NEXT_PUBLIC_TINA_CLIENT_ID}/github/${branch}`
 
+console.log({ apiURL })
 export const tinaConfig = defineConfig({
   schema,
-  apiURL,
-  cmsCallback: (cms) => {
-    cms.flags.set('tina-admin', true)
-    return cms
-  },
+  apiURL: 'http://localhost:4001/graphql',
 })
 
 export default schema
