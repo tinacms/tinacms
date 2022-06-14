faba8cc6296b77f2b791add6a697a177662cc8f2
Logan Anderson <landerson2@upei.ca>
2022-06-14 11:08:54 -0400

    update path logic

---
+++ packages/@tinacms/cli/src/utils/removeStartingAndTrailingSlash.test.ts
@@ -0,0 +1,23 @@
+import { parseMediaFolder } from './removeStartingAndTrailingSlash'
+
+describe('parseMediaFolder', () => {
+  it('removes the starting slash when it is there', () => {
+    expect(parseMediaFolder('/')).toBe('')
+
+    expect(parseMediaFolder('/foo')).toBe('foo')
+    expect(parseMediaFolder('/foo/bar')).toBe('foo/bar')
+  })
+  it('does not remove the starting slash when there is no starting slash', () => {
+    expect(parseMediaFolder('foo')).toBe('foo')
+    expect(parseMediaFolder('foo/bar')).toBe('foo/bar')
+  })
+  it('removes the trailing slash when there is one', () => {
+    expect(parseMediaFolder('foo/')).toBe('foo')
+    expect(parseMediaFolder('foo/bar/')).toBe('foo/bar')
+  })
+  it('removes the starting and trailing slash when there is both', () => {
+    expect(parseMediaFolder('//')).toBe('')
+    expect(parseMediaFolder('/foo/')).toBe('foo')
+    expect(parseMediaFolder('/foo/bar/')).toBe('foo/bar')
+  })
+})
