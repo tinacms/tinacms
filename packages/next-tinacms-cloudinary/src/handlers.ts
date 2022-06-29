2148d596da45993e5a2d2654ad576a3384890844
Jeff See <jeffsee.55@gmail.com>
2022-06-29 16:38:26 -0700

    Undo ts-ignore

--- packages/next-tinacms-cloudinary/src/handlers.ts
+++ packages/next-tinacms-cloudinary/src/handlers.ts
@@ -65,7 +65,6 @@ async function uploadMedia(req: NextApiRequest, res: NextApiResponse) {
   const upload = promisify(
     multer({
       storage: multer.diskStorage({
-        // @ts-ignore FIXME: multer was being interpretted as `any`
         directory: (req, file, cb) => {
           cb(null, '/tmp')
         },
@@ -76,7 +75,6 @@ async function uploadMedia(req: NextApiRequest, res: NextApiResponse) {
     }).single('file')
   )
 
-  // @ts-ignore multer was being interpreted as `any`
   await upload(req, res)
 
   const { directory } = req.body
