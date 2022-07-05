333dddad0870ba5593eae46d618966a217d29689
Logan Anderson <landerson2@upei.ca>
2022-07-05 15:36:55 -0400

    remove import

--- packages/@tinacms/schema-tools/src/schema/addNamespaceToSchema.ts
+++ packages/@tinacms/schema-tools/src/schema/addNamespaceToSchema.ts
@@ -10,7 +10,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-import deepclone from 'lodash.clonedeep'
 export function addNamespaceToSchema<T extends object | string>(
   maybeNode: T,
   namespace: string[] = []
