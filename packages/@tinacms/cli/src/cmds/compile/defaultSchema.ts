5ba8ba6fd26f31327b71cf15f5ecb8907fadfa58
Logan Anderson <landerson2@upei.ca>
2022-07-11 15:47:21 -0400

    update generate files

--- packages/@tinacms/cli/src/cmds/compile/defaultSchema.ts
+++ packages/@tinacms/cli/src/cmds/compile/defaultSchema.ts
@@ -12,51 +12,52 @@ limitations under the License.
 */
 
 export const defaultSchema = `
-import { defineSchema, defineConfig } from "tinacms";
+import { defineSchema, defineConfig } from 'tinacms'
 
 const schema = defineSchema({
   collections: [
     {
-      label: "Blog Posts",
-      name: "posts",
-      path: "content/posts",
+      label: 'Blog Posts',
+      name: 'post',
+      path: 'content/posts',
+      format: 'mdx',
       fields: [
         {
-          type: "string",
-          label: "Title",
-          name: "title",
+          type: 'string',
+          label: 'Title',
+          name: 'title',
         },
         {
-          type: "rich-text",
-          label: "Blog Post Body",
-          name: "body",
+          type: 'rich-text',
+          label: 'Blog Post Body',
+          name: 'body',
           isBody: true,
           templates: [
             {
-              name: "PageSection",
-              label: "Page Section",
+              name: 'PageSection',
+              label: 'Page Section',
               fields: [
                 {
-                  type: "string",
-                  name: "heading",
-                  label: "Heading",
+                  type: 'string',
+                  name: 'heading',
+                  label: 'Heading',
                 },
                 {
-                  type: "string",
-                  name: "content",
-                  label: "Content",
+                  type: 'string',
+                  name: 'content',
+                  label: 'Content',
                   ui: {
-                    component: "textarea"
-                  }
-                }
+                    component: 'textarea',
+                  },
+                },
               ],
             },
-          ]
+          ],
         },
       ],
     },
   ],
-});
+})
 
 export default schema
 
@@ -68,7 +69,7 @@ const branch = 'main'
 const apiURL =
   process.env.NODE_ENV == 'development'
     ? 'http://localhost:4001/graphql'
-    : \`https://content.tinajs.io/content/\${process.env.NEXT_PUBLIC_TINA_CLIENT_ID}/github/\${branch}\`
+    : \`https://content.tinajs.io/content/${process.env.NEXT_PUBLIC_TINA_CLIENT_ID}/github/\${branch}\`
 
 export const tinaConfig = defineConfig({
   apiURL,
@@ -80,20 +81,21 @@ export const tinaConfig = defineConfig({
     /**
      * 1. Import \`tinacms\` and \`RouteMappingPlugin\`
      **/
-    import("tinacms").then(({ RouteMappingPlugin }) => {
+    import('tinacms').then(({ RouteMappingPlugin }) => {
       /**
        * 2. Define the \`RouteMappingPlugin\` see https://tina.io/docs/tinacms-context/#the-routemappingplugin for more details
        **/
       const RouteMapping = new RouteMappingPlugin((collection, document) => {
-        return undefined;
-      });
+        return undefined
+      })
       /**
        * 3. Add the \`RouteMappingPlugin\` to the \`cms\`.
        **/
-      cms.plugins.add(RouteMapping);
-    });
+      cms.plugins.add(RouteMapping)
+    })
 
-    return cms;
+    return cms
   },
-});
+})
+
 `
