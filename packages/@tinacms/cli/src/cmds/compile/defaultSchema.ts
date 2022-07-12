668e7260fa4d59504273a5fddf3368b42cb03a31
Logan Anderson <landerson2@upei.ca>
2022-07-12 10:36:48 -0400

    fixed typo's

--- packages/@tinacms/cli/src/cmds/compile/defaultSchema.ts
+++ packages/@tinacms/cli/src/cmds/compile/defaultSchema.ts
@@ -69,7 +69,7 @@ const branch = 'main'
 const apiURL =
   process.env.NODE_ENV == 'development'
     ? 'http://localhost:4001/graphql'
-    : \`https://content.tinajs.io/content/${process.env.NEXT_PUBLIC_TINA_CLIENT_ID}/github/\${branch}\`
+    : \`https://content.tinajs.io/content/\${process.env.NEXT_PUBLIC_TINA_CLIENT_ID}/github/\${branch}\`
 
 export const tinaConfig = defineConfig({
   apiURL,
