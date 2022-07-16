c49efe68e0c022cc6246cfa85d5a8e7b804eb8cb
Jeff See <jeffsee.55@gmail.com>
2022-07-16 14:47:16 -0700

    Add licenses

--- packages/@tinacms/mdx/src/index.test.ts
+++ packages/@tinacms/mdx/src/index.test.ts
@@ -1,7 +1,27 @@
+/**
+
+Copyright 2021 Forestry.io Holdings, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+import { expect, it, describe } from 'vitest'
 import type { RichTypeInner } from '@tinacms/schema-tools'
 import { parseMDX } from './parse/index'
 import { stringifyMDX } from './stringify'
 import fs from 'fs'
+
+const test = it
 
 const field: RichTypeInner = { name: 'body', type: 'rich-text', templates: [] }
 
