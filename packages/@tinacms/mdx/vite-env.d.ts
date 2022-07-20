061ae0fdb49dc4b3ee6b7016f9472fccd3bee856
Jeff See <jeffsee.55@gmail.com>
2022-07-19 18:42:14 -0700

    cleanup

--- packages/@tinacms/mdx/vite-env.d.ts
+++ packages/@tinacms/mdx/vite-env.d.ts
@@ -2,19 +2,3 @@ declare module '*?raw' {
   const content: string
   export default content
 }
-
-declare module 'mdast' {
-  interface StaticPhrasingContentMap {
-    mdxJsxTextElement: MdxJsxTextElement
-  }
-  interface PhrasingContentMap {
-    mdxJsxTextElement: MdxJsxTextElement
-  }
-
-  interface BlockContentMap {
-    mdxJsxFlowElement: MdxJsxFlowElement
-  }
-  interface ContentMap {
-    mdxJsxFlowElement: MdxJsxFlowElement
-  }
-}
