0f268810f6fd1b098f20eff6cfe5e928aa1a67e4
Jeff See <jeffsee.55@gmail.com>
2022-07-20 07:39:19 -0700

    Add tests for mdx -> html

--- packages/@tinacms/mdx/src/tests/autotest/_runner.test.ts
+++ packages/@tinacms/mdx/src/tests/autotest/_runner.test.ts
@@ -8,8 +8,7 @@ const outputString = import.meta.glob('./*.ts', { as: 'raw' })
 setupNewTests(content, outputString, ({ name, markdownContent }) => {
   describe(`setting up ${name}`, () => {
     it(`verifies that stringifying the parsed output will match the original string`, async () => {
-      // import.meta.glob type doesn't take `raw` into consideration, so cast it to a string
-      const markdownString = (await markdownContent()) as any as string
+      const markdownString = await markdownContent()
       const astResult = parseMDX(markdownString, field, (v) => v)
       expect(stringifyMDX(astResult, field, (v) => v)).toEqual(markdownString)
       // If `expect` doesn't throw, save the file
