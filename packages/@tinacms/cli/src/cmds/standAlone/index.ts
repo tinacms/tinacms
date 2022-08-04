bfb930261f2d3422984ae51f584c467bb82e5b84
Logan Anderson <landerson2@upei.ca>
2022-08-04 15:15:20 -0400

    udpate vite plugin

--- packages/@tinacms/cli/src/cmds/standAlone/index.ts
+++ packages/@tinacms/cli/src/cmds/standAlone/index.ts
@@ -1,5 +1,6 @@
 import { viteBuild } from '@tinacms/app'
 
+// Build the app using vite
 export const viteBuildCmd = async (ctx: any, next: () => void, _options) => {
   const rootPath = ctx.rootPath
   await viteBuild({ rootPath })
