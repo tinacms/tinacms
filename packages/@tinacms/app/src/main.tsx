b6b1cea3ea3c5d95a7004e3a1371ae4d327c66f0
Logan Anderson <landerson2@upei.ca>
2022-08-04 10:19:27 -0400

    added files

---
+++ packages/@tinacms/app/src/main.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// This value is substituted by our plugin to use
+// the user's actual schema
+// @ts-ignore
+import { tinaConfig } from '../.tina/schema'
+import TinaCMS, { TinaAdmin } from 'tinacms'
+import { TinaEditProvider } from 'tinacms/dist/edit-state'
+
+const App = () => {
+  // TinaAdmin checks edit state, hence why it's still being wrapped by it
+  // we should get ride of that check
+  return (
+    <>
+      <TinaEditProvider
+        editMode={
+          // Stricter tsconfig in this package shows this error
+          <TinaCMS {...tinaConfig}>
+            <TinaAdmin />
+          </TinaCMS>
+        }
+      ></TinaEditProvider>
+    </>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
