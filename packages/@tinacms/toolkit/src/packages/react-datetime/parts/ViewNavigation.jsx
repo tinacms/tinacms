80a8bc634dbdc2e84a792ae8206036bab0c90a0e
Logan Anderson <landerson2@upei.ca>
2022-08-09 09:57:40 -0400

    added react-datetime package

---
+++ packages/@tinacms/toolkit/src/packages/react-datetime/parts/ViewNavigation.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function ViewNavigation({
+  onClickPrev,
+  onClickSwitch,
+  onClickNext,
+  switchContent,
+  switchColSpan,
+  switchProps,
+}) {
+  return (
+    <tr>
+      <th className="rdtPrev" onClick={onClickPrev}>
+        <span>‹</span>
+      </th>
+      <th
+        className="rdtSwitch"
+        colSpan={switchColSpan}
+        onClick={onClickSwitch}
+        {...switchProps}
+      >
+        {switchContent}
+      </th>
+      <th className="rdtNext" onClick={onClickNext}>
+        <span>›</span>
+      </th>
+    </tr>
+  )
+}
