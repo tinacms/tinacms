a0c9aef0d68815a3ce7473404b5e1778f330d798
Jeff See <jeffsee.55@gmail.com>
2022-08-17 12:32:24 -0700

    Add useTina hook from 'react' folder for iframe functionality

---
+++ packages/tinacms/src/react.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const useTina = <T extends object>(props: T): T => {
+  const [data, setData] = React.useState(props.data)
+  React.useEffect(() => {
+    const id = btoa(JSON.stringify({ query: props.query }))
+    parent.postMessage({ type: 'open', ...props, id })
+    window.addEventListener('message', (event) => {
+      if (event.data.id === id) {
+        console.log('child: event received')
+        setData(event.data.data)
+      }
+    })
+
+    return () => parent.postMessage({ type: 'close', id })
+  }, [])
+  return { data } as any
+}
