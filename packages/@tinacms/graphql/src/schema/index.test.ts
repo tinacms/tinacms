0c167a88fb99aad500489c652ddc4a30c8a905b1
James O'Halloran <james.p.ohalloran@gmail.com>
2022-09-13 08:51:56 -0300

    Update packages/@tinacms/graphql/src/schema/index.test.ts

--- packages/@tinacms/graphql/src/schema/index.test.ts
+++ packages/@tinacms/graphql/src/schema/index.test.ts
@@ -17,7 +17,6 @@ describe('TinaSchema', () => {
   describe('with sibling collections of similar names', () => {
     test('fetches correct collection with getCollectionByFullPath', async () => {
       const schema = createSchema()
-      console.log({ collections: schema.getCollections() })
 
       const collection1 = schema.getCollectionByFullPath('content/test/foobar')
       expect(collection1.name).toEqual('test')
