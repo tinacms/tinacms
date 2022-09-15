9d53350b186da32fe44c35c7bdb682d741421a6d
Phoenix-Alpha <betafoenix@gmail.com>
2022-09-15 22:24:57 +0800

    Update license header

--- packages/tinacms/src/hooks/use-prompt.ts
+++ packages/tinacms/src/hooks/use-prompt.ts
@@ -1,5 +1,5 @@
 /**
-Copyright 2022 Forestry.io Holdings, Inc.
+Copyright 2021 Forestry.io Holdings, Inc.
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
