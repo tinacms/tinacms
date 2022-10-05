4efb98548778e2f9fad7b2acfccd6a950581fb48
Logan Anderson <landerson2@upei.ca>
2022-10-05 14:54:32 -0400

    add basic vue example

---
+++ examples/starter-basic-iframe-vue/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <main>{{ JSON.stringify({ post: this.data }) }}</main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loading: false,
+      data: null,
+      error: null,
+    }
+  },
+  mounted() {
+    this.fetchPost()
+  },
+  methods: {
+    fetchPost() {
+      this.error = this.data = null
+      this.loading = true
+      setTimeout(() => {
+        this.data = { foo: 'bar' }
+        this.loading = false
+      }, 1000)
+    },
+  },
+}
+</script>
