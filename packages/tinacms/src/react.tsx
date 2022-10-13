1404fed77f0352d2fcb7f12fa71ab0636e1de810
Jeff See <jeffsee.55@gmail.com>
2022-10-13 13:51:30 -0700

    Specify id for unmount in useTina hook

--- packages/tinacms/src/react.tsx
+++ packages/tinacms/src/react.tsx
@@ -22,8 +22,10 @@ export function useTina<T extends object>(props: {
   data: T
 }): { data: T } {
   const [data, setData] = React.useState(props.data)
+  const id = btoa(
+    JSON.stringify({ query: props.query, variables: props.variables })
+  )
   React.useEffect(() => {
-    const id = btoa(JSON.stringify({ query: props.query }))
     parent.postMessage({ type: 'open', ...props, id }, window.location.origin)
     window.addEventListener('message', (event) => {
       if (event.data.id === id) {
@@ -34,6 +36,6 @@ export function useTina<T extends object>(props: {
 
     return () =>
       parent.postMessage({ type: 'close', id }, window.location.origin)
-  }, [])
+  }, [id])
   return { data } as any
 }
