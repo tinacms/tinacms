9f360dbbbce4513e5b94aede5a0a7bb5842b782a
Jeff See <jeffsee.55@gmail.com>
2022-10-13 13:52:19 -0700

    Add iframe version of tina-cloud-starter

---
+++ experimental-examples/tina-cloud-starter-iframe/components/blocks/content.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Container } from "../container";
+import { Section } from "../section";
+import { TinaMarkdown } from "tinacms/dist/rich-text";
+import type { TinaTemplate } from "tinacms";
+
+export const Content = ({ data, parentField = "" }) => {
+  return (
+    <Section color={data.color}>
+      <Container
+        className={`max-w-4xl prose prose-lg ${
+          data.color === "primary" ? `prose-primary` : `dark:prose-dark`
+        }`}
+        data-tinafield={`${parentField}.body`}
+        size="large"
+      >
+        <TinaMarkdown content={data.body} />
+      </Container>
+    </Section>
+  );
+};
+
+export const contentBlockSchema: TinaTemplate = {
+  name: "content",
+  label: "Content",
+  ui: {
+    previewSrc: "/blocks/content.png",
+    defaultItem: {
+      body: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.",
+    },
+  },
+  fields: [
+    {
+      type: "rich-text",
+      label: "Body",
+      name: "body",
+    },
+    {
+      type: "string",
+      label: "Color",
+      name: "color",
+      options: [
+        { label: "Default", value: "default" },
+        { label: "Tint", value: "tint" },
+        { label: "Primary", value: "primary" },
+      ],
+    },
+  ],
+};
---
+++ experimental-examples/tina-cloud-starter-iframe/pages/[filename].tsx
@@ -0,0 +1,45 @@
+import { Blocks } from "../components/blocks";
+import { client } from "../.tina/__generated__/client";
+import { useTina } from "tinacms/dist/react";
+import { Layout } from "../components/layout";
+
+export default function HomePage(
+  props: AsyncReturnType<typeof getStaticProps>["props"]
+) {
+  const { data } = useTina({
+    query: props.query,
+    variables: props.variables,
+    data: props.data,
+  });
+  return (
+    <Layout rawData={data} data={data.global}>
+      <Blocks {...data.pages} />
+    </Layout>
+  );
+}
+
+export const getStaticProps = async ({ params }) => {
+  const tinaProps = await client.queries.ContentQuery({
+    relativePath: `${params.filename}.md`,
+  });
+  return {
+    props: {
+      data: tinaProps.data,
+      query: tinaProps.query,
+      variables: tinaProps.variables,
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const pagesListData = await client.queries.pagesConnection();
+  return {
+    paths: pagesListData.data.pagesConnection.edges.map((page) => ({
+      params: { filename: page.node._sys.filename },
+    })),
+    fallback: false,
+  };
+};
+
+export type AsyncReturnType<T extends (...args: any) => Promise<any>> =
+  T extends (...args: any) => Promise<infer R> ? R : any;
