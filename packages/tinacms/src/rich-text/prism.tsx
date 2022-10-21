609955c88e50f1b857dd64bc6453a54930d6c4b1
Jeff See <jeffsee.55@gmail.com>
2022-10-20 17:06:56 -0700

    Update the shape of the code_block component in tina markdown, export a prism react component for ease of use.

---
+++ packages/tinacms/src/rich-text/prism.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Highlight, { defaultProps } from 'prism-react-renderer'
+import theme from 'prism-react-renderer/themes/github'
+
+export const Prism = (props: { value: string; lang?: string }) => {
+  return (
+    <Highlight
+      {...defaultProps}
+      theme={theme}
+      code={props.value}
+      // @ts-ignore prism will ignore syntax for languages it doesn't have
+      language={props.lang}
+    >
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <pre className={className} style={style}>
+          {tokens.map((line, i) => (
+            <div {...getLineProps({ line, key: i })}>
+              {line.map((token, key) => (
+                <span {...getTokenProps({ token, key })} />
+              ))}
+            </div>
+          ))}
+        </pre>
+      )}
+    </Highlight>
+  )
+}
