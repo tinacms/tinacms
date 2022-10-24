8b18a952cf218e352ee5aa30e412cf5007b70a85
Alex <betafoenix@gmail.com>
2022-10-24 14:20:39 +0800

    fix onPristineChange not calling issue

--- packages/@tinacms/toolkit/src/packages/form-builder/FormBuilder.tsx
+++ packages/@tinacms/toolkit/src/packages/form-builder/FormBuilder.tsx
@@ -109,31 +109,31 @@ export const FormBuilder: FC<FormBuilderProps> = ({
   /**
    * Prevent navigation away from the window when the form is dirty
    */
-  // React.useEffect(() => {
-  //   const onBeforeUnload = (event) => {
-  //     event.preventDefault()
-  //     event.returnValue = ''
-  //   }
+  React.useEffect(() => {
+    // const onBeforeUnload = (event) => {
+    //   event.preventDefault()
+    //   event.returnValue = ''
+    // }
 
-  //   const unsubscribe = finalForm.subscribe(
-  //     ({ pristine }) => {
-  //       if (onPristineChange) {
-  //         onPristineChange(pristine)
-  //       }
+    const unsubscribe = finalForm.subscribe(
+      ({ pristine }) => {
+        if (onPristineChange) {
+          onPristineChange(pristine)
+        }
 
-  //       if (!pristine) {
-  //         window.addEventListener('beforeunload', onBeforeUnload)
-  //       } else {
-  //         window.removeEventListener('beforeunload', onBeforeUnload)
-  //       }
-  //     },
-  //     { pristine: true }
-  //   )
-  //   return () => {
-  //     window.removeEventListener('beforeunload', onBeforeUnload)
-  //     unsubscribe()
-  //   }
-  // }, [finalForm])
+        // if (!pristine) {
+        //   window.addEventListener('beforeunload', onBeforeUnload)
+        // } else {
+        //   window.removeEventListener('beforeunload', onBeforeUnload)
+        // }
+      },
+      { pristine: true }
+    )
+    return () => {
+      // window.removeEventListener('beforeunload', onBeforeUnload)
+      unsubscribe()
+    }
+  }, [finalForm])
 
   useOnChangeEventDispatch({ finalForm, tinaForm })
 
