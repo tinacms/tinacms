b9c8622953a3f68efcac5fe1cdb9794ef8b82495
James O'Halloran <james.p.ohalloran@gmail.com>
2022-11-30 12:36:31 -0400

    tina init formatting

--- packages/@tinacms/cli/src/utils/theme.ts
+++ packages/@tinacms/cli/src/utils/theme.ts
@@ -20,7 +20,9 @@ export const neutralText = chalk.bold.cyan
 export const linkText = chalk.bold.cyan
 export const labelText = chalk.bold
 export const cmdText = chalk.inverse
-
+export const indentedCmd = (str: any) => {
+  return `  ┃ ` + str
+}
 export const logText = chalk.italic.gray
 export const warnText = chalk.yellowBright.bgBlack
 export const titleText = chalk.bgHex('d2f1f8').hex('ec4816')
