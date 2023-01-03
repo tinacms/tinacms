bf8da2675fa2f540abe502326e9f619eeeffd827
James O'Halloran <james.p.ohalloran@gmail.com>
2023-01-03 18:34:01 -0400

    non-greedy match

--- packages/@tinacms/mdx/src/stringify/index.ts
+++ packages/@tinacms/mdx/src/stringify/index.ts
@@ -75,8 +75,8 @@ export const stringifyMDX = (
     }
     if (template.match) {
       const regex = !!template.fields.find((t) => t.name == 'text')
-        ? `<[\\s]*${template.name}[\\s]*text=(.*?)>((?:.|\n)*)<\/\\s*${template.name}\\s*>`
-        : `<[\\s]*${template.name}(.+?)?[\\s]*>((?:.|\n)*)<\/\\s*${template.name}\\s*>`
+        ? `<[\\s]*${template.name}[\\s]*text=(.*?)>((?:.|\n)*?)<\/\\s*${template.name}\\s*>`
+        : `<[\\s]*${template.name}(.+?)?[\\s]*>((?:.|\n)*?)<\/\\s*${template.name}\\s*>`
 
       const replace = template.fields.find((t) => t.name == 'children')
         ? `${template.match.start} ${template.match.name || template.name} $1 ${
