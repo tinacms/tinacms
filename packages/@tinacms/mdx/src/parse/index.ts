fc5121036c6659d70409704236adb42107dddc1f
James O'Halloran <james.p.ohalloran@gmail.com>
2023-01-03 16:19:03 -0400

    handle children in parse

--- packages/@tinacms/mdx/src/parse/index.ts
+++ packages/@tinacms/mdx/src/parse/index.ts
@@ -86,16 +86,27 @@ export const markdownToAst = (value: string, field: RichTypeInner) => {
           (t) => t.name == 'text'
         )
 
+        const hasChildren = !!template.fields.find((t) => t.name == 'children')
+
         const replacement = `<${template.name} ${
           unkeyedAttributes ? "text='$1'" : '$1'
-        }>\n</${template.name}>`
+        }>${hasChildren ? '$2' : '\n'}</${template.name}>`
+
+        const endRegex = `((?:.|\\n)*)${template.match.start}\\s\/\\s*${
+          template.match.name || template.name
+        }[\\s]*${template.match.end}`
+
         const regex = unkeyedAttributes
           ? `${template.match.start}\\s*${
               template.match.name || template.name
-            }[\\s]+[\'\"]?(.*?)[\'\"]?[\\s]*${template.match.end}`
+            }[\\s]+[\'\"]?(.*?)[\'\"]?[\\s]*${template.match.end}${
+              hasChildren ? endRegex : ''
+            }`
           : `${template.match.start}\\s*${
               template.match.name || template.name
-            }[\\s]+(.*?)[\\s]*${template.match.end}`
+            }[\\s]+(.*?)[\\s]*${template.match.end}\n${
+              hasChildren ? endRegex : ''
+            }`
 
         // console.log('replacement: ', replacement)
         // console.log('regex: ', regex)
