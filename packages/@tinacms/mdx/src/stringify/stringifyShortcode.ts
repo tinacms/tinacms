13f53e083723c65d06b8cc75d33b3c1e69596a2a
James O'Halloran <james.p.ohalloran@gmail.com>
2023-01-04 11:30:51 -0400

    import from schema-tools

--- packages/@tinacms/mdx/src/stringify/stringifyShortcode.ts
+++ packages/@tinacms/mdx/src/stringify/stringifyShortcode.ts
@@ -16,7 +16,7 @@ limitations under the License.
 
 */
 
-import { Template } from '../../../schema-tools/src'
+import { Template } from '@tinacms/schema-tools'
 import { replaceAll } from '../parse'
 
 export function stringifyShortcode(
