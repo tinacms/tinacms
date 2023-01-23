3d429b8016ca9579831361055c8f6a4ad5e7b0c8
Logan Anderson <landerson2@upei.ca>
2023-01-23 14:42:10 -0500

    added example for filesystem

--- examples/basic-iframe/.tina/database.js
+++ examples/basic-iframe/.tina/database.js
@@ -2,6 +2,8 @@ import { createDatabase } from '@tinacms/graphql'
 import { FilesystemBridge } from '@tinacms/datalayer'
 import { TinaLocalLevel } from '@tinacms/cli'
 import { Octokit } from '@octokit/rest'
+import fs from 'fs'
+import path from 'path'
 import { Base64 } from 'js-base64'
 
 const owner = ''
@@ -19,55 +21,60 @@ export default createDatabase({
   bridge: new FilesystemBridge(process.cwd()),
   level: localLevelStore,
   onPut: async (key, value) => {
-    let sha
-    try {
-      const {
-        data: { sha: existingSha },
-      } = await octokit.repos.getContent({
-        owner,
-        repo,
-        path: key,
-        branch,
-      })
-      sha = existingSha
-    } catch (e) {}
+    const filePath = path.join(process.cwd(), key)
+    fs.writeFileSync(filePath, value)
+    console.log('putting', key, value)
+    // let sha
+    // try {
+    //   const {
+    //     data: { sha: existingSha },
+    //   } = await octokit.repos.getContent({
+    //     owner,
+    //     repo,
+    //     path: key,
+    //     branch,
+    //   })
+    //   sha = existingSha
+    // } catch (e) {}
 
-    const { data } = await octokit.repos.createOrUpdateFileContents({
-      owner,
-      repo,
-      path: key,
-      message: 'commit from self-hosted tina',
-      content: Base64.encode(value),
-      branch,
-      sha,
-    })
+    // const { data } = await octokit.repos.createOrUpdateFileContents({
+    //   owner,
+    //   repo,
+    //   path: key,
+    //   message: 'commit from self-hosted tina',
+    //   content: Base64.encode(value),
+    //   branch,
+    //   sha,
+    // })
   },
   onDelete: async (key) => {
-    console.log('deleting', key)
-    let sha
-    try {
-      const {
-        data: { sha: existingSha },
-      } = await octokit.repos.getContent({
-        owner,
-        repo,
-        path: key,
-        branch,
-      })
-      sha = existingSha
-    } catch (e) {
-      console.log(e)
-    }
-    if (sha) {
-      const { data } = await octokit.repos.deleteFile({
-        owner,
-        repo,
-        path: key,
-        message: 'commit from self-hosted tina',
-        branch,
-        sha,
-      })
-      console.log('data', data)
-    }
+    const filePath = path.join(process.cwd(), key)
+    console.log('deleting', filePath)
+    fs.rmSync(filePath)
+    // let sha
+    // try {
+    //   const {
+    //     data: { sha: existingSha },
+    //   } = await octokit.repos.getContent({
+    //     owner,
+    //     repo,
+    //     path: key,
+    //     branch,
+    //   })
+    //   sha = existingSha
+    // } catch (e) {
+    //   console.log(e)
+    // }
+    // if (sha) {
+    //   const { data } = await octokit.repos.deleteFile({
+    //     owner,
+    //     repo,
+    //     path: key,
+    //     message: 'commit from self-hosted tina',
+    //     branch,
+    //     sha,
+    //   })
+    //   console.log('data', data)
+    // }
   },
 })
