d879a266c4ad62b0ef8644e80dbf1f593744ffee
Logan Anderson <landerson2@upei.ca>
2023-01-24 09:39:34 -0500

    update import to fix tests

--- packages/@tinacms/schema-tools/src/util/parseURL.spec.ts
+++ packages/@tinacms/schema-tools/src/util/parseURL.spec.ts
@@ -11,7 +11,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import { parseURL, TINA_HOST } from './parseUrl'
+import { parseURL, TINA_HOST } from './parseURL'
 const MOCK_HOST_PROD = `https://${TINA_HOST}`
 const MOCK_HOST_LOCAL = `https://localhost:4001`
 const MOCK_CLIENT_ID = '1234'
