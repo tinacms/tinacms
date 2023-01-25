fb27d1ffe39d9a5fef505e18fa2c6b1e969c25b0
Logan Anderson <landerson2@upei.ca>
2023-01-25 14:19:48 -0500

    add self hosted demo

---
+++ examples/tina-self-hosted-demo/lib/databaseConnection.ts
@@ -0,0 +1,42 @@
+import database from '../.tina/database'
+import { queries } from '../.tina/__generated__/types'
+import { resolve } from '@tinacms/graphql'
+import type { Database } from '@tinacms/graphql'
+import type { TinaClient } from 'tinacms/dist/client'
+
+export async function databaseRequest({ query, variables }) {
+  const config = {
+    useRelativeMedia: true,
+  } as any
+
+  const result = await resolve({
+    config,
+    database,
+    query,
+    variables,
+    verbose: true,
+  })
+
+  return result
+}
+
+export function getDatabaseConnection<GenQueries = Record<string, unknown>>({
+  database,
+  queries,
+}: {
+  database: Database
+  queries: (client: {
+    request: TinaClient<GenQueries>['request']
+  }) => GenQueries
+}) {
+  const request = async ({ query, variables }) => {
+    const data = await databaseRequest({ query, variables })
+    return { data: data.data as any, query, variables, errors: data.errors }
+  }
+  const q = queries({
+    request,
+  })
+  return { queries: q, request }
+}
+
+export const dbConnection = getDatabaseConnection({ database, queries })
---
+++ examples/tina-self-hosted-demo/pages/[filename].tsx
@@ -0,0 +1,45 @@
+import { Blocks } from '../components/blocks-renderer'
+import { useTina } from 'tinacms/dist/react'
+import { Layout } from '../components/layout'
+import { dbConnection } from '../lib/databaseConnection'
+
+export default function HomePage(
+  props: AsyncReturnType<typeof getStaticProps>['props']
+) {
+  const { data } = useTina({
+    query: props.query,
+    variables: props.variables,
+    data: props.data,
+  })
+  return (
+    <Layout rawData={data} data={data.global as any}>
+      <Blocks {...data.page} />
+    </Layout>
+  )
+}
+
+export const getStaticProps = async ({ params }) => {
+  const tinaProps = await dbConnection.queries.contentQuery({
+    relativePath: `${params.filename}.md`,
+  })
+  return {
+    props: {
+      data: tinaProps.data,
+      query: tinaProps.query,
+      variables: tinaProps.variables,
+    },
+  }
+}
+
+export const getStaticPaths = async () => {
+  const pagesListData = await dbConnection.queries.pageConnection()
+  return {
+    paths: pagesListData.data.pageConnection.edges.map((page) => ({
+      params: { filename: page.node._sys.filename },
+    })),
+    fallback: false,
+  }
+}
+
+export type AsyncReturnType<T extends (...args: any) => Promise<any>> =
+  T extends (...args: any) => Promise<infer R> ? R : any
