d5dfad3b8db4beddcb879c2a8fc7ba39c919b573
Scott Byrne <scott.byrne@forestry.io>
2023-03-03 12:55:45 -0400

    fix index file

--- packages/@tinacms/toolkit/src/components/media/index.ts
+++ packages/@tinacms/toolkit/src/components/media/index.ts
@@ -5,7 +5,7 @@
 */
 
 export * from './media-manager'
-export { MediaItem } from './media-item'
+export { ListMediaItem, GridMediaItem } from './media-item'
 export { Breadcrumb } from './breadcrumb'
 export { CursorPaginator } from './pagination'
 export type { MediaPaginatorProps } from './pagination'
