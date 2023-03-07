dd063749f2cafbdf129df028ac03e673cb03c3cd
Scott Byrne <scott.byrne@forestry.io>
2023-03-07 11:04:25 -0400

    error styles

--- packages/tinacms/src/admin/components/FullscreenError.tsx
+++ packages/tinacms/src/admin/components/FullscreenError.tsx
@@ -1,18 +1,21 @@
 import React from 'react'
+import { Button } from '@tinacms/toolkit'
+import { BiError, BiSync } from 'react-icons/bi'
 
 export const FullscreenError = ({
-  errorMessage = 'It looks like something went wrong',
+  title = 'Error',
+  errorMessage = 'It looks like something went wrong.',
 }) => {
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-gray-100">
-      <div className="text-red-500 text-4xl mb-4">Error</div>
+      <div className="text-red-500 text-4xl mb-6 flex items-center">
+        <BiError className="w-12 h-auto fill-current text-red-400 opacity-70 mr-1" />{' '}
+        {title}
+      </div>
       <p className="text-gray-700 text-xl mb-8">{errorMessage}</p>
-      <button
-        className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-        onClick={() => window.location.reload()}
-      >
-        Reload
-      </button>
+      <Button variant="danger" onClick={() => window.location.reload()}>
+        <BiSync className="w-7 h-auto fill-current opacity-70 mr-1" /> Reload
+      </Button>
     </div>
   )
 }
