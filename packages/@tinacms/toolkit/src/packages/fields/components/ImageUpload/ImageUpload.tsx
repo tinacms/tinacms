e32b3c8b522a15f092220e18570f9ef66e79800d
Scott Byrne <scott.byrne@forestry.io>
2023-03-14 10:29:55 -0300

    image upload flex fix

--- packages/@tinacms/toolkit/src/packages/fields/components/ImageUpload/ImageUpload.tsx
+++ packages/@tinacms/toolkit/src/packages/fields/components/ImageUpload/ImageUpload.tsx
@@ -36,8 +36,8 @@ const StyledImage = ({ src }) => (
 
 const StyledFile = ({ src }) => {
   return (
-    <div className="max-w-full flex-1 flex justify-start items-center gap-3">
-      <div className="w-14 h-14 bg-gray-50 shadow border border-gray-100 rounded overflow-hidden flex justify-center shrink-0">
+    <div className="max-w-full w-full overflow-hidden flex-1 flex justify-start items-center gap-3">
+      <div className="w-14 h-14 bg-gray-50 shadow border border-gray-100 rounded flex justify-center flex-none">
         <BiFileBlank className="w-3/5 h-full fill-gray-300" />
       </div>
       <span className="text-base text-left flex-1 text-gray-500 w-full break-words truncate">
@@ -66,19 +66,19 @@ export const ImageUpload = ({
     noClick: !!onClick,
   })
   return (
-    <div className="flex-1" {...getRootProps()}>
+    <div className="w-full max-w-full" {...getRootProps()}>
       <input {...getInputProps()} />
       {value ? (
         loading ? (
           <ImageLoadingIndicator />
         ) : (
           <div
-            className={`relative w-full max-w-full flex flex-1 justify-between gap-3 ${
+            className={`relative w-full max-w-full flex justify-between gap-3 ${
               isImage ? `items-start` : `items-center`
             }`}
           >
             <button
-              className="outline-none flex-1 w-full max-w-full cursor-pointer border-none hover:opacity-60 transition ease-out duration-100"
+              className="outline-none overflow-visible flex-1 w-[0px] cursor-pointer border-none hover:opacity-60 transition ease-out duration-100"
               onClick={onClick}
             >
               {isImage ? <StyledImage src={src} /> : <StyledFile src={src} />}
@@ -119,7 +119,7 @@ export const DeleteImageButton = ({
   onClick: (_event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
 }) => {
   return (
-    <IconButton variant="white" className="shrink-0" onClick={onClick}>
+    <IconButton variant="white" className="flex-none" onClick={onClick}>
       <TrashIcon className="w-7 h-auto" />
     </IconButton>
   )
