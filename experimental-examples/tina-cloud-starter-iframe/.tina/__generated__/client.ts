76b6206fe9b0a45fd3e49ca7d3479a964c21beb3
James O'Halloran <james.p.ohalloran@gmail.com>
2023-03-29 10:40:47 -0300

    rm old tina

--- experimental-examples/tina-cloud-starter-iframe/.tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'ed05987e10e19e6e3118024610b53d2377ae07b7', queries });
-export default client;
-  
--- experimental-examples/tina-cloud-starter/.tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'foo', queries });
-export default client;
-  
