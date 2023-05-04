c4b871a7090a6b4c1bcb567cf7dc4acd710097e4
Grzegorz Leoniec <greg@app-interactive.de>
2023-05-04 12:21:05 +0200

    prevent colors from disappearing in admin

--- experimental-examples/tina-cloud-starter-iframe/tina/config.tsx
+++ experimental-examples/tina-cloud-starter-iframe/tina/config.tsx
@@ -306,6 +306,17 @@ const config = defineStaticConfig({
                 label: "Primary Color",
                 name: "color",
                 ui: {
+                  /* HACK TO KEEP COLORS IN TAILWIND AFTER PURGE
+                    blue: "bg-blue-500 border-blue-600",
+                    teal: "bg-teal-500 border-teal-600",
+                    green: "bg-green-500 border-green-600",
+                    yellow: "bg-yellow-500 border-yellow-600",
+                    orange: "bg-orange-500 border-orange-600",
+                    red: "bg-red-500 border-red-600",
+                    pink: "bg-pink-500 border-pink-600",
+                    purple: "bg-purple-500 border-purple-600",
+                    white: "bg-white border-gray-150",
+                  */
                   component: ColorPickerInput,
                 },
               },
