20fd5a1d0807e738e2205550b3c199bc597e0030
Grzegorz Leoniec <greg@app-interactive.de>
2023-05-04 12:20:31 +0200

    fixed tailwind config in example

--- experimental-examples/tina-cloud-starter-iframe/tailwind.config.js
+++ experimental-examples/tina-cloud-starter-iframe/tailwind.config.js
@@ -2,8 +2,10 @@ const colors = require("tailwindcss/colors");
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: "class",
   theme: {
     colors: {
