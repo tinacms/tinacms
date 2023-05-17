5039db6a5eab6a68116b3f5b7ac280c954aa665b
James O'Halloran <james.p.ohalloran@gmail.com>
2023-05-17 14:37:09 -0300

    update endpoint

--- packages/tinacms/src/internalClient/index.ts
+++ packages/tinacms/src/internalClient/index.ts
@@ -495,7 +495,7 @@ mutation addPendingDocumentMutation(
 
   async getProject() {
     const res = await this.fetchWithToken(
-      `${this.identityApiUrl}/apps/${this.clientId}`,
+      `${this.identityApiUrl}/v2/apps/${this.clientId}`,
       {
         method: 'GET',
       }
