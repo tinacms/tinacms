491c9454b7415ea472f07dc1abdb880d741a0bc7
Jeff See <jeffsee.55@gmail.com>
2023-05-17 09:19:25 -0700

    Update changeset, remove unnecessary utilities in starter example

--- .changeset/cuddly-donuts-try.md
+++ .changeset/cuddly-donuts-try.md
@@ -1,5 +1,17 @@
 ---
 '@tinacms/vercel-previews': patch
+'@tinacms/cli': patch
+'tinacms': patch
 ---
 
-Fix import and update README
+Add support for "quick editing". By adding the `[data-tina-field]` attribute to your elements, editors can click to see the
+correct form and field focused in the sidebar.
+
+This work closely resembles the ["Active Feild Indicator"](https://tina-io-git-quick-edit-tinacms.vercel.app/docs/editing/active-field-indicator/) feature.
+Which will be phased in out place of this in the future. Note that the attribute name is different, `[data-tinafield]` is the value
+for the "Active Field Indicator" while `[data-tina-field]` is the new attribute.
+
+The `tinaField` helper function should now only be used with the `[data-tina-field]` attibute.
+
+Adds experimental support for Vercel previews, the `useVisualEditing` hook from `@tinacms/vercel-previews` can be used
+to activate edit mode and listen for Vercel edit events.
--- experimental-examples/tina-cloud-starter/pages/[filename].tsx
+++ experimental-examples/tina-cloud-starter/pages/[filename].tsx
@@ -4,43 +4,14 @@ import { useTina } from 'tinacms/dist/react'
 import { Layout } from '../components/layout'
 import { client } from '../.tina/__generated__/client'
 import React from 'react'
-import { useVisualEditing } from '@tinacms/vercel-previews'
-import { encodeAtPath } from '../.tina/config'
 
 export default function HomePage(
   props: InferGetStaticPropsType<typeof getStaticProps>
 ) {
-  const [enabledPreview, setPreviewEnabled] = React.useState(false)
-  const { data: tinaData } = useTina(props, {
-    redirect: '/admin',
-    quickEditEnabled: enabledPreview,
-  })
-  const data = useVisualEditing({
-    data: tinaData,
-    query: props.query,
-    variables: props.variables,
-    redirect: '/admin',
-    enabled: false,
-    // stringEncoding: true,
-    stringEncoding: {
-      skipPaths: (path) => {
-        if ('page.blocks.0.headline' === path) {
-          return false
-        }
-
-        return true
-      },
-    },
-  })
+  const { data } = useTina(props)
 
   return (
     <Layout rawData={data} data={data.global as any}>
-      <button
-        className="absolute bottom-2 right-2 p-4 rounded-sm bg-blue-500 text-white z-40"
-        onClick={() => setPreviewEnabled((preview) => !preview)}
-      >
-        Toggle Preview
-      </button>
       <Blocks {...data.page} />
     </Layout>
   )
