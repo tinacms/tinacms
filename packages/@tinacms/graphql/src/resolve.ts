24307f3c04d5b8d0077e62eb85b65dcf5db90c09
Kelly Davis <kelly.davis@forestry.io>
2023-05-19 15:17:19 -0400

    more debug introspection query3

--- packages/@tinacms/graphql/src/resolve.ts
+++ packages/@tinacms/graphql/src/resolve.ts
@@ -40,11 +40,15 @@ export const resolve = async ({
   isAudit?: boolean
 }) => {
   try {
+    console.log('resolve1')
     const verboseValue = verbose ?? true
     const graphQLSchemaAst = await database.getGraphQLSchema()
+    console.log('resolve2')
     const graphQLSchema = buildASTSchema(graphQLSchemaAst)
+    console.log('resolve3')
 
     const tinaConfig = await database.getTinaSchema()
+    console.log('resolve4')
     const tinaSchema = (await createSchema({
       // TODO: please update all the types to import from @tinacms/schema-tools
       // @ts-ignore
@@ -52,12 +56,14 @@ export const resolve = async ({
       // @ts-ignore
       flags: tinaConfig?.meta?.flags,
     })) as unknown as TinaSchema
+    console.log('resolve5')
     const resolver = await createResolver({
       config,
       database,
       tinaSchema,
       isAudit: isAudit || false,
     })
+    console.log('resolve6')
 
     const res = await graphql({
       schema: graphQLSchema,
