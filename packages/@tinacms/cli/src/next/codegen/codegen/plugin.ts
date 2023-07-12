193d98dfa4ff51faf9513cea26e8227925243e8b
Jeff See <jeffsee.55@gmail.com>
2023-07-12 12:21:18 -0700

    Adds ability to specify branch name in the request for the autogenerated client

--- packages/@tinacms/cli/src/next/codegen/codegen/plugin.ts
+++ packages/@tinacms/cli/src/next/codegen/codegen/plugin.ts
@@ -12,36 +12,55 @@ export const AddGeneratedClientFunc: (apiURL: string) => PluginFunction = (
 // TinaSDK generated code
 import { createClient, TinaClient } from "tinacms/dist/client";
 
-const generateRequester = (client: TinaClient) => {
+const generateRequester = (
+  client: TinaClient,
+  options?: { branch?: string }
+) => {
   const requester: (
     doc: any,
     vars?: any,
-    options?: any,
+    options?: { branch?: string },
     client
-  ) => Promise<any> = async (doc, vars, _options) => {
+  ) => Promise<any> = async (doc, vars, options) => {
+    let url = client.apiUrl
+    if (options?.branch) {
+      const index = client.apiUrl.lastIndexOf('/')
+      url = client.apiUrl.substring(0, index + 1) + options.branch
+    }
     const data = await client.request({
       query: doc,
       variables: vars,
-    });
+      url,
+    })
 
-    return { data: data?.data, query: doc, variables: vars || {} };
-  };
+    return { data: data?.data, query: doc, variables: vars || {} }
+  }
 
-  return requester;
-};
+  return requester
+}
 
 /**
  * @experimental this class can be used but may change in the future
  **/
 export const ExperimentalGetTinaClient = () =>
   getSdk(
-    generateRequester(createClient({ url: "${apiURL}", queries }))
-  );
+    generateRequester(
+      createClient({
+        url: "${apiURL}",
+        queries,
+      })
+    )
+  )
 
-export const queries = (client: TinaClient) => {
-  const requester = generateRequester(client);
-  return getSdk(requester);
-};
+export const queries = (
+  client: TinaClient,
+  options?: {
+    branch?: string
+  }
+) => {
+  const requester = generateRequester(client, options)
+  return getSdk(requester)
+}
 `
   }
 }
