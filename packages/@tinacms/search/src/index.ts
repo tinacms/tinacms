9c4b08a41c9e1a4ef7a80e04f2e7cecea1f32766
Kelly Davis <kelly.davis@forestry.io>
2023-07-13 17:29:30 -0400

    fix: use stopwords to filter the search query

--- packages/@tinacms/search/src/index.ts
+++ packages/@tinacms/search/src/index.ts
@@ -1,21 +1,5 @@
-import * as sw from 'stopword'
 import si from 'search-index'
 export { SearchIndexer } from './indexer'
 export { LocalSearchIndexClient, TinaCMSSearchIndexClient } from './client'
 export type { SearchClient } from './types'
-export { default as sw } from 'stopword'
 export { si }
-
-export const lookupStopwords = (
-  keys?: string[],
-  defaultStopWords: string[] = sw.eng
-) => {
-  let stopwords = defaultStopWords
-  if (keys) {
-    stopwords = []
-    for (const key of keys) {
-      stopwords.push(...sw[key])
-    }
-  }
-  return stopwords
-}
--- packages/tinacms/src/internalClient/index.ts
+++ packages/tinacms/src/internalClient/index.ts
@@ -849,7 +849,10 @@ export class LocalClient extends Client {
 }
 
 export class TinaCMSSearchClient implements SearchClient {
-  constructor(private client: Client) {}
+  constructor(
+    private client: Client,
+    private tinaSearchConfig?: { stopwordLanguages?: string[] }
+  ) {}
   async query(
     query: string,
     options?: {
@@ -862,7 +865,10 @@ export class TinaCMSSearchClient implements SearchClient {
     total: number
     prevCursor: string | null
   }> {
-    const q = queryToSearchIndexQuery(query)
+    const q = queryToSearchIndexQuery(
+      query,
+      this.tinaSearchConfig?.stopwordLanguages
+    )
     const opt = optionsToSearchIndexOptions(options)
     const optionsParam = opt['PAGE'] ? `&options=${JSON.stringify(opt)}` : ''
     const res = await this.client.fetchWithToken(
