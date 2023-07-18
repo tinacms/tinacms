7f576b6516141690920ca64b66a81f2be718636c
Logan Anderson <landerson2@upei.ca>
2023-07-18 16:19:33 -0400

    update database files

--- examples/tina-self-hosted-demo/tina/database.ts
+++ examples/tina-self-hosted-demo/tina/database.ts
@@ -7,23 +7,18 @@ import { MongodbLevel } from 'mongodb-level'
 
 const isLocal = process.env.TINA_PUBLIC_IS_LOCAL === 'true'
 
-const owner = process.env.GITHUB_OWNER as string
-const repo = process.env.GITHUB_REPO as string
-const token = process.env.GITHUB_PERSONAL_ACCESS_TOKEN as string
-const branch = process.env.GITHUB_BRANCH as string
-const mongoUri = process.env.MONGODB_URI as string
-
-const adapter = new MongodbLevel<string, Record<string, any>>({
-  collectionName: branch,
-  dbName: 'tina-test',
-  mongoUri,
-})
-
-const gitProvider = new GitHubProvider({ branch, owner, repo, token })
-
 export default isLocal
   ? createLocalDatabase()
   : createDatabase({
-      gitProvider: gitProvider,
-      databaseAdapter: adapter,
+      gitProvider: new GitHubProvider({
+        branch: process.env.GITHUB_BRANCH,
+        owner: process.env.GITHUB_OWNER,
+        repo: process.env.GITHUB_REPO,
+        token: process.env.GITHUB_PERSONAL_ACCESS_TOKEN,
+      }),
+      databaseAdapter: new MongodbLevel<string, Record<string, any>>({
+        collectionName: process.env.GITHUB_BRANCH,
+        dbName: 'tinacms',
+        mongoUri: process.env.MONGODB_URI,
+      }),
     })
