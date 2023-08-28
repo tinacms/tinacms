47ec08f5cbf7ff0b62a7df97e392a4421df4db7c
James O'Halloran <james.p.ohalloran@gmail.com>
2023-08-28 15:22:34 -0300

    ssr-demo

--- examples/next-ssr/app/page.tsx
+++ examples/next-ssr/app/page.tsx
@@ -1,53 +1,16 @@
 import Image from 'next/image'
-import client from '../../../tina/client'
-import { useTinaSSR } from 'tinacms/dist/react'
-import { RegisterTina } from '../../components/registerTina'
-
-const variables = { relativePath: `hello-world.md` }
-const query =
-  '\n' +
-  '    query post($relativePath: String!) {\n' +
-  '  post(relativePath: $relativePath) {\n' +
-  '    ... on Document {\n' +
-  '      _sys {\n' +
-  '        filename\n' +
-  '        basename\n' +
-  '        breadcrumbs\n' +
-  '        path\n' +
-  '        relativePath\n' +
-  '        extension\n' +
-  '      }\n' +
-  '      id\n' +
-  '    }\n' +
-  '    ...PostParts\n' +
-  '  }\n' +
-  '}\n' +
-  '    \n' +
-  '    fragment PostParts on Post {\n' +
-  '  title\n' +
-  '  body\n' +
-  '}\n' +
-  '    '
-export default async function About({
-  searchParams: pageData,
-}: {
-  searchParams?: any
-}) {
-  const { data } = await useTinaSSR({
-    data: pageData,
-    loadData: () => client.queries.post(variables),
-  })
-
+import Link from 'next/link'
+export default async function Home({}: {}) {
   return (
     <>
-      <RegisterTina data={data} query={query} variables={variables} />
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
           <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
             Get started by editing&nbsp;
             <code className="font-mono font-bold">app/page.tsx</code>
           </p>
-          <h1>{data.post.title}</h1>
+          <h1>HOME PAGE</h1>
+          <Link href="/about">GO TO ABOUT</Link>
 
           <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
             <a
