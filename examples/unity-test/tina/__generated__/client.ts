e3f103ba0cd14bf40f04fa8908a131a7dcdc1179
Jeff See <jeffsee.55@gmail.com>
2023-09-19 13:14:50 -0700

    rename folder

--- examples/unity-test/tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'null', queries });
-export default client;
-  
