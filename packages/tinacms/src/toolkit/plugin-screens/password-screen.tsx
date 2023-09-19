716351c45c8aee810de309fe2e3f74870a714cca
Kelly Davis <kelly.davis@forestry.io>
2023-09-19 16:14:16 -0400

    fix: icon for password change

--- packages/tinacms/src/toolkit/plugin-screens/password-screen.tsx
+++ packages/tinacms/src/toolkit/plugin-screens/password-screen.tsx
@@ -1,11 +1,11 @@
-import { MdOutlinePhotoLibrary } from 'react-icons/md'
+import { MdVpnKey } from 'react-icons/md'
 import { createScreen } from '@toolkit/react-screens'
 import { UpdatePassword } from '@toolkit/components/account/update-password'
 
 export const PasswordScreenPlugin = createScreen({
   name: 'Change Password',
   Component: UpdatePassword,
-  Icon: MdOutlinePhotoLibrary,
+  Icon: MdVpnKey,
   layout: 'fullscreen',
   navCategory: 'Account',
 })
