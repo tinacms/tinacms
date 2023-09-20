5592fd9f9337955e36bf2a8ecbb769c20d56123f
James O'Halloran <james.p.ohalloran@gmail.com>
2023-09-20 11:48:34 -0300

    dummy logo sidebar

--- examples/routing-poc/app/layout.tsx
+++ examples/routing-poc/app/layout.tsx
@@ -19,6 +19,22 @@ export default function RootLayout({
       <body className={inter.className}>
         <div className="h-screen w-full flex">
           <div className="h-screen w-12 border-r border-slate-200 flex flex-col gap-2">
+            <div className="p-2 pb-4">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="3"
+                stroke="currentColor"
+                className="w-fill"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9"
+                />
+              </svg>
+            </div>
             <div className="p-2">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
