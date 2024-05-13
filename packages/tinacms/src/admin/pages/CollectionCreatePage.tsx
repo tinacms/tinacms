e162527d9e276d35359c9b996ad8fc327e221034
JackDevAU <jpr178@live.com.au>
2024-05-13 13:27:29 +1000

    fix: Refactor slug generation logic to fix Stack overflow

--- packages/tinacms/src/admin/pages/CollectionCreatePage.tsx
+++ packages/tinacms/src/admin/pages/CollectionCreatePage.tsx
@@ -190,6 +190,15 @@ export const RenderForm = ({
     template?.defaultItem ||
     {}
 
+  const slug = useMemo(() => {
+    if (slugFunction) {
+      return slugFunction(defaultItem, {
+        template,
+        collection: schemaCollection,
+      })
+    }
+  }, [slugFunction])
+
   const form = useMemo(() => {
     const folderName = folder.fullyQualifiedName ? folder.name : ''
     return new Form({
@@ -219,10 +228,7 @@ export const RenderForm = ({
           !values?.submitting &&
           !values.touched?.filename
         ) {
-          const value = slugFunction(values.values, {
-            template,
-            collection: schemaCollection,
-          })
+          const value = slug
           form.finalForm.change('filename', value)
         }
       },
