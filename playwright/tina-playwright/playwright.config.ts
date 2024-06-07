bc2f9c25f2a82f4a459a7ba6fa80d80adae134a4
benneoh <benneoh@ssw.com.au>
2024-06-07 16:14:11 +1000

    add timeout config for playwright

--- playwright/tina-playwright/playwright.config.ts
+++ playwright/tina-playwright/playwright.config.ts
@@ -10,6 +10,8 @@ import { defineConfig, devices } from "@playwright/test";
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  timeout: 30 * 1000,
+
   testDir: "./tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
