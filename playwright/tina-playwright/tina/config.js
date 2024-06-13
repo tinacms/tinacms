16346a88150936e0535fc48ae59fa94f72f9ddfb
benneoh <benneoh@ssw.com.au>
2024-06-13 11:48:42 +1000

    add author to example project

--- playwright/tina-playwright/tina/config.js
+++ playwright/tina-playwright/tina/config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "tinacms";
 import page from "./collections/page";
 import post from "./collections/post";
+import author from "./collections/author";
 
 export const config = defineConfig({
   branch: "",
@@ -11,7 +12,7 @@ export const config = defineConfig({
     outputFolder: "admin", // within the public folder
   },
   schema: {
-    collections: [page, post],
+    collections: [page, post, author],
   },
 });
 
