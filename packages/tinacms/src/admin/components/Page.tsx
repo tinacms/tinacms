e2a0c202f638ed8d5e4f72e7256ecde772dc5a0f
Jord Gui [SSW] <41951199+Jord-Gui@users.noreply.github.com>
2024-07-08 11:31:25 +1000

    refactor header styling for large screens

--- packages/tinacms/src/admin/components/Page.tsx
+++ packages/tinacms/src/admin/components/Page.tsx
@@ -49,7 +49,7 @@ export const PageHeader = ({
       {isLocalMode && <LocalWarning />}
       {!isLocalMode && <BillingWarning />}
 
-      <div className="pt-16 lg:pt-12 px-12">
+      <div className="pt-16 xl:pt-12 px-12">
         <div className="w-full mx-auto max-w-screen-xl">
           <div className="w-full flex justify-between items-end">
             {children}
--- packages/tinacms/src/admin/pages/CollectionListPage.tsx
+++ packages/tinacms/src/admin/pages/CollectionListPage.tsx
@@ -296,7 +296,6 @@ const CollectionListPage = () => {
                 reFetchCollection,
                 collectionExtra: Collection<true>
               ) => {
-                const totalCount = collection.documents.totalCount
                 const documents = collection.documents.edges
                 const admin: TinaAdminApi = cms.api.admin
                 const pageInfo = collection.documents.pageInfo
@@ -464,15 +463,14 @@ const CollectionListPage = () => {
                     )}
 
                     <PageHeader isLocalMode={cms?.api?.tina?.isLocalMode}>
-                      <div className="w-full grid grid-flow-col items-end gap-4">
-                        <div className="flex flex-col gap-4">
-                          <h3 className="font-sans text-2xl text-gray-700">
-                            {collection.label
-                              ? collection.label
-                              : collection.name}
-                          </h3>
-
-                          <div className="flex gap-4 items-start flex-wrap">
+                      <div className="w-full">
+                        <h3 className="font-sans text-2xl text-gray-700">
+                          {collection.label
+                            ? collection.label
+                            : collection.name}
+                        </h3>
+                        <div className="flex justify-between pt-2">
+                          <div className="flex gap-4 items-start">
                             {fields?.length > 0 && (
                               <>
                                 {!search && (
@@ -567,58 +565,58 @@ const CollectionListPage = () => {
                               )}
                             </div>
                           </div>
-                        </div>
-                        <div className="flex self-end	justify-self-end">
-                          {!collection.templates && allowCreate && (
-                            <>
-                              {allowCreateNestedFolder && (
+                          <div className="flex flex-row items-end">
+                            {!collection.templates && allowCreate && (
+                              <>
+                                {allowCreateNestedFolder && (
+                                  <Link
+                                    onMouseDown={(evt) => {
+                                      setVars((old) => ({
+                                        ...old,
+                                        collection: collectionName,
+                                        folderName: '',
+                                      }))
+                                      setFolderModalOpen(true)
+                                      evt.stopPropagation()
+                                    }}
+                                    to="/collections/new-folder"
+                                    className="icon-parent inline-flex items-center font-medium focus:outline-none focus:ring-2 focus:shadow-outline text-center rounded-full justify-center transition-all duration-150 ease-out whitespace-nowrap shadow text-blue-500 bg-white hover:bg-[#f1f5f9] focus:ring-white focus:ring-blue-500 text-sm h-10 px-6 mr-4"
+                                  >
+                                    <FaFolder className="mr-2" />
+                                    Add Folder{' '}
+                                  </Link>
+                                )}
                                 <Link
-                                  onMouseDown={(evt) => {
-                                    setVars((old) => ({
-                                      ...old,
-                                      collection: collectionName,
-                                      folderName: '',
-                                    }))
-                                    setFolderModalOpen(true)
-                                    evt.stopPropagation()
-                                  }}
-                                  to="/collections/new-folder"
-                                  className="icon-parent inline-flex items-center font-medium focus:outline-none focus:ring-2 focus:shadow-outline text-center rounded-full justify-center transition-all duration-150 ease-out whitespace-nowrap shadow text-blue-500 bg-white hover:bg-[#f1f5f9] focus:ring-white focus:ring-blue-500 text-sm h-10 px-6 mr-4"
+                                  to={`/${
+                                    folder.fullyQualifiedName
+                                      ? [
+                                          'collections',
+                                          'new',
+                                          collectionName,
+                                          '~',
+                                          folder.name,
+                                        ].join('/')
+                                      : [
+                                          'collections',
+                                          'new',
+                                          collectionName,
+                                        ].join('/')
+                                  }`}
+                                  className="inline-flex items-center font-medium focus:outline-none focus:ring-2 focus:shadow-outline text-center rounded-full justify-center transition-all duration-150 ease-out whitespace-nowrap shadow text-white bg-blue-500 hover:bg-blue-600 text-sm h-10 px-6"
                                 >
-                                  <FaFolder className="mr-2" />
-                                  Add Folder{' '}
+                                  <FaFile className="mr-2" />
+                                  Add Files{' '}
                                 </Link>
-                              )}
-                              <Link
-                                to={`/${
-                                  folder.fullyQualifiedName
-                                    ? [
-                                        'collections',
-                                        'new',
-                                        collectionName,
-                                        '~',
-                                        folder.name,
-                                      ].join('/')
-                                    : [
-                                        'collections',
-                                        'new',
-                                        collectionName,
-                                      ].join('/')
-                                }`}
-                                className="inline-flex items-center font-medium focus:outline-none focus:ring-2 focus:shadow-outline text-center rounded-full justify-center transition-all duration-150 ease-out whitespace-nowrap shadow text-white bg-blue-500 hover:bg-blue-600 text-sm h-10 px-6"
-                              >
-                                <FaFile className="mr-2" />
-                                Add Files{' '}
-                              </Link>
-                            </>
-                          )}
-                          {collection.templates && allowCreate && (
-                            <TemplateMenu
-                              collectionName={collectionName}
-                              templates={collection.templates}
-                              folder={folder}
-                            />
-                          )}
+                              </>
+                            )}
+                            {collection.templates && allowCreate && (
+                              <TemplateMenu
+                                collectionName={collectionName}
+                                templates={collection.templates}
+                                folder={folder}
+                              />
+                            )}
+                          </div>
                         </div>
                       </div>
                     </PageHeader>
