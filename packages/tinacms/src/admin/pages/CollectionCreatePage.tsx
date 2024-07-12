d0faf424e77dbaabdc88b3aba01daffc7b203b17
Jord Gui [SSW] <41951199+Jord-Gui@users.noreply.github.com>
2024-07-12 12:26:42 +1000

    standardise padding between pages

--- packages/tinacms/src/admin/pages/CollectionCreatePage.tsx
+++ packages/tinacms/src/admin/pages/CollectionCreatePage.tsx
@@ -332,7 +332,7 @@ export const RenderForm = ({
   const navBreakpoint = 1279
   const windowWidth = useWindowWidth()
   const renderNavToggle = windowWidth < navBreakpoint + 1
-  const headerPadding = renderNavToggle ? 'px-20' : 'px-6'
+  const headerPadding = renderNavToggle ? 'px-20' : 'px-12'
 
   React.useEffect(() => {
     cms.dispatch({ type: 'forms:add', value: form })
@@ -357,7 +357,7 @@ export const RenderForm = ({
         <div
           className={`pt-10 xl:pt-3 pb-10 xl:pb-4 border-b border-gray-200 bg-white w-full grow-0 shrink basis-0 flex justify-center ${headerPadding}`}
         >
-          <div className="w-full max-w-form flex gap-1.5 justify-between items-center">
+          <div className="w-full flex gap-1.5 justify-between items-center">
             <Link
               to={`/collections/${collection.name}${
                 folder.fullyQualifiedName ? `/${folder.fullyQualifiedName}` : ''
