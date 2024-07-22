26119b4251c9deea036483b9b8f993d43603270a
brookjeynes-ssw <brookjeynes@ssw.com.au>
2024-07-22 13:38:34 +1000

    fix type error

--- packages/tinacms/src/toolkit/fields/plugins/mdx-field-plugin/index.tsx
+++ packages/tinacms/src/toolkit/fields/plugins/mdx-field-plugin/index.tsx
@@ -80,43 +80,44 @@ export const MdxFieldPluginExtendible = {
       return undefined
     }
   },
-  Component: wrapFieldsWithMeta<InputProps, { templates: MdxTemplate[] }>(
-    (props) => {
-      const [key, setKey] = React.useState(0)
+  Component: wrapFieldsWithMeta<
+    InputProps,
+    { templates: MdxTemplate[]; toolbarOverride: toolbarItemName[] | undefined }
+  >((props) => {
+    const [key, setKey] = React.useState(0)
 
-      /**
-       * Since slate keeps track of it's own state, and that state is an object rather
-       * than something easily memoizable like a string it can be tricky to ensure
-       * resets are properly handled. So we sneak in a callback to the form's reset
-       * logic that just remounts slate entirely
-       */
-      React.useMemo(() => {
-        const { reset } = props.form
-        props.form.reset = (initialValues) => {
-          setKey((key) => key + 1)
-          return reset(initialValues)
-        }
-      }, [])
+    /**
+     * Since slate keeps track of it's own state, and that state is an object rather
+     * than something easily memoizable like a string it can be tricky to ensure
+     * resets are properly handled. So we sneak in a callback to the form's reset
+     * logic that just remounts slate entirely
+     */
+    React.useMemo(() => {
+      const { reset } = props.form
+      props.form.reset = (initialValues) => {
+        setKey((key) => key + 1)
+        return reset(initialValues)
+      }
+    }, [])
 
-      return (
-        <EditorContext.Provider
-          key={key}
-          value={{
-            fieldName: props.field.name,
-            templates: props.field.templates,
-            rawMode: props.rawMode,
-            setRawMode: props.setRawMode,
-          }}
+    return (
+      <EditorContext.Provider
+        key={key}
+        value={{
+          fieldName: props.field.name,
+          templates: props.field.templates,
+          rawMode: props.rawMode,
+          setRawMode: props.setRawMode,
+        }}
+      >
+        <div
+          className={
+            'min-h-[100px] max-w-full tina-prose relative shadow-inner focus-within:shadow-outline focus-within:border-blue-500 block w-full bg-white border border-gray-200 text-gray-600 focus-within:text-gray-900 rounded-md px-3 py-2'
+          }
         >
-          <div
-            className={
-              'min-h-[100px] max-w-full tina-prose relative shadow-inner focus-within:shadow-outline focus-within:border-blue-500 block w-full bg-white border border-gray-200 text-gray-600 focus-within:text-gray-900 rounded-md px-3 py-2'
-            }
-          >
-            {props.rawMode ? props.rawEditor : <RichEditor {...props} />}
-          </div>
-        </EditorContext.Provider>
-      )
-    }
-  ),
+          {props.rawMode ? props.rawEditor : <RichEditor {...props} />}
+        </div>
+      </EditorContext.Provider>
+    )
+  }),
 }
