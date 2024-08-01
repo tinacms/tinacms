0351acf90a91b0a289e969c8d1238b0e38518d48
benjaminneoh <benjaminneoh6343@gmail.com>
2024-08-01 16:21:25 +1000

    temporary shadcn component

--- packages/tinacms/src/toolkit/fields/components/reference/to-remove/button.tsx
+++ packages/tinacms/src/toolkit/fields/components/reference/to-remove/button.tsx
@@ -1,18 +1,19 @@
 import * as React from 'react'
-import { cva, type VariantProps } from 'class-variance-authority'
 import { Slot } from '@radix-ui/react-slot'
+import { cva, type VariantProps } from 'class-variance-authority'
 
-//TODO : Move this button component to other sharedd folder, this button is created to replace the current shared button which have some css issue
+import { cn } from '../../../../../lib/utils' //TODO : improve this import path (breaking vite build without ../../../../../)
+
 const buttonVariants = cva(
   'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
+        default: 'bg-white text-primary-foreground hover:bg-white/90',
         destructive:
           'bg-destructive text-destructive-foreground hover:bg-destructive/90',
         outline:
-          'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
+          'border border-input bg-white hover:bg-white hover:text-accent-foreground',
         secondary:
           'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
@@ -43,7 +44,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     const Comp = asChild ? Slot : 'button'
     return (
       <Comp
-        // className={cn(buttonVariants({ variant, size, className }))}
+        className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
       />
