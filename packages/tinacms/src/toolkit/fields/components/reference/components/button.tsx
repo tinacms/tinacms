488c714990d42f5a8db9b1a74883920081d0cd41
benneoh <benneoh@ssw.com.au>
2024-08-02 16:43:31 +1000

    remove redundant css

--- packages/tinacms/src/toolkit/fields/components/reference/components/button.tsx
+++ packages/tinacms/src/toolkit/fields/components/reference/components/button.tsx
@@ -9,15 +9,8 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: 'bg-white text-primary-foreground hover:bg-white/90',
-        destructive:
-          'bg-destructive text-destructive-foreground hover:bg-destructive/90',
         outline:
           'border border-input bg-white hover:bg-white hover:text-accent-foreground',
-        secondary:
-          'bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
-        link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
         default: 'h-10 px-4 py-2',
@@ -27,7 +20,7 @@ const buttonVariants = cva(
       },
     },
     defaultVariants: {
-      variant: 'default',
+      variant: 'outline',
       size: 'default',
     },
   }
