2c28adb0a79655422cc61f9ce403a4180e739670
benneoh <benneoh@ssw.com.au>
2024-08-08 11:23:13 +1000

    lint fix and added related PBI for todo

--- packages/tinacms/src/toolkit/fields/components/reference/components/command.tsx
+++ packages/tinacms/src/toolkit/fields/components/reference/components/command.tsx
@@ -88,7 +88,7 @@ const CommandItem = React.forwardRef<
   <CommandPrimitive.Item
     ref={ref}
     className={cn(
-      "hover:bg-slate-100	 relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none aria-selected:bg-accent aria-selected:text-accent-foreground data-[disabled='true']:pointer-events-none data-[disabled='true']:opacity-50",
+      "hover:bg-slate-100	relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none aria-selected:bg-accent aria-selected:text-accent-foreground data-[disabled='true']:pointer-events-none data-[disabled='true']:opacity-50",
       className
     )}
     {...props}
