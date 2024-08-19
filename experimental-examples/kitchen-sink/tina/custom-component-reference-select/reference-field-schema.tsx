caa08249b7328484d4aaf76eaed72359be75471d
benneoh <benneoh@ssw.com.au>
2024-08-19 15:25:20 +1000

    typesafe filepath

--- experimental-examples/kitchen-sink/tina/custom-component-reference-select/reference-field-schema.tsx
+++ experimental-examples/kitchen-sink/tina/custom-component-reference-select/reference-field-schema.tsx
@@ -8,12 +8,18 @@ const referenceField = {
   type: 'reference',
   ui: {
     //TODO: Update the docs so remind or suggest the user to use type safe ? maybe we can link the rules there
-    selectComponents: (props) => {
+    selectComponents: (props, filepath: string) => {
       const { _collection, title, something } = props
 
       if (_collection === 'author') {
+        console.log('props author', props)
+        console.log('props author filename', filepath)
+
         return <CustomDisplayAuthorComponent something={something} />
       } else if (_collection === 'post') {
+        console.log('props post', props)
+        console.log('props post filename', filepath)
+
         return <CustomDisplayPostComponent title={title} />
       } else {
         return null // TODO: Handle null case
