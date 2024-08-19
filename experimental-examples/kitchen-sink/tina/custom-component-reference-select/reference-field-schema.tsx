8d07ebe893bfe3425b7e13e1ec9944dba16d4310
benneoh <benneoh@ssw.com.au>
2024-08-19 16:33:25 +1000

    refactor example project reference field

--- experimental-examples/kitchen-sink/tina/custom-component-reference-select/reference-field-schema.tsx
+++ experimental-examples/kitchen-sink/tina/custom-component-reference-select/reference-field-schema.tsx
@@ -1,28 +1,28 @@
 import React from 'react'
 import CustomDisplayAuthorComponent from '../../component/custom-reference-select-author'
 import CustomDisplayPostComponent from '../../component/custom-reference-select-post'
+import { CollectionProps, COLLECTIONS } from './model'
 
 const referenceField = {
   label: 'Author',
   name: 'author',
   type: 'reference',
   ui: {
-    //TODO: Update the docs so remind or suggest the user to use type safe ? maybe we can link the rules there
-    selectComponents: (props, filepath: string) => {
-      const { _collection, title, something } = props
+    selectComponents: (props: CollectionProps, filepath: string) => {
+      switch (props._collection) {
+        case COLLECTIONS.AUTHOR:
+          return (
+            <CustomDisplayAuthorComponent
+              name={props.name}
+              description={props.description}
+            />
+          )
 
-      if (_collection === 'author') {
-        console.log('props author', props)
-        console.log('props author filename', filepath)
+        case COLLECTIONS.POST:
+          return <CustomDisplayPostComponent title={props.title} />
 
-        return <CustomDisplayAuthorComponent something={something} />
-      } else if (_collection === 'post') {
-        console.log('props post', props)
-        console.log('props post filename', filepath)
-
-        return <CustomDisplayPostComponent title={title} />
-      } else {
-        return null // TODO: Handle null case
+        default:
+          return filepath
       }
     },
   },
