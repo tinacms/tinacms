59cd522adeb55db92092dddde60b35f2c3ae1178
benneoh <benneoh@ssw.com.au>
2024-09-13 12:42:25 +1000

    remove unused model

--- packages/tinacms/src/toolkit/fields/components/reference/index.tsx
+++ packages/tinacms/src/toolkit/fields/components/reference/index.tsx
@@ -2,42 +2,7 @@ import * as React from 'react'
 import { useCMS } from '@toolkit/react-tinacms/use-cms'
 import ReferenceSelect from './reference-select'
 import ReferenceLink from './reference-link'
-
-type Option = {
-  value: string
-  label: string
-}
-
-type ReferenceFieldOptions = {
-  optionComponent?: OptionComponent
-  experimental___filter?: (list: Array<any>, searchQuery: string) => Array<any>
-}
-
-type OptionComponent = (
-  props: unknown,
-  _sys: InternalSys
-) => React.ReactElement | string
-
-export interface InternalSys {
-  filename: string
-  path: string
-}
-
-export interface ReferenceFieldProps extends ReferenceFieldOptions {
-  label?: string
-  name: string
-  component: string
-  collections: string[]
-  options: (Option | string)[]
-}
-
-export interface ReferenceProps {
-  name: string
-  input: any
-  field: ReferenceFieldProps
-  disabled?: boolean
-  options?: (Option | string)[]
-}
+import { ReferenceProps } from './model/reference-props'
 
 export const Reference: React.FC<ReferenceProps> = ({ input, field }) => {
   const cms = useCMS()
