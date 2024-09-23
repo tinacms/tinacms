f73bf80a1fe16578d7fb07be45842853786149fa
benneoh <benneoh@ssw.com.au>
2024-09-23 16:54:07 +1000

    support dynamic value filter

---
+++ packages/tinacms/src/toolkit/fields/components/reference/utils/fetch-optioins-query-builder.ts
@@ -0,0 +1,28 @@
+export type StaticFilterValue = string[] | string // extend this type if needed
+export type ValueToFilterFunction = () => string
+// export type CollectionFilters = Record<string, Record<string, FilterValue>>
+
+type DynamicFieldFilterValue = StaticFilterValue | ValueToFilterFunction
+type FieldFilter = Record<string, DynamicFieldFilterValue>
+export type CollectionFilters = Record<string, FieldFilter>
+
+//Currently only support eq for filter, this function will loop thorugh the record and build the filter query
+export const filterQueryBuilder = (
+  fieldFilterConfig: FieldFilter,
+  collection: string
+) => {
+  return {
+    [collection]: Object.entries(fieldFilterConfig).reduce(
+      (acc, [key, value]) => {
+        // Check if value is a function
+        const filterValue = typeof value === 'function' ? value() : value
+
+        console.log('value', value)
+
+        acc[key] = { in: filterValue }
+        return acc
+      },
+      {} as Record<string, Record<string, StaticFilterValue>>
+    ),
+  }
+}
