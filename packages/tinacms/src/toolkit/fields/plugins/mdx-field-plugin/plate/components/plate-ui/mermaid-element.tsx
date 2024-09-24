d82a9df734b2fcdeef2aa75a3f16b92e40578fbb
JackDevAU <jpr178@live.com.au>
2024-09-24 15:09:52 +1000

    lightmode toggle

--- packages/tinacms/src/toolkit/fields/plugins/mdx-field-plugin/plate/components/plate-ui/mermaid-element.tsx
+++ packages/tinacms/src/toolkit/fields/plugins/mdx-field-plugin/plate/components/plate-ui/mermaid-element.tsx
@@ -1,15 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { withRef } from '@udecode/cn'
 import { PlateElement } from '@udecode/plate-common'
 import { useMermaidElement } from '../../hooks/use-mermaid-element'
-import { PencilIcon } from 'lucide-react'
+import { Moon, PencilIcon, SunMoon } from 'lucide-react'
 import { CodeBlock } from '../../plugins/ui/code-block'
 import { ELEMENT_MERMAID } from '../../plugins/custom/mermaid-plugin'
 
+const LightModeComponent = ({ onToggleMode }) => {
+  const [isLightMode, setIsLightMode] = useState(true)
+
+  // Toggle the light mode state
+  const handleToggle = () => {
+    setIsLightMode((prevMode) => !prevMode)
+  }
+
+  // Notify parent component of the class change
+  useEffect(() => {
+    const modeClass = isLightMode ? 'light-mode' : 'dark-mode'
+    if (onToggleMode) {
+      onToggleMode(modeClass)
+    }
+  }, [isLightMode, onToggleMode])
+
+  return (
+    <div>
+      <button
+        type="button"
+        onClick={handleToggle}
+        className="flex items-center w-5 h-5 text-gray-500 cursor-pointer"
+      >
+        {isLightMode ? <SunMoon /> : <Moon />}
+      </button>
+    </div>
+  )
+}
 export const MermaidElement = withRef<typeof PlateElement>(
   ({ children, nodeProps, element, ...props }, ref) => {
     const [isEditing, setIsEditing] = React.useState(false)
-    const [isLightMode, setIsLightMode] = React.useState(false)
     const [mermaidConfig, setMermaidConfig] = React.useState(
       element.value || ''
     )
@@ -30,11 +57,8 @@ export const MermaidElement = withRef<typeof PlateElement>(
                 setIsEditing(!isEditing)
               }}
             />
-            <PencilIcon
-              className="w-5 h-5 text-gray-500 cursor-pointer"
-              onClick={() => {
-                setIsLightMode(!isLightMode)
-              }}
+            <LightModeComponent
+              onToggleMode={() => console.log('mode changed')}
             />
           </div>
           {isEditing ? (
