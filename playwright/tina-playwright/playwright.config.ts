63c0d1bca219f939d099e15181495fe5e2872832
benneoh <benneoh@ssw.com.au>
2024-10-21 16:27:02 +1100

    update report type

--- playwright/tina-playwright/playwright.config.ts
+++ playwright/tina-playwright/playwright.config.ts
@@ -16,7 +16,6 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     trace: "on-first-retry",
