397c05f603f122178762afc923d26eea227694fa
brookjeynes-ssw <brookjeynes@ssw.com.au>
2025-01-22 15:08:40 +1000

    fix: add extra timeout to tests to accomodate pr check

--- playwright/tina-playwright/playwright.config.ts
+++ playwright/tina-playwright/playwright.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, devices } from "@playwright/test";
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  timeout: 10000,
+  timeout: 30000,
   reporter: [
     ["list", { printSteps: true }],
     ["json", { outputFile: "playwright-test-results.json" }],
