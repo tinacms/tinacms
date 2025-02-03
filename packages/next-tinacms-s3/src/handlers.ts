0b51a7c04c2b96d7012d9cb404eb28984ac6eea2
Kourosh Alasti <coding@kouroshalasti.com>
2025-02-03 01:05:56 -0800

    add node:* prefix :) (#5469)
    Co-authored-by: Jack Pettit [SSW] <57518417+JackDevAU@users.noreply.github.com>

--- packages/next-tinacms-s3/src/handlers.ts
+++ packages/next-tinacms-s3/src/handlers.ts
@@ -16,7 +16,7 @@ import {
 } from '@aws-sdk/client-s3'
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 import { Media, MediaListOptions } from 'tinacms'
-import path from 'path'
+import path from 'node:path'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export interface S3Config {
