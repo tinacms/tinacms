34ff41d488d113d378842045fefed88ecd030c9e
Kelly Davis <kldavis4@gmail.com>
2025-04-20 20:22:03 -0400

    generate code challenge

--- packages/tinacms/src/auth/authenticate.ts
+++ packages/tinacms/src/auth/authenticate.ts
@@ -2,10 +2,30 @@
 
 */
 
-import popupWindow from './popupWindow';
+import popupWindow from "./popupWindow";
 
-const TINA_LOGIN_EVENT = 'tinaCloudLogin';
-export const AUTH_TOKEN_KEY = 'tinacms-auth';
+const TINA_LOGIN_EVENT = "tinaCloudLogin";
+export const AUTH_TOKEN_KEY = "tinacms-auth";
+
+// Helper function to generate a random string
+const generateRandomString = (length) => {
+  const array = new Uint32Array(length);
+  window.crypto.getRandomValues(array);
+  return Array.from(array, (dec) => ("0" + dec.toString(16)).slice(-2)).join(
+    ""
+  );
+};
+
+// Helper function to generate a code challenge
+const generateCodeChallenge = async (codeVerifier) => {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(codeVerifier);
+  const digest = await window.crypto.subtle.digest("SHA-256", data);
+  return btoa(String.fromCharCode(...new Uint8Array(digest)))
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_")
+    .replace(/=+$/, "");
+};
 
 export type TokenObject = {
   id_token: string;
@@ -17,24 +37,30 @@ export const authenticate = (
   frontendUrl: string,
   oauth2?: boolean
 ): Promise<TokenObject> => {
-  return new Promise((resolve) => {
+  return new Promise(async (resolve) => {
     const origin = `${window.location.protocol}//${window.location.host}`;
     if (oauth2) {
+      // Generate a code verifier and code challenge
+      const codeVerifier = generateRandomString(32);
+      const codeChallenge = await generateCodeChallenge(codeVerifier);
+
+      // Save the code verifier in localStorage
+      localStorage.setItem("code_verifier", codeVerifier);
+
       const redirectUri = encodeURIComponent(`${origin}/admin/auth/callback`);
-      const codeChallenge = 'jksdfjdsklf';
       window.location.href = `${frontendUrl}/oauth-signin?redirect_uri=${redirectUri}&code_challenge=${codeChallenge}&client_id=${clientId}`;
       return;
     }
     const authTab = popupWindow(
       `${frontendUrl}/signin?clientId=${clientId}&origin=${origin}`,
-      '_blank',
+      "_blank",
       window,
       1000,
       700
     );
 
     // TODO - Grab this from the URL instead of passing through localstorage
-    window.addEventListener('message', function (e: MessageEvent) {
+    window.addEventListener("message", function (e: MessageEvent) {
       if (e.data.source === TINA_LOGIN_EVENT) {
         if (authTab) {
           authTab.close();
