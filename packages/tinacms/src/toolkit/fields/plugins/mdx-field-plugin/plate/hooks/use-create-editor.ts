0710575026b69271b8d42902b27824766103af2b
benjaminneoh <benjaminneoh6343@gmail.com>
2025-05-07 11:04:30 +1000

    remove unused using

--- packages/tinacms/src/toolkit/fields/plugins/mdx-field-plugin/plate/hooks/use-create-editor.ts
+++ packages/tinacms/src/toolkit/fields/plugins/mdx-field-plugin/plate/hooks/use-create-editor.ts
@@ -2,42 +2,9 @@
 
 import type { Value } from '@udecode/plate';
 
-import { withProps } from '@udecode/cn';
 import {
-  BoldPlugin,
-  CodePlugin,
-  ItalicPlugin,
-  StrikethroughPlugin,
-  SubscriptPlugin,
-  SuperscriptPlugin,
-  UnderlinePlugin,
-} from '@udecode/plate-basic-marks/react';
-import { BlockquotePlugin } from '@udecode/plate-block-quote/react';
-import {
-  CodeBlockPlugin,
-  CodeLinePlugin,
-  CodeSyntaxPlugin,
-} from '@udecode/plate-code-block/react';
-import { HEADING_KEYS } from '@udecode/plate-heading';
-import { HeadingPlugin, TocPlugin } from '@udecode/plate-heading/react';
-import { HorizontalRulePlugin } from '@udecode/plate-horizontal-rule/react';
-import { LinkPlugin } from '@udecode/plate-link/react';
-import { SlashInputPlugin } from '@udecode/plate-slash-command/react';
-import {
-  TableCellHeaderPlugin,
-  TableCellPlugin,
-  TablePlugin,
-  TableRowPlugin,
-} from '@udecode/plate-table/react';
-import {
-  type CreatePlateEditorOptions,
-  ParagraphPlugin,
-  PlateElement,
-  PlateLeaf,
-  usePlateEditor,
+  usePlateEditor
 } from '@udecode/plate/react';
-import { editorPlugins } from '../plugins/editor-plugins';
-import { withPlaceholders } from '../components/placeholder';
 
 export const useCreateEditor = ({
   plugins,
