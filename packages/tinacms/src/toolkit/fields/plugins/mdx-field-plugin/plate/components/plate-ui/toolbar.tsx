ec7b8421a485f92a87b865fe36718f39d6e36614
isaaclombardssw <isaaclombard@ssw.com.au>
2025-06-04 11:37:35 +1000

    toolbar styling width fixes, and title fix for more tools button

--- packages/tinacms/src/toolkit/fields/plugins/mdx-field-plugin/plate/components/plate-ui/toolbar.tsx
+++ packages/tinacms/src/toolkit/fields/plugins/mdx-field-plugin/plate/components/plate-ui/toolbar.tsx
@@ -1,55 +1,55 @@
-import * as React from 'react';
+import * as React from "react";
 
-import * as ToolbarPrimitive from '@radix-ui/react-toolbar';
-import { cn, withCn, withRef, withVariants } from '@udecode/cn';
-import { type VariantProps, cva } from 'class-variance-authority';
+import * as ToolbarPrimitive from "@radix-ui/react-toolbar";
+import { cn, withCn, withRef, withVariants } from "@udecode/cn";
+import { type VariantProps, cva } from "class-variance-authority";
 
-import { Icons } from './icons';
+import { Icons } from "./icons";
 
-import { Separator } from './separator';
-import { withTooltip } from './tooltip';
+import { Separator } from "./separator";
+import { withTooltip } from "./tooltip";
 
 export const Toolbar = withCn(
   ToolbarPrimitive.Root,
-  'relative flex select-none items-center gap-1 bg-background'
+  "relative flex select-none items-center gap-1 bg-background"
 );
 
 export const ToolbarToggleGroup = withCn(
   ToolbarPrimitive.ToolbarToggleGroup,
-  'flex items-center'
+  "flex items-center"
 );
 
 export const ToolbarLink = withCn(
   ToolbarPrimitive.Link,
-  'font-medium underline underline-offset-4'
+  "font-medium underline underline-offset-4"
 );
 
 export const ToolbarSeparator = withCn(
   ToolbarPrimitive.Separator,
-  'my-1 w-px shrink-0 bg-border'
+  "my-1 w-px shrink-0 bg-border"
 );
 
 const toolbarButtonVariants = cva(
   cn(
-    'inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
-    '[&_svg:not([data-icon])]:size-5'
+    "inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+    "[&_svg:not([data-icon])]:size-5"
   ),
   {
     defaultVariants: {
-      size: 'sm',
-      variant: 'default',
+      size: "sm",
+      variant: "default",
     },
     variants: {
       size: {
-        default: 'h-10 px-3',
-        lg: 'h-11 px-5',
-        sm: 'h-9 px-2',
+        default: "h-10 px-3",
+        lg: "h-11 px-5",
+        sm: "h-9 px-2",
       },
       variant: {
         default:
-          'bg-transparent hover:bg-muted hover:text-muted-foreground aria-checked:bg-accent aria-checked:text-accent-foreground',
+          "bg-transparent hover:bg-muted hover:text-muted-foreground aria-checked:bg-accent aria-checked:text-accent-foreground",
         outline:
-          'border border-input bg-transparent hover:bg-accent hover:text-accent-foreground',
+          "border border-input bg-transparent hover:bg-accent hover:text-accent-foreground",
       },
     },
   }
@@ -65,7 +65,7 @@ const ToolbarButton = withTooltip(
       showArrow?: boolean;
     } & Omit<
       React.ComponentPropsWithoutRef<typeof ToolbarToggleItem>,
-      'asChild' | 'value'
+      "asChild" | "value"
     > &
       VariantProps<typeof toolbarButtonVariants>
   >(
@@ -82,11 +82,11 @@ const ToolbarButton = withTooltip(
       },
       ref
     ) => {
-      return typeof pressed === 'boolean' ? (
+      return typeof pressed === "boolean" ? (
         <ToolbarToggleGroup
           disabled={props.disabled}
-          type='single'
-          value='single'
+          type="single"
+          value="single"
         >
           <ToolbarToggleItem
             className={cn(
@@ -94,18 +94,18 @@ const ToolbarButton = withTooltip(
                 size,
                 variant,
               }),
-              isDropdown && 'my-1 justify-between pr-1',
+              isDropdown && "my-1 justify-between",
               className
             )}
             ref={ref}
-            value={pressed ? 'single' : ''}
+            value={pressed ? "single" : ""}
             {...props}
           >
             {isDropdown && showArrow ? (
               <>
-                <div className='flex flex-1'>{children}</div>
+                <div className="flex flex-1">{children}</div>
                 <div>
-                  <Icons.arrowDown className='ml-0.5 size-4' data-icon />
+                  <Icons.arrowDown className="ml-0.5 size-4" data-icon />
                 </div>
               </>
             ) : (
@@ -120,7 +120,7 @@ const ToolbarButton = withTooltip(
               size,
               variant,
             }),
-            isDropdown && 'pr-1',
+            isDropdown && "pr-1",
             className
           )}
           ref={ref}
@@ -132,18 +132,18 @@ const ToolbarButton = withTooltip(
     }
   )
 );
-ToolbarButton.displayName = 'ToolbarButton';
+ToolbarButton.displayName = "ToolbarButton";
 
 export { ToolbarButton };
 
 export const ToolbarToggleItem = withVariants(
   ToolbarPrimitive.ToggleItem,
   toolbarButtonVariants,
-  ['variant', 'size']
+  ["variant", "size"]
 );
 
 export const ToolbarGroup = withRef<
-  'div',
+  "div",
   {
     noSeparator?: boolean;
   }
@@ -153,14 +153,14 @@ export const ToolbarGroup = withRef<
   if (!childArr || childArr.length === 0) return null;
 
   return (
-    <div className={cn('flex', className)} ref={ref}>
+    <div className={cn("flex", className)} ref={ref}>
       {!noSeparator && (
-        <div className='h-full py-1'>
-          <Separator orientation='vertical' />
+        <div className="h-full py-1">
+          <Separator orientation="vertical" />
         </div>
       )}
 
-      <div className='mx-1 flex items-center gap-1'>{children}</div>
+      <div className="mx-1 flex items-center gap-1">{children}</div>
     </div>
   );
 });
