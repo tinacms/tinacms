878e5ba42560f5e3932df8649e6186ba792430be
benjaminneoh <benjaminneoh6343@gmail.com>
2025-06-05 15:27:15 +1000

    move code block into folder and extract error display block to component

--- packages/tinacms/src/toolkit/fields/plugins/mdx-field-plugin/plate/components/plate-ui/code-block/code-block-element.tsx
+++ packages/tinacms/src/toolkit/fields/plugins/mdx-field-plugin/plate/components/plate-ui/code-block/code-block-element.tsx
@@ -12,9 +12,10 @@ import {
   CodeLineElement,
   CodeBlockElement as PlateCodeBlockElement,
 } from '@tinacms/mdx';
-import { Button } from './button';
-import { CodeBlockCombobox } from './code-block-combobox';
-import { MermaidElementWithRef } from './mermaid-element';
+import { Button } from '../button';
+import { CodeBlockCombobox } from '../code-block-combobox';
+import { MermaidElementWithRef } from '../mermaid-element';
+import { ErrorMessage } from './error-message';
 
 export function codeLineToString(content: PlateCodeBlockElement): string {
   return (content.children || [])
@@ -25,30 +26,6 @@ export function codeLineToString(content: PlateCodeBlockElement): string {
     )
     .join('\n');
 }
-
-const ErrorMsg = ({ error }) => {
-  if (!error) {
-    return null;
-  }
-
-  return (
-    <div
-      contentEditable={false}
-      className='mt-2 flex items-start rounded-md border border-red-300 bg-red-50 p-3 shadow-sm'
-      role='alert' // For accessibility
-    >
-      <div className='flex-shrink-0'>
-        <AlertTriangle className='h-5 w-5 text-red-400' aria-hidden='true' />
-      </div>
-      <div className='ml-3 flex-1'>
-        {/* Using <pre> to respect formatting (like newlines) in the error message */}
-        <pre className='m-0 font-mono text-sm text-red-700 whitespace-pre-wrap break-words'>
-          {error}
-        </pre>
-      </div>
-    </div>
-  );
-};
 
 //the default code block element from @udecode/plate-code-block
 //custom styling to match TinaCMS branding + remove copy button
@@ -128,7 +105,7 @@ export const CodeBlockElement = withRef<typeof PlateElement>(
           {isEditing ? (
             <pre className='overflow-x-auto p-4 pt-12 font-mono text-sm leading-[normal] [tab-size:2] print:break-inside-avoid my-2 tina-code-block'>
               <code>{children}</code>
-              <ErrorMsg error={mermaidError} />
+              <ErrorMessage error={mermaidError} />
             </pre>
           ) : (
             <MermaidElementWithRef
