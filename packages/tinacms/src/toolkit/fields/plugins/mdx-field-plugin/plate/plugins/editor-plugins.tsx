274158cbc74f8bb6974d2875eba10ee5253bda85
benjaminneoh <benjaminneoh6343@gmail.com>
2025-06-12 12:27:38 +1000

    remove soft break for blockquote

--- packages/tinacms/src/toolkit/fields/plugins/mdx-field-plugin/plate/plugins/editor-plugins.tsx
+++ packages/tinacms/src/toolkit/fields/plugins/mdx-field-plugin/plate/plugins/editor-plugins.tsx
@@ -224,7 +224,7 @@ export const editorPlugins = [
         {
           hotkey: 'enter',
           query: {
-            allow: [CodeBlockPlugin.key, BlockquotePlugin.key],
+            allow: [CodeBlockPlugin.key],
           },
         },
       ],
