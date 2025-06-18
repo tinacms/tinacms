6dd73d4883940d8151285c111c8bbb658cbf6823
Eduard <73804268+eduardf4s@users.noreply.github.com>
2025-06-18 01:19:24 +0000

    [streamliners-main] Streamliners logo and page titles

--- packages/@tinacms/cli/src/next/commands/dev-command/html.ts
+++ packages/@tinacms/cli/src/next/commands/dev-command/html.ts
@@ -48,7 +48,7 @@ export const devHTML = (port: string) => `<!DOCTYPE html>
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>TinaCMS</title>
+    <title>Streamliners Publishing</title>
   </head>
 
   <!-- if development -->
