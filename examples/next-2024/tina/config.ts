3bf55dcc9a3f3726ef53580b435c6dd45713e33a
Matt Wicks <mattwicks@tina.io>
2025-06-19 14:29:30 +1000

    chore: add schema to repro different breadcrumb issue conditions

--- examples/next-2024/tina/config.ts
+++ examples/next-2024/tina/config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'tinacms';
+import PathwaySchema, { contentBlock } from './collection/bugReproduction';
 
 // Your hosting provider likely exposes this as an environment variable
 const branch =
@@ -28,6 +29,196 @@ export default defineConfig({
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
   schema: {
     collections: [
+      {
+        name: 'page',
+        path: 'content/page',
+        label: 'Page',
+        format: 'mdx',
+        frontmatterFormat: 'toml',
+        frontmatterDelimiters: ['+++', '+++'],
+        ui: {
+          filename: {
+            description:
+              'The name of the <span style="text-decoration:underline;font-weight:bold">file</span>',
+            showFirst: true,
+          },
+          // router: ({ document }) => {
+          //   if (document._sys.breadcrumbs[0] === 'home') {
+          //     return '/';
+          //   }
+          //   return `/${document._sys.filename}`;
+          // },
+          // Example of beforeSubmit
+          beforeSubmit: async ({ values, cms, form, tinaForm }) => {
+            return {
+              ...values,
+              lastUpdated: new Date().toISOString(),
+            };
+          },
+        },
+        fields: [
+          {
+            label: 'Title',
+            name: 'Title',
+            type: 'string',
+            ui: {
+              description:
+                'The title of the <span style="text-decoration:underline;font-weight:bold">page</span>',
+              // defaultValue: 'Title',
+              // Examples of how you COULD use a custom form
+              // component: ({ form, field, input }) => {
+              //   return (
+              //     <div>
+              //       <label>
+              //         This is a test React
+              //       </label>
+              //       <input {...input}></input>
+              //     </div>
+              //   )
+              // },
+              // validate: (val) => {
+              //   if (val?.length > 5) {
+              //     return 'Too Long!!!'
+              //   }
+              // },
+            },
+          },
+          {
+            name: 'lastUpdated',
+            type: 'datetime',
+            ui: {
+              component: 'hidden',
+            },
+          },
+          {
+            name: 'body',
+            label: 'Body',
+            type: 'rich-text',
+            isBody: true,
+            templates: [
+              {
+                name: 'apiReference',
+                label: 'API Reference',
+                fields: [
+                  {
+                    type: 'string',
+                    name: 'title',
+                    label: 'Title',
+                  },
+                  {
+                    type: 'object',
+                    name: 'property',
+                    label: 'Property',
+                    list: true,
+                    ui: {
+                      itemProps: (item) => {
+                        return {
+                          label: item.groupName
+                            ? `📂 ${item.groupName} | ${item.name}`
+                            : item.name,
+                        };
+                      },
+                    },
+                    fields: [
+                      {
+                        type: 'string',
+                        name: 'groupName',
+                        label: 'Group Name',
+                        description:
+                          'Adjacent properties with the same group name will be grouped together',
+                      },
+                      {
+                        type: 'string',
+                        name: 'name',
+                        label: 'Name',
+                      },
+                      {
+                        type: 'rich-text',
+                        name: 'description',
+                        label: 'Description',
+                      },
+                      {
+                        type: 'string',
+                        name: 'type',
+                        label: 'Type',
+                      },
+                      {
+                        type: 'string',
+                        name: 'default',
+                        label: 'Default',
+                      },
+                      {
+                        type: 'boolean',
+                        name: 'required',
+                        label: 'Required',
+                      },
+                      {
+                        type: 'boolean',
+                        name: 'experimental',
+                        label: 'Experimental',
+                      },
+                    ],
+                  },
+                ],
+              },
+              {
+                name: 'WarningCallout',
+                label: 'WarningCallout',
+                match: {
+                  start: '{%',
+                  end: '%}',
+                },
+                fields: [
+                  {
+                    name: 'text',
+                    label: 'Text',
+                    type: 'string',
+                    required: true,
+                    ui: {
+                      component: 'textarea',
+                    },
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+      contentBlock,
+      PathwaySchema,
+      {
+        name: 'slateJson',
+        path: 'content/slate-json',
+        format: 'json',
+        fields: [
+          {
+            name: 'title',
+            type: 'string',
+            label: 'Title',
+          },
+          {
+            type: 'rich-text',
+            name: 'body',
+            label: 'Body',
+            parser: { type: 'slatejson' },
+            templates: [
+              {
+                name: 'DateTime',
+                label: 'Date & Time',
+                inline: true,
+                fields: [
+                  {
+                    name: 'format',
+                    label: 'Format',
+                    type: 'string',
+                    options: ['utc', 'iso', 'local'],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
       {
         name: 'simpleRef',
         path: 'content/refs',
