0850c7e78d8a237c8c14a4feb98248e736767764
Eli Kent <kulewidak@gmail.com>
2025-07-07 15:09:15 +1000

    ✨ feat: integrate ProgressIndicatorTest component for development in TinaCMSProvider

--- packages/tinacms/src/tina-cms.tsx
+++ packages/tinacms/src/tina-cms.tsx
@@ -5,6 +5,7 @@ import { LocalClient } from './internalClient/index';
 import { useDocumentCreatorPlugin } from './hooks/use-content-creator';
 import { parseURL } from '@tinacms/schema-tools';
 import type { TinaCMSProviderDefaultProps } from './types/cms';
+import { ProgressIndicatorTest } from '@toolkit/form-builder/ProgressIndicatorTest';
 
 const errorButtonStyles = {
   background: '#eb6337',
@@ -180,6 +181,10 @@ export const TinaCMSProvider2 = ({
 
   return (
     <>
+    {/* Progress Indicator Test Component - Always visible for development */}
+    <div style={{ position: 'fixed', top: '16px', right: '16px', zIndex: 10000 }}>
+        <ProgressIndicatorTest />
+      </div>  
       <TinaCloudProvider
         branch={branch}
         clientId={clientId || schema?.config?.clientId}
