d9eda55c342a5b11ec4b67a3e0cf5c641871bf47
Nick Curran [SSW] <132531112+ncn-ssw@users.noreply.github.com>
2025-07-16 15:00:05 +1000

    Adds test for filtering on dates.

---
+++ packages/@tinacms/graphql/tests/date-filtering-query/tina/config.ts
@@ -0,0 +1,38 @@
+/**
+ * Date Filtering Query Test - TinaCMS Schema Configuration
+ *
+ * Focused schema for testing date filtering operations (after, before, between)
+ * on the releaseDate field.
+ */
+
+import { Schema } from '@tinacms/schema-tools';
+
+export const schema: Schema = {
+  collections: [
+    {
+      label: 'Movie',
+      name: 'movie',
+      path: 'movies',
+      fields: [
+        {
+          name: 'title',
+          label: 'Title',
+          type: 'string',
+        },
+        {
+          name: 'releaseDate',
+          label: 'Release Date',
+          type: 'datetime',
+        },
+      ],
+      indexes: [
+        {
+          name: 'releaseDate',
+          fields: [{ name: 'releaseDate' }],
+        },
+      ],
+    },
+  ],
+};
+
+export default { schema };
