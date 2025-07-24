b5f94eefcbe4a5c6e0eb0d52f957e5dcb31b1bc2
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2025-07-24 16:40:25 +0200

    Add unit tests for stripNativeTrailingSlash

---
+++ packages/@tinacms/cli/src/utils/path.test.ts
@@ -0,0 +1,82 @@
+import type { stripNativeTrailingSlash as StripFn } from './path';
+
+describe('stripNativeTrailingSlash under Windows', () => {
+  let stripNativeTrailingSlash: typeof StripFn;
+
+  beforeAll(() => {
+    const winPath = require('path').win32;
+    jest.doMock('path', () => ({
+      __esModule: true,
+      default: winPath,
+    }));
+
+    stripNativeTrailingSlash = require('./path').stripNativeTrailingSlash;
+  });
+
+  afterAll(() => {
+    jest.resetModules();
+    jest.unmock('path');
+  });
+
+  it('strips single trailing backslash "\\"', () => {
+    expect(stripNativeTrailingSlash('C:\\development\\tina\\')).toBe(
+      'C:\\development\\tina'
+    );
+  });
+  it('strips multiple trailing backslashes "\\"', () => {
+    expect(stripNativeTrailingSlash('C:\\development\\tina\\\\\\\\\\\\')).toBe(
+      'C:\\development\\tina'
+    );
+  });
+  it('doesn\'t strip a forward slash "/"', () => {
+    expect(stripNativeTrailingSlash('C:\\development\\tina/')).toBe(
+      'C:\\development\\tina/'
+    );
+  });
+  it('doesn\'t strip the "C:\\" root', () => {
+    expect(stripNativeTrailingSlash('C:\\')).toBe('C:\\');
+  });
+  it('returns empty string on empty path', () => {
+    expect(stripNativeTrailingSlash('')).toBe('');
+  });
+});
+
+describe('stripNativeTrailingSlash under POSIX/UNIX', () => {
+  let stripNativeTrailingSlash: typeof StripFn;
+
+  beforeAll(() => {
+    const posixPath = require('path').posix;
+    jest.doMock('path', () => ({
+      __esModule: true,
+      default: posixPath,
+    }));
+    stripNativeTrailingSlash = require('./path').stripNativeTrailingSlash;
+  });
+
+  afterAll(() => {
+    jest.resetModules();
+    jest.unmock('path');
+  });
+
+  it('strips single trailing forward slash "/"', () => {
+    expect(stripNativeTrailingSlash('/home/user/development/tina/')).toBe(
+      '/home/user/development/tina'
+    );
+  });
+  it('strips multiple trailing forward slashes "/"', () => {
+    expect(stripNativeTrailingSlash('/home/user/development/tina//////')).toBe(
+      '/home/user/development/tina'
+    );
+  });
+  it('doesn\'t strip a backslash "\\"', () => {
+    expect(stripNativeTrailingSlash('/home/user/development/tina\\')).toBe(
+      '/home/user/development/tina\\'
+    );
+  });
+  it('doesn\'t strip the "/" root', () => {
+    expect(stripNativeTrailingSlash('/')).toBe('/');
+  });
+  it('returns empty string on empty path', () => {
+    expect(stripNativeTrailingSlash('')).toBe('');
+  });
+});
