6fa22f23dd4ba9f11402cf8ad8f958135a709919
Josh Berman [SSW] <JoshBerman@ssw.com.au>
2025-08-05 09:14:46 +1000

    rm conflict const

--- packages/create-tina-app/src/index.ts
+++ packages/create-tina-app/src/index.ts
@@ -21,14 +21,6 @@ import { PackageManager, PKG_MANAGERS } from './util/packageManagers';
 import validate from 'validate-npm-package-name';
 import { THEMES } from './themes';
 import { writeFile } from 'fs/promises';
-
-/**
- * The available package managers a user can use.
- * To add a new supported package manager, add the usage command to this list.
- * The `PackageManager` type will be automatically updated as a result.
- */
-const PKG_MANAGERS = ['npm', 'yarn', 'pnpm', 'bun'] as const;
-export type PackageManager = (typeof PKG_MANAGERS)[number];
 import * as ascii from './util/asciiArt';
 
 export async function run() {
