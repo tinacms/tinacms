6bd1008bad78602498e5773df701b68de4a86885
Nick Curran <6274048+n-crrn@users.noreply.github.com>
2025-08-05 01:30:46 +0000

    Adds first draft of resolver/auth-fields module.

---
+++ packages/@tinacms/graphql/src/resolver/auth-fields.ts
@@ -0,0 +1,210 @@
+import type { TinaSchema } from '@tinacms/schema-tools';
+import type { GraphQLResolveInfo } from 'graphql';
+import { get } from '../util';
+import set from 'lodash.set';
+import { checkPasswordHash, mapUserFields } from '../auth/utils';
+
+export interface AuthContext {
+  sub?: string;
+}
+
+export interface AuthenticateArgs {
+  sub?: string;
+  password: string;
+}
+
+export interface UpdatePasswordArgs {
+  password: string;
+}
+
+export interface AuthFieldsParams {
+  tinaSchema: TinaSchema;
+  resolver: any;
+  args: any;
+  context: any;
+  info: GraphQLResolveInfo;
+  ctxUser?: AuthContext;
+}
+
+export async function handleAuthenticate({
+  tinaSchema,
+  resolver,
+  args,
+  ctxUser,
+}: AuthFieldsParams): Promise<any> {
+  const sub = args.sub || ctxUser?.sub;
+  const collection = tinaSchema
+    .getCollections()
+    .find((c) => c.isAuthCollection);
+  if (!collection) {
+    throw new Error('Auth collection not found');
+  }
+
+  const userFields = mapUserFields(collection, ['_rawData']);
+  if (!userFields.length) {
+    throw new Error(
+      `No user field found in collection ${collection.name}`
+    );
+  }
+  if (userFields.length > 1) {
+    throw new Error(
+      `Multiple user fields found in collection ${collection.name}`
+    );
+  }
+  const userField = userFields[0];
+
+  const realPath = `${collection.path}/index.json`;
+  const userDoc = await resolver.getDocument(realPath);
+  const users = get(userDoc, userField.path);
+  if (!users) {
+    throw new Error('No users found');
+  }
+  const { idFieldName, passwordFieldName } = userField;
+  if (!idFieldName) {
+    throw new Error('No uid field found on user field');
+  }
+  const user = users.find((u) => u[idFieldName] === sub);
+  if (!user) {
+    return null;
+  }
+
+  const saltedHash = get(user, [passwordFieldName || '', 'value']);
+  if (!saltedHash) {
+    throw new Error('No password field found on user field');
+  }
+
+  const matches = await checkPasswordHash({
+    saltedHash,
+    password: args.password,
+  });
+
+  if (matches) {
+    return user;
+  }
+  return null;
+}
+
+export async function handleAuthorize({
+  tinaSchema,
+  resolver,
+  args,
+  ctxUser,
+}: AuthFieldsParams): Promise<any> {
+  const sub = args.sub || ctxUser?.sub;
+  const collection = tinaSchema
+    .getCollections()
+    .find((c) => c.isAuthCollection);
+  if (!collection) {
+    throw new Error('Auth collection not found');
+  }
+
+  const userFields = mapUserFields(collection, ['_rawData']);
+  if (!userFields.length) {
+    throw new Error(
+      `No user field found in collection ${collection.name}`
+    );
+  }
+  if (userFields.length > 1) {
+    throw new Error(
+      `Multiple user fields found in collection ${collection.name}`
+    );
+  }
+  const userField = userFields[0];
+
+  const realPath = `${collection.path}/index.json`;
+  const userDoc = await resolver.getDocument(realPath);
+  const users = get(userDoc, userField.path);
+  if (!users) {
+    throw new Error('No users found');
+  }
+  const { idFieldName } = userField;
+  if (!idFieldName) {
+    throw new Error('No uid field found on user field');
+  }
+  const user = users.find((u) => u[idFieldName] === sub);
+  if (!user) {
+    return null;
+  }
+
+  return user;
+}
+
+export async function handleUpdatePassword({
+  tinaSchema,
+  resolver,
+  args,
+  ctxUser,
+}: AuthFieldsParams): Promise<boolean> {
+  if (!ctxUser?.sub) {
+    throw new Error('Not authorized');
+  }
+
+  if (!args.password) {
+    throw new Error('No password provided');
+  }
+
+  const collection = tinaSchema
+    .getCollections()
+    .find((c) => c.isAuthCollection);
+  if (!collection) {
+    throw new Error('Auth collection not found');
+  }
+
+  const userFields = mapUserFields(collection, ['_rawData']);
+  if (!userFields.length) {
+    throw new Error(
+      `No user field found in collection ${collection.name}`
+    );
+  }
+  if (userFields.length > 1) {
+    throw new Error(
+      `Multiple user fields found in collection ${collection.name}`
+    );
+  }
+  const userField = userFields[0];
+  const realPath = `${collection.path}/index.json`;
+  const userDoc = await resolver.getDocument(realPath);
+  const users = get(userDoc, userField.path);
+  if (!users) {
+    throw new Error('No users found');
+  }
+  const { idFieldName, passwordFieldName } = userField;
+  const user = users.find((u) => u[idFieldName] === ctxUser.sub);
+  if (!user) {
+    throw new Error('Not authorized');
+  }
+
+  user[passwordFieldName] = {
+    value: args.password,
+    passwordChangeRequired: false,
+  };
+
+  const params = {};
+  set(
+    params,
+    userField.path.slice(1), // remove _rawData from users path
+    users.map((u) => {
+      if (user[idFieldName] === u[idFieldName]) {
+        return user;
+      }
+      return {
+        // don't overwrite other users' passwords
+        ...u,
+        [passwordFieldName]: {
+          ...u[passwordFieldName],
+          value: '',
+        },
+      };
+    })
+  );
+
+  await resolver.updateResolveDocument({
+    collection,
+    args: { params },
+    realPath,
+    isCollectionSpecific: true,
+    isAddPendingDocument: false,
+  });
+
+  return true;
+}
