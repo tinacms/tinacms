29c06c274ab052767b6dd18e2012791825b60017
Josh Berman [SSW] <JoshBerman@ssw.com.au>
2025-09-19 14:35:49 +1000

    add info alert

--- packages/tinacms/src/toolkit/react-sidebar/components/alert.tsx
+++ packages/tinacms/src/toolkit/react-sidebar/components/alert.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { AiFillWarning } from 'react-icons/ai';
 import { cn } from '../../../lib/utils';
+import { MdInfo } from 'react-icons/md';
 
 export const Alert = ({
   children,
@@ -9,21 +10,28 @@ export const Alert = ({
   ...props
 }: {
   children?: React.ReactNode;
-  alertStyle?: 'warning';
+  alertStyle?: 'warning' | 'info';
 } & React.HTMLProps<HTMLDivElement>) => {
+  const commonAlertStyles = 'ml-8 text-sm px-4 py-2 rounded-md border';
+
   const styles = {
     warning:
-      'ml-8 text-sm px-4 py-2 text-amber-700 bg-amber-100 rounded border border-amber-700/20',
+      ` text-amber-700 bg-amber-100 border-amber-700/20`,
+    info:
+      ` text-[#2563EB] bg-[#1C4ED8]/20 border-[#2563EB]/20`,
   };
 
   const icon = {
     warning: (
       <AiFillWarning className='w-5 h-auto inline-block mr-1 opacity-70 text-amber-600' />
     ),
+    info: (
+      <MdInfo className='w-5 h-auto inline-block mr-1 opacity-70 text-[#2563EB]' />
+    ),
   };
 
   return (
-    <div className={cn(styles[alertStyle], className)} {...props}>
+    <div className={cn(commonAlertStyles, styles[alertStyle], className)} {...props}>
       {icon[alertStyle]} {children}
     </div>
   );
