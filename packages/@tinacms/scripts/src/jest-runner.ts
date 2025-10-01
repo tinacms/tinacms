a0d04f1c0cd2eec55023404f84a01ff13fa834e6
Ben Neoh [SSW] <benjaminneoh6343@gmail.com>
2025-10-01 12:58:31 +1000

    update format

--- packages/@tinacms/scripts/src/jest-runner.ts
+++ packages/@tinacms/scripts/src/jest-runner.ts
@@ -7,7 +7,8 @@ const config: Config = {
   },
   testPathIgnorePatterns: ['/dist/'],
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/../../__mocks__/fileMock.js',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/../../__mocks__/fileMock.js',
     '\\.(css|less|scss|sass)$': '@tinacms/scripts/__mocks__/styleMock.js',
   },
 };
