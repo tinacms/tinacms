c4297aa84f81b2030b7dbe7376813dd7001c7234
Eli Kent [SSW] <69125238+kulesy@users.noreply.github.com>
2025-11-14 16:19:28 +1100

    Remove min text from timer

--- packages/tinacms/src/toolkit/form-builder/create-branch-modal.tsx
+++ packages/tinacms/src/toolkit/form-builder/create-branch-modal.tsx
@@ -84,7 +84,7 @@ export const CreateBranchModal = ({
   const formatTime = (seconds: number) => {
     const mins = Math.floor(seconds / 60);
     const secs = seconds % 60;
-    return `${mins}:${secs.toString().padStart(2, '0')} min`;
+    return `${mins}:${secs.toString().padStart(2, '0')}`;
   };
 
   const steps = [
