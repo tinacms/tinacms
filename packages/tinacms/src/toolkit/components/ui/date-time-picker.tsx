5438ccbf8801983a379706f98e59590d17713d43
Calinator444 <calebwilliams@ssw.com.au>
2025-11-24 18:16:31 +1100

    Fixed weird calendar icon bug

--- packages/tinacms/src/toolkit/components/ui/date-time-picker.tsx
+++ packages/tinacms/src/toolkit/components/ui/date-time-picker.tsx
@@ -6,7 +6,7 @@ import 'moment-timezone';
 import { add, //format, 
   Locale } from 'date-fns';
 import { enUS } from 'date-fns/locale';
-import { Calendar as ChevronLeft, ChevronRight } from 'lucide-react';
+import {  ChevronLeft, ChevronRight } from 'lucide-react';
 import { Clock } from 'lucide-react';
 import * as React from 'react';
 import { useImperativeHandle, useRef } from 'react';
