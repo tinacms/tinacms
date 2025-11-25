af8b7168e0109de7057e770843d1e29dceab1d0b
Calinator444 <calebwilliams@ssw.com.au>
2025-11-25 14:42:49 +1100

    added date picker button in the style of Tina

--- packages/tinacms/src/toolkit/components/ui/date-time-picker.tsx
+++ packages/tinacms/src/toolkit/components/ui/date-time-picker.tsx
@@ -1,4 +1,6 @@
 import { Button, buttonVariants } from './button';
+import { RotateCw } from 'lucide-react';
+import { Calendar as CalendarSVG } from 'lucide-react';
 import { Input } from './input';
 import { Popover, PopoverContent, PopoverTrigger } from './popover';
 import moment from 'moment';
@@ -787,12 +789,17 @@ const DateTimePicker = React.forwardRef<Partial<DateTimePickerRef>, DateTimePick
     }
 
     return (
-
-
-      
       <Popover open={open} onOpenChange={setOpen}> 
         <PopoverTrigger asChild disabled={disabled}>
-          <BaseTextField className='text-left' ref={buttonRef} value={displayDate? formatCurrentDate({dateFormat, displayDate, }): placeholder} />
+          <div ref={buttonRef} tabIndex={0} className='text-xs pointer overflow-hidden group hover:text-gray-600 cursor-pointer rounded border border-gray-100 flex font-semibold shadow group transition-colors bg-white text-gray-500'>
+            <div className='my-auto relative size-7'>
+              <CalendarSVG className='absolute size-5 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-in size-10 transition-colors' />
+            </div>
+            <span className='my-auto group-hover:text-blue-600'>{displayDate? formatCurrentDate({dateFormat, displayDate, }): placeholder}</span>
+            <button  className='ml-auto px-1 w-8 py-2.5 hover:text-blue-600 hover:text-inherit text-gray-200 flex items-center justify-center hover:bg-gray-50'>
+              <RotateCw className='h-5 p-0.5 w-auto  ' />
+            </button>
+          </div>
         </PopoverTrigger>
         
         <PopoverContent 
