50fa9f4bf932a3dc45b88d245e35917efe8d3423
Pat Stuart [SSW] <71543754+Pat-Stuart@users.noreply.github.com>
2025-12-02 09:09:34 +1100

    💄 Nav menu UI bugfix (#6182)
    [Relevant
    PBI](https://github.com/orgs/tinacms/projects/38?pane=issue&itemId=139252958&issue=tinacms%7Ctinacms%7C6118)
    
    This PR implements a context and trigger for the nav menu. 
    This allows us to fix a UI bug in which the menu icon took up an entire
    column of the screen pushing all content to the side.
    
    This is a stripped back, different approach to the refactor I did.
    
    From:
    
    <img width="1920" height="916" alt="image"
    src="https://github.com/user-attachments/assets/09ba5f3c-055a-49ce-b9bd-773452134d38"
    />
    
    
    To:
    
    <img width="1920" height="916" alt="image"
    src="https://github.com/user-attachments/assets/aa52d8c1-dc63-4712-ae8e-8416edbed35b"
    />
    
    From:
    
    <img width="778" height="760" alt="image"
    src="https://github.com/user-attachments/assets/88261c16-4e18-4629-b53e-ab6601386705"
    />
    
    
    To:
    
    <img width="772" height="781" alt="image"
    src="https://github.com/user-attachments/assets/8af7123b-0c66-479e-bd9f-cd4c318d4bda"
    />
    
    <img width="789" height="763" alt="image"
    src="https://github.com/user-attachments/assets/048717a3-995a-4475-81d5-bd091f18416f"
    />

--- packages/tinacms/src/toolkit/index.ts
+++ packages/tinacms/src/toolkit/index.ts
@@ -34,6 +34,10 @@ export * from '@toolkit/icons';
 export * from '@toolkit/react-dismissible';
 export {
   Nav,
+  NavCloudLink,
+  NavProvider,
+  NavMenuTrigger,
+  useNav,
   LocalWarning,
   BillingWarning,
   SyncStatusButton,
--- packages/tinacms/src/toolkit/react-sidebar/index.ts
+++ packages/tinacms/src/toolkit/react-sidebar/index.ts
@@ -3,3 +3,6 @@ export { SidebarProvider } from './components/sidebar';
 export { Nav } from './components/nav';
 export { LocalWarning, BillingWarning } from './components/local-warning';
 export { SyncStatusButton } from './components/sync-status';
+export { NavCloudLink } from './components/nav-components';
+export { NavProvider, useNav } from './components/nav-context';
+export { NavMenuTrigger } from './components/NavMenuTrigger';
