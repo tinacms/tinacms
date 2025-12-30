b567187f9b6f227f7b34cc30130ea4078e675ddf
brookjeynes-ssw <brookjeynes@ssw.com.au>
2025-12-30 14:04:00 +1000

    feat(posthog): add posthog event constants

---
+++ packages/tinacms/src/lib/posthog/posthog.ts
@@ -0,0 +1,12 @@
+export const CreateBranchStartedEvent: string = 'create-branch-started';
+export const CreateBranchFinishedEvent: string = 'create-branch-finished';
+export const BranchSwitchedEvent: string = 'branch-switched';
+export const SavedContentEvent: string = 'saved-content';
+export const MediaManagerContentUploadedEvent: string =
+  'media-manager-content-uploaded';
+export const MediaManagerContentDeletedEvent: string =
+  'media-manager-content-deleted';
+export const EditorSwitchedToMarkdownEvent: string =
+  'editor-switched-to-markdown';
+export const EditorSwitchedToRichtextEvent: string =
+  'editor-switched-to-richtext';
