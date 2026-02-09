5e8440e8b601d35e17d5a8009541c91fcdc0f81b
Josh Berman [SSW] <JoshBerman@ssw.com.au>
2026-02-10 08:55:34 +1100

    add payload

--- packages/tinacms/src/lib/posthog/posthog.ts
+++ packages/tinacms/src/lib/posthog/posthog.ts
@@ -1,39 +1,65 @@
 // User begins creating branch
-export const CreateBranchStartedEvent: string = 'create-branch-started';
+export const CreateBranchEvent: string = 'create-branch';
+export type CreateBranchPayload = {
+  branchName: string;
+  status: 'indexed' | 'unindexed';
+};
 
 // The user finishes creating a branch
-export const CreateBranchFinishedEvent: string = 'create-branch-finished';
+export const CreatePullRequestEvent: string = 'create-pull-request';
+export type CreatePullRequestPayload = {
+  pullRequestUrl: string;
+  branchName: string;
+  createdAt: string;
+};
 
-// When the user switches branches 
+export const FinishPullRequestEvent: string = 'finish-pull-request';
+export type FinishPullRequestPayload = {
+  pullRequestUrl: string;
+  branchName: string;
+  title: string;
+  description: string;
+  createdAt: string;
+  updatedAt: string;
+  status: string;
+}
+
+// When the user switches branches
 export const BranchSwitchedEvent: string = 'branch-switched';
+export type BranchSwitchedPayload = {
+  branchName: string;
+};
 
-// When a user clicks 'save' in the TinaCMS Editor 
+// When a user clicks 'save' in the TinaCMS Editor
 export const SavedContentEvent: string = 'saved-content';
+export type SavedContentPayload = {
+  collection?: string;
+  documentPath?: string;
+};
 
-// When a user adds an item to the media manager 
+// When a user adds an item to the media manager
 export const MediaManagerContentUploadedEvent: string =
   'media-manager-content-uploaded';
+export type MediaManagerContentUploadedPayload = Record<string, never>;
 
-// When a user deletes an item from the media manager 
+// When a user deletes an item from the media manager
 export const MediaManagerContentDeletedEvent: string =
   'media-manager-content-deleted';
+export type MediaManagerContentDeletedPayload = Record<string, never>;
 
-// The format of the content uploaded to the media manager (.webm, .png, etc)
-export const MediaManagerContentFormat: string = 
-  'media-manager-content-type';
-
-// When a rich-text field is switched to Markdown mode (from Rich-text mode)
-export const EditorSwitchedToMarkdownEvent: string =
-  'editor-switched-to-markdown';
-
-// When a rich-text field is switched to Rich-text mode (from Markdown mode)
-export const EditorSwitchedToRichtextEvent: string =
-  'editor-switched-to-richtext';
+// Switching between raw and rich-text editor modes
+export const RichTextEditorSwitchedEvent: string =
+  'rich-text-editor-switched';
+export type RichTextEditorSwitchedPayload = {
+  to: 'markdown' | 'richtext';
+};
 
 // When the user navigates to Project Config (TinaCloud) from the TinaCMS editor
-export const ProjectConfigNavigatedToFromWebsiteEvent: string = 
+export const ProjectConfigNavigatedToFromWebsiteEvent: string =
   'cloud-navigated-to-from-website';
+export type ProjectConfigNavigatedToFromWebsitePayload = Record<string, never>;
 
 // When the user navigates to user management (TinaCloud) from the TinaCMS editor
-export const UserManagementNavigatedToFromWebsiteEvent: string = 
+export const UserManagementNavigatedToFromWebsiteEvent: string =
   'user-management-navigated-to-from-website';
+export type UserManagementNavigatedToFromWebsitePayload = Record<string, never>;
