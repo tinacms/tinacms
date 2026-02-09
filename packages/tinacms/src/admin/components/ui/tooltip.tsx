80e910f783d0062907cc7df7fdf931530943b54c
Josh Berman [SSW] <JoshBerman@ssw.com.au>
2026-02-09 12:32:54 +1100

    format

--- packages/tinacms/src/admin/components/ui/tooltip.tsx
+++ packages/tinacms/src/admin/components/ui/tooltip.tsx
@@ -1,7 +1,7 @@
 import * as TooltipPrimitive from '@radix-ui/react-tooltip';
 import * as React from 'react';
 
-import { cn } from '@utils/cn'
+import { cn } from '@utils/cn';
 
 const TooltipProvider = TooltipPrimitive.Provider;
 
