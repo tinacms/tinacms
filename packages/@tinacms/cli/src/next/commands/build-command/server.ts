23cf17f7af734f5ae8951beadd37bb20d8f168bf
Ivan Gaiduk <ivangaiduk@ssw.com.au>
2026-02-12 14:11:47 +1000

    Adding tests to confirm that a security advisory was fixed
    * Moved filtering public env variable into its own file and wrapped into a test harness to prevent regression
    * Removed unused public env variable filtering from the next/build-command/server.ts. The code path was unused and seems to have been put there by mistake

--- packages/@tinacms/cli/src/next/commands/build-command/server.ts
+++ packages/@tinacms/cli/src/next/commands/build-command/server.ts
@@ -9,32 +9,6 @@ export const buildProductionSpa = async (
   database: Database,
   apiURL: string
 ) => {
-  // TODO: make this configurable
-  const publicEnv: Record<string, string> = {};
-  Object.keys(process.env).forEach((key) => {
-    if (
-      key.startsWith('TINA_PUBLIC_') ||
-      key.startsWith('NEXT_PUBLIC_') ||
-      key === 'NODE_ENV' ||
-      key === 'HEAD'
-    ) {
-      try {
-        // if the value is a string, we can just use it
-        if (typeof process.env[key] === 'string') {
-          publicEnv[key] = process.env[key] as string;
-        } else {
-          // otherwise, we need to stringify it
-          publicEnv[key] = JSON.stringify(process.env[key]);
-        }
-      } catch (error) {
-        // if we can't stringify it, we'll just warn the user
-        console.warn(
-          `Could not stringify public env process.env.${key} env variable`
-        );
-        console.warn(error);
-      }
-    }
-  });
   const config = await createConfig({
     plugins: [transformTsxPlugin({ configManager }), viteTransformExtension()],
     configManager,
