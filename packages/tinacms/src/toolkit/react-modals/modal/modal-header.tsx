95c1d5766f10010da73dde21083ab04292614d0c
Tiago Araújo [SSW] <tiagov8@gmail.com>
2026-02-25 08:12:16 -0300

    Update modal-header.tsx

--- packages/tinacms/src/toolkit/react-modals/modal/modal-header.tsx
+++ packages/tinacms/src/toolkit/react-modals/modal/modal-header.tsx
@@ -8,7 +8,7 @@ export interface ModalHeaderProps {
 
 export const ModalHeader = ({ children, close }: ModalHeaderProps) => {
   return (
-    <div className='min-h-14 flex items-center px-5 py-2 border-b border-gray-200 m-0'>
+    <div className='flex items-center px-5 py-2 border-b border-gray-200'>
       <TinaIcon className='w-10 h-auto -ml-1 mr-1 fill-tina-orange' />
       <ModalTitle>{children}</ModalTitle>
       {close && (
@@ -25,7 +25,7 @@ export const ModalHeader = ({ children, close }: ModalHeaderProps) => {
 
 const ModalTitle = ({ children }) => {
   return (
-    <h2 className='text-black font-sans font-medium text-base m-0 flex items-center'>
+    <h2 className='font-medium flex items-center'>
       {children}
     </h2>
   );
