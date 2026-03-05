f5e579443c22d96c5160e1d880cc3c43b8a489ef
Josh Berman [SSW] <JoshBerman@ssw.com.au>
2026-03-05 14:42:58 +1100

    cleanup playwright config

--- playwright/tina-playwright/tina/config.js
+++ playwright/tina-playwright/tina/config.js
@@ -12,12 +12,6 @@ export const config = defineConfig({
     publicFolder: "public", // The public asset folder for your framework
     outputFolder: "admin", // within the public folder
   },
-  media: {
-    tina: {
-      mediaRoot: "uploads",
-      publicFolder: "public",
-    },
-  },
   schema: {
     collections: [page, post, author],
   },
