3b44e6ad9bd2ff336aa8147b016de8b9bbb6f90d
Josh Berman [SSW] <137844305+joshbermanssw@users.noreply.github.com>
2026-03-06 15:18:24 +1100

    🐖 PostHog - Cloud Nav Items (#6494)

--- packages/tinacms/src/lib/posthog/posthog.ts
+++ packages/tinacms/src/lib/posthog/posthog.ts
@@ -85,4 +85,8 @@ export type CollectionListPageSearchPayload = {
   searchQuery: string;
 };
 
-export const EventLogPageViewedEvent: string = 'event-log-page-viewed';
+export const CloudConfigNavComponentClickedEvent: string =
+  'cloud-config-nav-component-clicked';
+export type CloudConfigNavComponentClickedPayload = {
+  itemType: 'Project Config' | 'User Management' | 'Support';
+};
