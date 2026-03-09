7b7c1d4a4f450f7eab049924a0ed8b1c195ec394
Ivan Gaiduk <ivangaiduk@ssw.com.au>
2026-03-09 17:28:55 +1000

    Remove the gates

--- packages/@tinacms/graphql/vitest.config.ts
+++ packages/@tinacms/graphql/vitest.config.ts
@@ -5,12 +5,6 @@ export default defineConfig({
   test: {
     coverage: {
       include: ['src/database/datalayer.ts', 'src/database/index.ts'],
-      thresholds: {
-        lines: 80,
-        functions: 80,
-        branches: 80,
-        statements: 80,
-      },
     },
   },
 });
