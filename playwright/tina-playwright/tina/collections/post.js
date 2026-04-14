36acf3493817c58a40818375de8f25c5b81f2162
Ben Neoh [SSW] <benjaminneoh6343@gmail.com>
2026-04-14 16:30:19 +1000

    revert to double quote

--- playwright/tina-playwright/tina/collections/post.js
+++ playwright/tina-playwright/tina/collections/post.js
@@ -1,30 +1,30 @@
 /**
- * @type {import('tinacms').Collection}
+ * @type {import("tinacms").Collection}
  */
 export default {
-  label: 'Blog Posts',
-  name: 'post',
-  path: 'content/post',
+  label: "Blog Posts",
+  name: "post",
+  path: "content/post",
   indexes: [
     {
-      name: 'title',
-      fields: [{ name: 'title' }],
+      name: "title",
+      fields: [{ name: "title" }],
     },
   ],
   fields: [
     {
-      type: 'string',
-      label: 'Title',
-      name: 'title',
+      type: "string",
+      label: "Title",
+      name: "title",
       searchable: true,
     },
     {
-      type: 'string',
-      label: 'Blog Post Body',
-      name: 'body',
+      type: "string",
+      label: "Blog Post Body",
+      name: "body",
       isBody: true,
       ui: {
-        component: 'textarea',
+        component: "textarea",
       },
     },
   ],
