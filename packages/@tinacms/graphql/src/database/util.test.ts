494b78c39842f12002b41b7f1e80dd76070e287d
Ben Neoh [SSW] <87377903+Ben0189@users.noreply.github.com>
2026-04-21 16:17:26 +1000

    Apply suggestions from code review
    Co-authored-by: Ivan Gaiduk [SSW] <gaidukfb@gmail.com>

--- packages/@tinacms/graphql/src/database/util.test.ts
+++ packages/@tinacms/graphql/src/database/util.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, vi } from 'vitest';
 import { TinaSchema } from '@tinacms/schema-tools';
 import { parseFile, stringifyFile, transformDocument } from './util';
 
