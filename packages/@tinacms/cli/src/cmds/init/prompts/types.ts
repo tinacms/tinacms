640afa734c0728b93a37b22f7ea21a75bdc17285
brookjeynes-ssw <brookjeynes@ssw.com.au>
2026-06-26 09:33:04 +1000

    feat(@tinacms/cli): add deno support

--- packages/@tinacms/cli/src/cmds/init/prompts/types.ts
+++ packages/@tinacms/cli/src/cmds/init/prompts/types.ts
@@ -5,7 +5,7 @@ export type Config = {
   typescript: boolean;
   publicFolder?: string;
   framework: Framework;
-  packageManager: 'pnpm' | 'yarn' | 'npm' | 'bun';
+  packageManager: 'pnpm' | 'yarn' | 'npm' | 'bun' | 'deno';
   forestryMigrate: boolean;
   frontMatterFormat?: ContentFrontmatterFormat;
   hosting?: 'tina-cloud' | 'self-host';
