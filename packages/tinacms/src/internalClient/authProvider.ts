c1830d9fd7ca3ee41fedf12447ec924f2571dca0
brookjeynes-ssw <brookjeynes@ssw.com.au>
2026-07-22 14:52:11 +1000

    fix: resolve possible null value usage

--- packages/tinacms/src/internalClient/authProvider.ts
+++ packages/tinacms/src/internalClient/authProvider.ts
@@ -210,7 +210,7 @@ export class TinaCloudAuthProvider extends AbstractAuthProvider {
           refresh_token: val.refresh_token ?? refresh_token,
         };
         this.setToken(newToken);
-        return Promise.resolve(newToken);
+        return newToken;
       } catch (e) {
         console.error(e);
         throw new Error('Unable to refresh auth tokens', { cause: e });
